a667fc74b46064a4b83fd552ae90e9337ee17436
Morski <morallesss@gmail.com>
2018-12-12 18:07:59 +0100

    Changed Attack value for 1st Robot.

--- RobotWars/RobotWars/Program.cs
+++ RobotWars/RobotWars/Program.cs
@@ -9,7 +9,7 @@ namespace RobotWars
         {
             List<Robot> robots = new List<Robot>()
             {
-                new Robot(){Attack = 12, Defense = 5, HP = 100},
+                new Robot(){Attack = 14, Defense = 5, HP = 100},
                 new Robot(){Attack = 6, Defense = 11, HP = 100}
             };
             Arena arena = new Arena();
