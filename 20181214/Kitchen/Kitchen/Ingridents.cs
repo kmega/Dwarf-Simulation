66994aeb27e4d19050ffed06668b95ace3c6e69f
Lukasz Radzik <radziklukasz@wp.pl>
2018-12-14 19:01:07 +0100

    niedzialajaca aplikacja

---
+++ 20181214/Kitchen/Kitchen/Ingridents.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kitchen
+{
+    public enum Ingridents
+    {
+        Chicken,
+        Tune,
+        Potatoes,
+        Asparagus,
+        Milk,
+        Honey,
+        Paprika,
+        Garlic,
+        Water,
+        Lemon,
+        Tomatoes,
+        Pickles,
+        Feta,
+        Vinegar,
+        Rice,
+        Chocolate
+    }
+}
--- 20181214/Kitchen/Kitchen/Program.cs
+++ 20181214/Kitchen/Kitchen/Program.cs
@@ -10,6 +10,31 @@ namespace Kitchen
     {
         static void Main(string[] args)
         {
+
+
+            //Użytkownik -> nazwisko, zamowienie
+            Klient klient = new Klient();
+            klient.First_Name = name_client.Adam.ToString();
+            klient.Second_Name = second_name_client.Smith.ToString();
+
+            //nazwisko -> alergie[]
+
+            string alergia = Alergic.List_Bad_Indigriends(Klient.Adam_Smith.ToString());
+
+            Console.WriteLine(alergia);
+           
+            //zamowienie -> składniki[]
+
+            string food = Food_Indigriends.List_Indigriends(Food.Emperor_Chicken.ToString());
+            Console.WriteLine(food);
+
+            //porownaniee aleri i składników
+
+            CompareList.Result(food, alergia);
+            Console.ReadKey();
+            //wyświetlenie rezultatu
+
+
         }
     }
 }
