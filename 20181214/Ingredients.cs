55c31f6b666c805d58cf05e0473e4bc691626c4d
Dawid Zajda <dawidlogan@gmail.com>
2018-12-14 18:50:20 +0100

    12-14-2018 DZ

---
+++ 20181214/Ingredients.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChickenKitchen
+{
+    public enum Ingredient
+    {
+        Chicken,
+        Tuna,
+        Potatoes,
+        Asparagus,
+        Milk,
+        Honey,
+        Paprika,
+        Garlic,
+        Water,
+        Lemon,
+        Tomatoes,
+        Pickles,
+        Feta,
+        Vinegar,
+        Rice,
+        Chocolate
+    }
+
+
+    public class Ingredients
+    {
+        public string Name { get; set; }
+        public int Quanity { get; set; }
+
+    
+        
+    }
+
+
+
+}
---
+++ 20181214/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChickenKitchen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Wejście lista składników, ilości
+
+            Dictionary<Ingredient, int> ingredientsWarehouse = new Dictionary<Ingredient, int>();
+            ingredientsWarehouse.Add(Ingredient.Potatoes, 10);
+
+            //Lista potraw
+            List<Dishes> dishesList = new List<Dishes>();
+            dishesList.Add(Dishes.Fries);
+            dishesList.Add(Dishes.SmashedPotatoes);
+
+            //Wybór i sprawdzenie potrawy
+
+            string choice = Console.ReadLine();
+
+            for (int i = 0; i < dishesList.Count -1; i++)
+            {
+                if (choice == dishesList[i].ToString())
+                {
+                    Console.WriteLine("yes");
+                }
+            }
+
+            //Sprawdzenie składników
+
+            //stworzenie potrawy - odjęcie dostępnych składników 
+
+            Console.ReadKey();
+
+
+        }
+
+    
+    }
+}
