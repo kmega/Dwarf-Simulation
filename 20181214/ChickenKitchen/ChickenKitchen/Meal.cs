b42040cb3d69b1b6c44149d7d2bc6e9e187fba90
--replace-all <luki990@onet.eu>
2018-12-14 18:44:54 +0100

    Adam smith can eat fries

---
+++ 20181214/ChickenKitchen/ChickenKitchen/Meal.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChickenKitchen
+{
+    class Meal
+    {
+        Dictionary<Enums.Meal, Enums.AllergicBase> MealWithIngredients = 
+            new Dictionary<Enums.Meal, Enums.AllergicBase>();
+
+        public Meal(Enums.Meal order)
+        {
+            MealWithIngredients.Add(order, Enums.AllergicBase.Potatoes);
+        }
+    }
+}
---
+++ 20181214/ChickenKitchen/ChickenKitchen/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChickenKitchen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //0.) Stworzenie Klienta ( Przedstawienie klienta i podanie swoich alergii)
+            Client Adam_Smith = new  Client();
+            //1.) Przyjęcie zamówienia
+            Meal order = new Meal(Enums.Meal.Fries);
+            //2.) Sprawdzenie czy osoba zamawiająca nie jest uczulona na składnik jej potrawy
+            bool result = CheckingAllergens(Adam_Smith, Enums.AllergicBase.Potatoes);
+            //3.) Jeśli osoba uczulona na składnik podanie komunikatu, że nie może zjeść tego posiłku
+            //3.) Jeśli osoba nie jest uczulona na żaden składnik, podanie dania
+        }
+
+        private static bool CheckingAllergens(Client person, Enums.AllergicBase baseOfAllergy)
+        {
+            return true;
+        }
+    }
+}
