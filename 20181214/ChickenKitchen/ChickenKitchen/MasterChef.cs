20e6926a56ef2eead6000f1fd18f2578861df120
Michał C <michalczyrnek93@gmail.com>
2018-12-15 23:08:21 +0100

    Klient could have more allergry, move prepareMeal method to separate class

---
+++ 20181214/ChickenKitchen/ChickenKitchen/MasterChef.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChickenKitchen
+{
+    class MasterChef
+    {
+        public Boolean CheckIngredients(Klient klient, Order dinner)
+        {
+            if (dinner.ingredientsToOrder.Equals(klient.allergy))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void PrepareMeal(Order dinner, Dictionary<Ingredients, int> store, Klient klient)
+        {
+
+            if (CheckIngredients(klient, dinner))
+            {
+                Console.WriteLine("This meal will kill you");
+            }
+
+            else
+            {
+                List<Ingredients> usedingredients = new List<Ingredients>();
+                foreach (var item in dinner.ingredientsToOrder)
+                {
+
+                    if (store.ContainsKey(item))
+                    {
+
+                        store[item] -= 1;
+                        usedingredients.Add(item);
+
+                    }
+
+                }
+                Console.WriteLine("We prepared {0} for {1} and it cost 1: {2}",
+                           dinner.orderName, klient.name, String.Join(", ", usedingredients));
+            }
+        }
+
+    }
+}
