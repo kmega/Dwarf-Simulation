909449559cc22f0b181ef1d078f0800071b070cd
piotr570 <piotr.rzeznik@edu.uekat.pl>
2018-12-17 19:01:54 +0100

    Task one with modded List

---
+++ 20181217/Task.1/Task.1/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            CompareBooks cb = new CompareBooks();
+            foreach (var item in cb.Compare())
+            {
+                Console.WriteLine("{0}, {1}", item.Key, item.Value);
+            }
+        }
+    }
+
+    class BooksFile
+    {
+        public List<Book> ReadBooks = new List<Book>
+        {
+            {new Book("fantasy", "titel 1")},
+            {new Book( "horror", "title 2")},
+            {new Book("fantasy", "titel 4")}
+        };
+
+        public List<Book> AllBooks = new List<Book>
+            {
+                {new Book("fantasy", "titel 1")},
+                {new Book( "horror", "title 2")},
+                {new Book("fantasy", "titel 4")},
+                {new Book("fantasy", "titel 3")},
+                {new Book( "horror", "title 5")},
+                {new Book("fantasy", "titel 6")}
+            };
+    }
+    class Book
+    {
+        public string tag;
+        public string tytul;
+        public Book(string tag, string tytul)
+        {
+            this.tag = tag;
+            this.tytul = tytul;
+        }
+    }
+    class CompareBooks
+    {
+        BooksFile bf = new BooksFile();
+
+        public List<Book> Compare()
+        {
+            List<Book> TempBooks = new List<Book>();
+
+            foreach (Book AllBook in bf.AllBooks)
+            {
+                foreach(Book ReadBook in bf.ReadBooks)
+                {
+                    if(ReadBook.tag && ReadBook.tytul)
+                }
+            }
+            return TempBooks;
+        }
+    }
+    
+     
+}
