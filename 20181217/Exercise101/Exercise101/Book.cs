ee58bf2a4bc47c67d3d69f00e633a0657c1aaf56
Keboon <jankocon7@gmail.com>
2018-12-17 21:23:01 +0100

    Added Task 101

---
+++ 20181217/Exercise101/Exercise101/Book.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise101
+{
+    enum BookType { Fantasy, Other}
+    class Book
+    {
+        public static int numberOfBooks = 0;
+        public int id { get; }
+        public BookType bookType;
+        public bool read { get; set; }
+        public Book(BookType bookType,bool read)
+        {
+            id = numberOfBooks;
+            numberOfBooks += 1;
+            this.read = read;
+            this.bookType = bookType;
+        }
+    }
+}
---
+++ 20181217/Exercise101/Exercise101/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise101
+{
+    class Program
+    {
+        public static List<Book> getFantasyBooks(List<Book> allBooks)
+        {
+            List<Book> result = new List<Book>();
+            foreach (Book book in allBooks)
+                if (book.bookType == BookType.Fantasy)
+                    result.Add(book);
+            return result;
+        }
+        public static List<Book> getLibrary()
+        {
+            List<Book> library = new List<Book>()
+            {
+                new Book(BookType.Fantasy,true),
+                new Book(BookType.Other,false),
+                new Book(BookType.Fantasy,false),
+                new Book(BookType.Fantasy,false),
+            };
+            return library;
+        }
+        public static Book getUnreadFantasyBook(List<Book> bookList)
+        {
+            foreach (Book book in bookList)
+            {
+                if (book.read == false)
+                    return book;
+            }
+            return null;
+        }
+
+        static void Main(string[] args)
+        {
+            List<Book> library = getLibrary();
+            Console.WriteLine("Pobieram wszystkie książki fantasy z biblioteki");
+            List<Book> fantasyBooks = getFantasyBooks(library);
+            Console.WriteLine("Wybieram nieprzeczytaną książkę fantasy");
+            Book selectedBook = getUnreadFantasyBook(fantasyBooks);
+            int indexOfSelectedBook = selectedBook.id;
+            library.Remove(selectedBook);
+            Console.WriteLine("Biore książkę i czytam 3h");
+            Console.WriteLine("Nadaje książce status przeczytane");
+            selectedBook.read = true;
+            Console.WriteLine("Odkładam książke na swoje miejsce");
+            library.Insert(indexOfSelectedBook, selectedBook);
+            Console.ReadKey();
+        }
+    }
+}
