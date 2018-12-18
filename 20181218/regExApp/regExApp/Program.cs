ebecad0b7e85dcb34a824c07388f838ffce261d3
Matthew-1 <matikm2@gmail.com>
2018-12-18 17:24:08 +0100

    System.ArgumentException error

--- 20181218/regExApp/regExApp/Program.cs
+++ 20181218/regExApp/regExApp/Program.cs
@@ -4,16 +4,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.Text.RegularExpressions;
 namespace regExApp
 {
     class Program
     {
+
         static void Main(string[] args)
         {
             
-            string path = @"\cybermagic\karty-postaci\1807-fryderyk-komciur.md";
+            string path = "1807-fryderyk-komciur.md";
 
             string[] fryderyk = File.ReadAllLines(path);
+
+            Console.WriteLine(fryderyk);
+
+           /* Regex readFile = new Regex(@"\((\d\d) min.*\");
+
+            MatchCollection timeFromFile;
+
+            for (int i=0; i<fryderyk.Length;i++)
+            {
+                timeFromFile = readFile.Matches(fryderyk[i]);
+
+                if (timeFromFile != null) { Console.WriteLine(timeFromFile); }
+            }
+            */
+            Console.ReadKey();
+
+
 
         }
     }
