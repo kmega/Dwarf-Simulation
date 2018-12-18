fc1c3229f65d0aa00e2a1610b85d766fe22837dc
Karol Szelążek <charles.sz@protonmail.com>
2018-12-18 18:30:03 +0100

    Zdanie 1 zrobione.

--- 20181218/workspace/workspace/Program.cs
+++ 20181218/workspace/workspace/Program.cs
@@ -11,19 +11,22 @@ namespace workspace
     {
         static string Exercise1()
         {
+            Console.Clear();
+            bool isName = true;
             string[] text = System.IO.File.ReadAllLines(@"c:\Users\Lenovo\.ssh\primary\20181218\cybermagic\karty-postaci\1807-fryderyk-komciur.md");
-            Regex rx = new Regex("title: \"(\w + \w +)\"");
+            Regex rx = new Regex(@"title: ""(\w+ \w+)""");
             for (int i = 0; i < text.Length; i++)
             {
-                if (rx.Match(text[i]).Success)
+                if (rx.Match(text[i]).Success && isName == true)
                 {
-                    Console.WriteLine(text[i]);
+                    Console.Write(text[i].Split(' ')[1].Replace('"', ' ') + " " + text[i].Split(' ')[2].Replace('"', ' '));
                     rx = new Regex(@"\((\d\d) min.*\)");
+                    isName = false;
                 }
                 if (rx.Match(text[i]).Success)
                 {
                     text[i] = text[i].Replace("(", "").Replace(")", "").Replace("min", "");
-                    Console.WriteLine("Fryderyk Komciur był budowany " + text[i] + "minuty.");
+                    Console.Write("był budowany " + text[i] + "minuty.");
                     break;
                 }
             }
