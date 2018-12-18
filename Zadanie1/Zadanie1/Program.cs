21eb8c75d4ac1f63702d86681f3eba57037686af
Marcin Pochrzest <pochrzest.marcin@gmail.com>
2018-12-18 18:07:07 +0100

    Zadanie 1 wykonane

--- Zadanie1/Zadanie1/Program.cs
+++ Zadanie1/Zadanie1/Program.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Text.RegularExpressions;
+using VivaRegex;
 
 namespace Zadanie1
 {
@@ -12,12 +13,13 @@ namespace Zadanie1
     {
         static void Main(string[] args)
         {
+            TextParser textParser = new TextParser();
             var readFile = File.ReadAllText("C:/Users/Lenovo/primary/20181218/cybermagic/karty-postaci/1807-fryderyk-komciur.md");
-            string timePattern = @"\((\d\d) min.*\)";
-            string titlepattern = @"(\w+ \w+)";
-            Match timeMatch = Regex.Match(readFile, timePattern);
-            Match titleMatch = Regex.Match(readFile, titlepattern);
-            Console.WriteLine($"{titleMatch.ToString()} był budowany {timeMatch.ToString()}");
+            string profileName = textParser.ExtractProfileName(readFile);
+            string timeToBuild = textParser.ExtractTimeToCreate(readFile);
+
+            TextFileCreator.Create($"{profileName} był budowany {timeToBuild} min");
+
             Console.ReadLine();
         }
     }
---
+++ Zadanie1/Zadanie1/TextFileCreator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie1
+{
+    public class TextFileCreator
+    {
+        public static void Create(string text)
+        {
+            File.AppendAllText("result.txt", text);
+        }
+    }
+}
