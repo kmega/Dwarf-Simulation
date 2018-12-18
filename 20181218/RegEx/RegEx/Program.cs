83dc8be6b4b8c9ac8fe7ba2a8439794295589782
Paweł Howaniec <paolopek@gmail.com>
2018-12-18 17:31:24 +0100

    commit nr 2

---
+++ 20181218/RegEx/RegEx/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace RegEx
+{
+    class Program
+    {
+        public class GetRegexResult
+        {
+            Regex regex = new Regex(@"\((\d\d) min.*\)");
+
+            public static void MatchKomciur(string inFile, string outFile)
+            {                
+                string fKomciur = File.ReadAllText(inFile);
+                string[] fKomciutrResult = Regex.Split(inFile, @"\((\d\d) min.*\)");
+            }
+            
+        }
+        static void Main(string[] args)
+        {
+            Regex regex = new Regex(@"\((\d\d) min.*\)");
+            string pathFKomciur = @"C:\Code\primary\20181218\cybermagic\karty-postaci\1807-fryderyk-komciur.md";
+            string result1 = @"result1.txt";
+
+            GetRegexResult komciurRegex = new GetRegexResult.MatchKomciur(pathFKomciur, result1);
+           
+            
+        }
+    }
+}
