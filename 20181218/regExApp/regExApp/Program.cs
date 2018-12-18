267118f65a0fae9c48c8f5aae471a44efc68cf1e
Matthew-1 <matikm2@gmail.com>
2018-12-18 16:52:05 +0100

    task 1

---
+++ 20181218/regExApp/regExApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace regExApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string path = @"\cybermagic\karty-postaci\1807-fryderyk-komciur.md";
+
+            string[] fryderyk = File.ReadAllLines(path);
+
+        }
+    }
+}
