38eb507b17abaef807d86b44f33fad8887c37582
hinren <projekt.pierscien@gmail.com>
2018-12-18 17:29:03 +0100

    New project and fist step

---
+++ 20181218_patryk_wisniewski/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            //Regex regex = new Regex(@"(\d\d) min.*");
+            string information = File.ReadAllText(@"C:\Users\Lenovo\code\primary\20181218\cybermagic\karty-postaci\1807-fryderyk-komciur.md");
+            //string[] digits = Regex.Split(sentence, @"\D+");
+            //zadanie 2
+            string[] digits = Regex.Split(information, @"(\d\d min.*)");
+            string folderPath = @"C:\Users\Lenovo\code\primary\20181218\cybermagic\karty-postaci";
+            foreach (string file in Directory.EnumerateFiles(folderPath, "*.md"))
+            {
+                string contents = File.ReadAllText(file);
+                Console.WriteLine(contents);
+            }
+            Console.ReadKey();
+        }
+    }
+}
