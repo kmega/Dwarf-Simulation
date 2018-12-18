9075fbafad9c913df9f1720172a9d5a431465cf5
Karol Szelążek <charles.sz@protonmail.com>
2018-12-18 16:51:09 +0100

    changes

---
+++ 20181218/workspace/ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = @"c:\Users\Lenovo\.ssh\20181218\cybermagic\karty-postaci\1807-fryderyk-komciur.md";
+            Console.WriteLine(text[1]);
+
+        }
+    }
+}
