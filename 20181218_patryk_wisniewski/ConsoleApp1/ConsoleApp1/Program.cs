1877a42d44174cc03e56c59ff60a5d731e8f64d0
hinren <projekt.pierscien@gmail.com>
2018-12-18 18:25:27 +0100

    Task 2 done..

--- 20181218_patryk_wisniewski/ConsoleApp1/ConsoleApp1/Program.cs
+++ 20181218_patryk_wisniewski/ConsoleApp1/ConsoleApp1/Program.cs
@@ -17,12 +17,7 @@ namespace ConsoleApp1
             //string[] digits = Regex.Split(sentence, @"\D+");
             //zadanie 2
             /*string[] digits = Regex.Split(information, @"\((\d\d) min.*\)");
-            string folderPath = @"C:\Users\Lenovo\code\primary\20181218\cybermagic\karty-postaci";
-            foreach (string file in Directory.EnumerateFiles(folderPath, "*.md"))
-            {
-                string contents = File.ReadAllText(file);
-                Console.WriteLine(contents);
-            }*/
+            */
             TextParser textParser = new TextParser();
        
             string information = File.ReadAllText(@"C:\Users\Lenovo\code\primary\20181218\cybermagic\karty-postaci\1807-fryderyk-komciur.md");
@@ -33,7 +28,21 @@ namespace ConsoleApp1
             StreamWriter resulutTask1 = new StreamWriter(path);
             resulutTask1.Write("Czas tworzenia pliku to: " + time);
             resulutTask1.Close();
-            Console.ReadKey();
+
+            string pathTask2 = @"E:\Informacje\result_task2.txt";
+            StreamWriter resulutTask2 = new StreamWriter(pathTask2);
+
+            int fullTime = 0;
+            string folderPath = @"C:\Users\Lenovo\code\primary\20181218\cybermagic\karty-postaci";
+            foreach (string file in Directory.EnumerateFiles(folderPath, "*.md"))
+            {
+                string contents = File.ReadAllText(file);
+                time = textParser.ExtractTimeToCreate(contents);
+                if( time != "")
+                    fullTime += int.Parse(time);
+            }
+            resulutTask2.WriteLine("Czas tworzenia pliku to: " + fullTime + " minut");
+            resulutTask2.Close();
         }
     }
 }
