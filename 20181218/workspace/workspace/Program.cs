79c121d85a93fd43f9d056fc614eb362cc7d84cd
Karol Szelążek <charles.sz@protonmail.com>
2018-12-18 17:27:58 +0100

    dunno2

--- 20181218/workspace/workspace/Program.cs
+++ 20181218/workspace/workspace/Program.cs
@@ -3,15 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace workspace
 {
     class Program
     {
-        static void Main(string[] args)
+        static string Exercise1()
         {
             string[] text = System.IO.File.ReadAllLines(@"c:\Users\Lenovo\.ssh\primary\20181218\cybermagic\karty-postaci\1807-fryderyk-komciur.md");
-            Console.WriteLine(text[1]);
+            Regex rx = new Regex(@"\((\d\d) min.*\)");
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (rx.Match(text[i]).Success)
+                {
+                    Console.WriteLine(text[i]);
+                }
+            }
+            return null;
+        }
+        static void Main(string[] args)
+        {
+            int userPick = 0;
+            string input = "";
+            input = Console.ReadLine();
+            userPick = Convert.ToInt32(input);
+            Console.WriteLine("");
+            switch (userPick)
+            {
+                case 1:
+                    Exercise1();
+                    break;
+            }
             Console.ReadLine();
         }
     }
