b9a7267695c1f69b1e31d92778df8ebc2fcb4be1
tetrael <kozlowskiprzemek@gmail.com>
2018-12-18 18:07:02 +0100

    something work

---
+++ 20181218/pkozlowski/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace pkozlowski
+{
+    class CharacterCard
+    {
+        public string filePath { get; set; }
+        public IEnumerable<string> content { get; set; }
+
+        public CharacterCard(string filepath)
+        {
+            filePath = filepath;
+            content = File.ReadLines(filepath);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Regex timeRegex = new Regex(@"((\d\d) min.*)");
+            //CharacterCard charCard = new CharacterCard(@"/Users/tetrael/Projekty/corealate/git/primary/20181218/cybermagic/karty-postaci/1807-fryderyk-komciur.md");
+            var file = File.ReadLines(@"/Users/tetrael/Projekty/corealate/git/primary/20181218/cybermagic/karty-postaci/1807-fryderyk-komciur.md");
+            Match match;
+            foreach (var item in file)
+            {
+                match = timeRegex.Match(item);
+                if (match.Success)
+                {
+                    Console.WriteLine(item.ToString());
+                }
+            }
+        }
+    }
+}
