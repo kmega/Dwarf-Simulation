6e0213f0d134caf3406b9fe05c7e2148017e0383
--replace-all <luki990@onet.eu>
2018-12-18 17:25:39 +0100

    Searching How Long Was Building Komciur

---
+++ RegexTasks/RegexTasks/RegexTasks/FileReader.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegexTasks
+{
+    class FileReader
+    {
+        public string[] fullstringsfromfile;
+
+        public string[] Read_File()
+        {
+            //string path = "..\\..\\..\\..\\..\\cybermagic\\karty-postaci\\1807-fryderyk-komciur.md";
+
+            // Read the stream to a string, and write the string to the console.
+
+            fullstringsfromfile = File.ReadAllLines(@"..\\..\\..\\..\\..\\20181218\\cybermagic\\karty-postaci\\1807-fryderyk-komciur.md");
+
+
+            return fullstringsfromfile;
+        }
+    }
+}
---
+++ RegexTasks/RegexTasks/RegexTasks/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegexTasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1. plik karty-postaci\1807-fryderyk-komciur -- zapisz do pliku --> resultTask1.txt
+            FileReader file1 = new FileReader();
+            String[] FryderyKomciurFile = file1.Read_File();
+
+            foreach (var item in FryderyKomciurFile)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+        }
+    }
+}
