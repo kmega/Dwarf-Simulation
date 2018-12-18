e22f95a6fac62584d976ca9256c7da694042c465
tetrael <kozlowskiprzemek@gmail.com>
2018-12-18 23:21:55 +0100

    Some of the third exercise working, rest tommorow

--- 20181218/pkozlowski/CharacterVault.cs
+++ 20181218/pkozlowski/CharacterVault.cs
@@ -1,4 +1,7 @@
 using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System;
 
 namespace pkozlowski
 {
@@ -9,6 +12,43 @@ namespace pkozlowski
         public CharacterVault()
         {
             Characters = new List<Character>();
+        }
+
+        public CharacterVault(string path, string searchPattern)
+        {
+            Characters = new List<Character>();
+            foreach (var item in Directory.EnumerateFiles(path, searchPattern))
+            {
+                if (!item.Contains("template"))
+                    Characters.Add(new Character(item));
+            }
+        }
+
+        // public void exportBuildTimeToFile(string filepath)
+        // {
+        //     int builtTime = charactersBuildTime();
+        //     TimeSpan builTime = TimeSpan.FromMinutes(builtTime);
+        //     var resultText = string.Format("Wszystkie postacie do tej pory budowne były {0} godzin i {1} minut.\n", builTime.Hours, builTime.Minutes);
+        //     File.AppendAllText(filepath, resultText);
+        // }
+
+        public void resultToFile(List<Character> characterList, string filepath, string addText = "")
+        {
+            File.AppendAllText(filepath, "Postacie, które nie mają podanego czasu to: \n");
+            characterList.ForEach(x => {
+                File.AppendAllText(filepath, x.Name + "\n");
+                }); 
+        }
+
+        public int charactersBuildTime(List<Character> characterList)
+        {
+            int builtTime = 
+                Characters.Sum(x => {
+                    int result;
+                    int.TryParse(x.BuildTime, out result);
+                    return result;
+                    });
+            return builtTime;
         }
     }
 }
--- 20181218/pkozlowski/Program.cs
+++ 20181218/pkozlowski/Program.cs
@@ -1,6 +1,5 @@
-﻿using System.IO;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 
 namespace pkozlowski
 {
@@ -8,16 +7,16 @@ namespace pkozlowski
     {
         static void Main(string[] args)
         {
-            CharacterVault characterVault = new CharacterVault();
-            //characterVault.Characters.Add(new Character(@"cybermagic/karty-postaci/1807-fryderyk-komciur.md"));
-            foreach (var item in Directory.EnumerateFiles(@"cybermagic/karty-postaci", "*.md"))
-            {
-                characterVault.Characters.Add(new Character(item));
-            }
+            CharacterVault characterVault = new CharacterVault(@"cybermagic/karty-postaci", "*.md");
+            int builtTime = characterVault.charactersBuildTime(characterVault.Characters);
+            characterVault.resultToFile(
+                characterVault.Characters.Where(x => x.BuildTime == "").ToList(), 
+                "wynik.txt");
+            int averageBuildTime = characterVault.charactersBuildTime(
+                characterVault.Characters.Where(x => x.BuildTime != "").ToList()
+                );
 
-            var result = characterVault.Characters.Where(x => x.Name == "Fryderyk Komciur").Single();
-            var resultText = result.Name + " był budowny " + result.BuildTime + " minuty.";
-            File.WriteAllText("wynik.txt", resultText);
+            //characterVault.exportBuildTimeToFile("wynik.txt");
         }
     }
 }
