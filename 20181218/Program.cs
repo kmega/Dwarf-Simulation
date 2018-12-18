e149911241fd6b980f7fa2e01186522d38ca561f
PatrycjuszL <patrycjusz4@gazeta.pl>
2018-12-18 18:24:43 +0100

    Regex-Relax-2

---
+++ 20181218/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.IO;
+
+
+
+namespace lab20181218
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Regex regex = new Regex(@"\s/Content/([a-zA-Z0-9\-]+?)\.aspx");
+            Regex regex = new Regex(@"\((\d\d) min.*\)");
+            //\((\d\d) min.*\)
+            using (StreamReader reader = new StreamReader(@"1807-fryderyk-komciur.md"))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    Match match = regex.Match(line);
+                    if (match.Success)
+                    {
+                        string v = match.Groups[1].Value;
+                        Console.WriteLine(line);
+                        Console.WriteLine("..." +v);
+                    }
+                }
+            }
+            
+        }
+    }
+}
