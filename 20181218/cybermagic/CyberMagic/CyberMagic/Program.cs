cb32f317e0560d7d1a18f0c9d88d0b26cd678dc0
Michał C <michalczyrnek93@gmail.com>
2018-12-18 18:30:45 +0100

    Zadanie 2 zrobione

--- 20181218/cybermagic/CyberMagic/CyberMagic/Program.cs
+++ 20181218/cybermagic/CyberMagic/CyberMagic/Program.cs
@@ -19,14 +19,18 @@ namespace CyberMagic
             //3. Przefiltrowaniepliku (polecenaRegexaimie) -> imie
             //imie, czas -> zapis do pliku - > result.txt
 
+            string path2 = @"C:\Users\esmic\primary\20181218\cybermagic\karty-postaci\";
             string path = @"C:\Users\esmic\primary\20181218\cybermagic\karty-postaci\1807-fryderyk-komciur.md";
             string time;
             string name;
             StreamWriter sr = new StreamWriter("result.txt");
+            StreamWriter sr2 = new StreamWriter("result2.txt");
+
+            List<string> pliki = Directory.GetFiles(path2).ToList();
             
             
 
-
+            //Zadanie 1
             string[] txt = File.ReadAllLines(path);
             TextParser tp = new TextParser();
             var xxx = string.Join("\n", txt);
@@ -38,6 +42,38 @@ namespace CyberMagic
             sr.Write("{0} był budowany {1} minuty", name, time);
             sr.Close();
 
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+
+            //Zadanie 2
+            int allTime=0;
+
+            foreach (var item in Directory.EnumerateFiles(path2)) {
+            
+                txt = File.ReadAllLines(item);
+                tp = new TextParser();
+                xxx = string.Join("\n", txt);
+                time = tp.ExtractTimeToCreate(xxx);
+               
+                name = tp.ExtractProfileName(xxx);
+                if (time == "" || name=="")
+                {
+                    continue;
+                }
+                Console.WriteLine("{0} był budowany {1} minuty", name, time);
+                sr2.WriteLine("{0} był budowany {1} minuty", name, time);
+                
+
+
+                allTime += Int32.Parse(time);
+
+            }
+            Console.WriteLine("Wszystkie postaci budowano {0} minut", allTime);
+
+            sr2.WriteLine();
+            sr2.Write("Wszystkie postaci budowano {0} minut", allTime);
+            sr2.Close();
           
 
 
