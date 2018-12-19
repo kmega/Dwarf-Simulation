bdc892b9fd185f5a57d6b230c0f79bbd5b0da828
Karol Szelążek <charles.sz@protonmail.com>
2018-12-19 17:07:40 +0100

    Zadanie 2 zrobione

--- 20181218/ConsoleApp1/ConsoleApp1/Program.cs
+++ 20181218/ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,7 +10,7 @@ namespace workspace
 {
     class Program
     {
-        static string TakeName(string[] linesOfText)
+        static string PickName(string[] linesOfText)
         {
             string name = "";
             Regex rx = new Regex(@"title: ""(\w+ \w+)""");
@@ -25,7 +25,7 @@ namespace workspace
             return name;
         }
 
-        static string TakeTime(string[] linesOfText)
+        static string PickTime(string[] linesOfText)
         {
             string time = "";
             Regex rx = new Regex(@"\((\d\d) min.*\)");
@@ -43,6 +43,8 @@ namespace workspace
             }
             else
             {
+                time = time.Replace("(", "").Replace(")", "").Replace("min", "");
+                time = time.Split(' ')[0];
                 return time;
             }
         }
@@ -52,12 +54,12 @@ namespace workspace
             Console.Clear();
             string[] linesOfText = System.IO.File.ReadAllLines(@"c:\Users\Lenovo\.ssh\primary\20181218\cybermagic\karty-postaci\1807-fryderyk-komciur.md");
             string text = "";
-            text = TakeName(linesOfText);
+            text = PickName(linesOfText);
             text = text.Replace('"', ' ').Replace("title:  ", "") + "byl budowany ";
-            text = text + TakeTime(linesOfText);
-            text = text.Replace("(", "").Replace(")", "").Replace("min", "") + "minuty";
+            text = text + PickTime(linesOfText);
+            text = text + " minuty";
             System.IO.File.WriteAllText(@"c:\Users\Lenovo\.ssh\primary\20181218\fryderykKomciur.txt", text);
-            Console.WriteLine(text);
+            Console.WriteLine(text + "\n\nChoose exercise using 1-n. Exit using 0.");
             return null;
         }
 
@@ -70,35 +72,46 @@ namespace workspace
             foreach (string file in Directory.EnumerateFiles(@"c:\Users\Lenovo\.ssh\primary\20181218\cybermagic\karty-postaci", "*.md"))
             {
                 linesOfText = File.ReadAllLines(file);
-                time = TakeTime(linesOfText);
-                time = time.Replace("(", "").Replace(")", "").Replace("min", "");
-                time = time.Split(' ')[0];
+                time = PickTime(linesOfText);
                 buildTime += Convert.ToInt32(time);
             }
-            time = "Wszystkie postacie do tej pory budowane były godzin minut";
-            System.IO.File.WriteAllText(@"c:\Users\Lenovo\.ssh\primary\20181218\fryderykKomciur.txt", time);
-            Console.WriteLine(time);
+            time = "Wszystkie postacie do tej pory budowane były " + buildTime / 60 + " godzin " + buildTime % 60 + " minut";
+            System.IO.File.AppendAllText(@"c:\Users\Lenovo\.ssh\primary\20181218\fryderykKomciur.txt", Environment.NewLine + time);
+            Console.WriteLine(time + "\n\nChoose exercise using 1-n. Exit using 0.");
             return null;
         }
 
         static void Main(string[] args)
         {
-            int userPick = 0;
+            int userPick = -1;
             string input = "";
-            Console.WriteLine("Choose exercise using 1-n.");
-            input = Console.ReadLine();
-            userPick = Convert.ToInt32(input);
-            switch (userPick)
+            Console.WriteLine("Choose exercise using 1-n. Exit using 0.");
+            while (userPick != 0)
             {
-                case 1:
-                    WriteFryderykKomciurBuildTime();
-                    break;
-                case 2:
-                    WriteAllCharactersBuildTime();
-                    break;
-                default:
-                    Console.WriteLine("Wrong input.");
-                    break;
+                input = Console.ReadLine();
+                try
+                {
+                    userPick = Convert.ToInt32(input);
+                }
+                catch
+                {
+                    Console.WriteLine("");
+                }
+                switch (userPick)
+                {
+                    case 0:
+                        Console.WriteLine("\nThanks.");
+                        break;
+                    case 1:
+                        WriteFryderykKomciurBuildTime();
+                        break;
+                    case 2:
+                        WriteAllCharactersBuildTime();
+                        break;
+                    default:
+                        Console.WriteLine("\nWrong input.");
+                        break;
+                }
             }
             Console.ReadLine();
         }
