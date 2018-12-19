72c2028e22256fddf7b1f36a48bffa3e40ffc27c
Hanna Maj-Kucharz <hanna.maj@edu.uekat.pl>
2018-12-19 18:26:56 +0100

    task 3 creating

--- Tasks/Tasks/Program.cs
+++ Tasks/Tasks/Program.cs
@@ -12,77 +12,255 @@ namespace Tasks
     {
         static void Main(string[] args)
         {
+            Task1();
+
+            Task2();
+
+            // Task 3
+
+
+            // Wczytanie wszystkich tekstów postaci w folderze
+
+            string directory = @"C:\Users\Lenovo\Desktop\primary\20181218\cybermagic\karty-postaci";
+            string[] fileNames = FindAllFileNames(directory);
+            //ścieżki plików => ignorowanie pliku template => ścieżki bez pliku template
+            List<string> ListOfCharactersTexts = FindTextInAllFiles(fileNames);
+
+            // karty postaci => zbuduj liste postaci => Lista postaci ze statami 
+            List<Character> ListOfCharactersStats = BuildAllCharactersTimeProfile(ListOfCharactersTexts);
+
+
+            // lista postaci ze statami => odfiltruj te bez czasu => Lista postaci bez czasu
+            List<Character> ListOfCharacterWithoutTimes = SelectCharactersWithoutTime(ListOfCharactersStats);
+
+
+            //lista postaci z filtrem czasu budowania => oblicz średni czas budowania postaci  =>średni czas budowania
+            double averangeTimeCharactersWithTime = AverangeCharacterBuildTime(ListOfCharactersStats);
+
+            // średni czas budowania postaci, lista postaci bez czasu, całkowity czas budowania postaci => wynik
+
+           // WriteResult(aa,bbb,ccc);
+
+
+            Console.ReadKey();
+        }
+
+        private static void WriteResult(string BuildingTime, List<Character> charactersWithoutTime, string AverangeTimee)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static double AverangeCharacterBuildTime(List<Character> listOfCharacter)
+        {
+            double sumOfTime = 0;
+            double charactersNumber = 0;
+
+            foreach (var item in listOfCharacter)
+            {
+                if (item.Time != "")
+                {
+                    sumOfTime += double.Parse(item.Time);
+                    charactersNumber += 1;
+                }
+            }
+
+            double ListOfCharactersWithoutTime = listOfCharacter.Count - charactersNumber;
+            
+            double averangeTime = sumOfTime / charactersNumber;
+
+            
+
+            foreach (var item in listOfCharacter)
+            {
+                if (item.Time == "")
+                {
+                    item.Time = averangeTime.ToString();
+                }
+            }
+            sumOfTime = 0;
+            charactersNumber = 0;
+
+            foreach (var item in listOfCharacter)
+            {
+               
+                    sumOfTime += double.Parse(item.Time);
+                    charactersNumber += 1;
+                
+            }
+            averangeTime = sumOfTime / charactersNumber;
+            return averangeTime;
+
+        }
+
+        private static List<Character> SelectCharactersWithoutTime(List<Character> listOfCharacters)
+        {
+            List<Character> listOfCharacter = new List<Character>();
+            foreach (var item in listOfCharacters)
+            {
+                if (item.Time == "")
+                {
+                    listOfCharacter.Add(item);
+                }
+            }
+            return listOfCharacter;
+        }
+
+        private static List<Character> BuildAllCharactersTimeProfile(List<string> listOfCharactersText)
+        {
+            List<Character> ListOfCharactersProfileTimes = new List<Character>();
+
+            foreach (var character in listOfCharactersText)
+            {
+                string time = FindTimeInSingleFile(character);
+                string name = FindNameInSingleFile(character);
+                ListOfCharactersProfileTimes.Add(new Character() { Time = time, Title = name });
+            };
+            return ListOfCharactersProfileTimes;
+
+
+        }
+
+        private static void Task2()
+        {
+            //Task 2
+
+            //ścieżka folderu => wyznacz wszystkie ścieżki plików => ścieżki plików
+            string directory = @"C:\Users\Lenovo\Desktop\primary\20181218\cybermagic\karty-postaci";
+            string[] fileNames = FindAllFileNames(directory);
+
+            // ścieżki plików => Wczytaj teksty plików => teksty w plikach
+
+            List<string> TextInFiles = FindTextInAllFiles(fileNames);
+
+            // lista tekstów plików => wyszukaj czasy   => czasy plików
+            List<string> ListOfTimes = TimeInFiles(TextInFiles);
+
+            // czasy plików => sumuj czasy => suma czasów
+            int sumOfTimes = SumAllFileTimes(ListOfTimes);
+
+            //suma czasów = >  konwersja czasu z minut na godziny
+            string convertedTime = HoursMinutesTimeConverter(sumOfTimes);
+
+            //Zapisz wynik
+
+            WriteResult(convertedTime);
+        }
+
+        private static void WriteResult(string time)
+        {
+            string result = $"Wszystkie postacie do tej pory budowane były {time}";
+
+            string path2 = @"C:\Users\Lenovo\Desktop\primary\Tasks\result2.md";
+            File.WriteAllText(path2, result);
+        }
+
+        private static string HoursMinutesTimeConverter(int sumOfTimes)
+        {
+            TimeSpan time = TimeSpan.FromMinutes(sumOfTimes);
+            string hours = time.Hours.ToString();
+            string minutes = time.Minutes.ToString();
+            string convertedTime = $"{hours} godzin i {minutes} minut";
+            return convertedTime;
+
+
+        }
+
+        private static int SumAllFileTimes(List<string> listOfTimes)
+        {
+            int timer = 0;
+            foreach (var time in listOfTimes)
+            {
+                if (time != "")
+                {
+                    timer += int.Parse(time);
+                }
+
+            }
+            return timer;
+        }
+
+        private static List<string> TimeInFiles(List<string> filesTime)
+        {
+            List<string> FilesTime = new List<string>();
+
+            foreach (var time in filesTime)
+            {
+                FilesTime.Add(FindTimeInSingleFile(time));
+
+            }
+            return FilesTime;
+        }
+
+        private static List<string> FindTextInAllFiles(string[] fileNames)
+        {
+            List<string> FilesText = new List<string>();
+
+
+            foreach (var file in fileNames)
+            {
+                if (!file.Contains("template"))
+                {
+                    FilesText.Add(ReadSingleFile(file));
+                }
+
+            }
+            return FilesText;
+
+
+        }
+
+        private static string[] FindAllFileNames(string directory)
+        {
+
+            string[] fileEntries = Directory.GetFiles(directory);
+            return fileEntries;
+        }
+
+        private static void Task1()
+        {
             // Task 1
             // dane wejściowe = > Wczytanie pliku = > tekst
             string path = @"C:\Users\Lenovo\Desktop\primary\20181218\cybermagic\karty-postaci\1807-fryderyk-komciur.md";
-            var openFile = ReadFile(path);
+            string openFile = ReadSingleFile(path);
 
 
             //plik wejściowy => wyszukanie czasu => czas
-            TextParser textParser = new TextParser();
-            string time = textParser.ExtractTimeToCreate(openFile);
-            //Regex regexTime = new Regex(@"\((\d\d) min.*\)");
-            //Match time = regexTime.Match(openFile);
-            //string buildTime = (time.ToString()).Substring(1, 2);
+            string fkTime = FindTimeInSingleFile(openFile);
+
 
             // plik wejściowy=> wyszukanie imienia = > imie
-            string name = textParser.ExtractProfileName(openFile);
-            //Regex regexTitle = new Regex(@"(\w+ \w+)");
-            //Match title = regexTitle.Match(openFile);
+            string fkName = FindNameInSingleFile(openFile);
 
 
             // dane wyjściowe => zapisz do pliku => plik
+            SaveResult(fkName, fkTime);
+        }
 
-            //string result = $"{title} był budowany {buildTime} minuty";
+        private static void SaveResult(string name, string time)
+        {
             string result = $"{name} był budowany {time} minuty";
 
             string path2 = @"C:\Users\Lenovo\Desktop\primary\Tasks\result.md";
             File.WriteAllText(path2, result);
-
-            //Task 2
-
-
-            string pathDirectory = @"C:\Users\Lenovo\Desktop\primary\20181218\cybermagic\karty-postaci";
-            string[] fileEntries = Directory.GetFiles(pathDirectory);
-
-
-            List<Character> ListOfCharacters = new List<Character>();
-
-            foreach (var item in fileEntries)
-            {
-                var openFile2 = ReadFile(item);
-                string name2 = textParser.ExtractProfileName(openFile2);
-                string time2 = textParser.ExtractTimeToCreate(openFile);
-
-                // poprawić z null
-                if (int.Parse(time2) > 0 && name2 != null)
-                {
-                    ListOfCharacters.Add(new Character { Title = name2, Time = time2, });
-                }
-             
-
-            }
-
-            foreach (var item in ListOfCharacters)
-            {
-                Console.WriteLine(item);
-            }
-
-
-
-            string path3 = @"C:\Users\Lenovo\Desktop\primary\Tasks\resul2.md";
-            File.WriteAllText(path3, result);
-
-
-
-            Console.ReadKey();
-
         }
 
-        private static string ReadFile(string path)
+        private static string FindTimeInSingleFile(string file)
         {
-            string source = path;
-            string openFile = File.ReadAllText(source);
+            TextParser textParser = new TextParser();
+            string time = textParser.ExtractTimeToCreate(file);
+            return time;
+        }
+
+        private static string FindNameInSingleFile(string file)
+        {
+            TextParser textParser = new TextParser();
+            string name = textParser.ExtractProfileName(file);
+            return name;
+        }
+
+        private static string ReadSingleFile(string path)
+        {
+            string openFile = File.ReadAllText(path);
             return openFile;
         }
     }
