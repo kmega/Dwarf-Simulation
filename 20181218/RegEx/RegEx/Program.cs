2d94f237954d9aa3c1e4203cc4571f666dcf0c0f
Paweł Howaniec <paolopek@gmail.com>
2018-12-19 16:03:36 +0100

    Iplemented task 1 with methods :)

--- 20181218/RegEx/RegEx/Program.cs
+++ 20181218/RegEx/RegEx/Program.cs
@@ -9,41 +9,79 @@ using VivaRegex;
 namespace RegEx
 {
     class Program
-    {            
+    {
         static void Main(string[] args)
         {
-            string timeKomciur;
-            string nameKomciur;
-            string timeWszyscy;
-            int allTime = 0;
+            // task1
+            //string ReadSingleFile(filePath) -> textFromFile
+            //string GetTime(text) -> time
+            //string GetName(text) -> name
+            //CombineResults(name + time) -> result
+            //WriteResultToFile()
+           
             string pathFKomciur = @"C:\Code\primary\20181218\cybermagic\karty-postaci\1807-fryderyk-komciur.md";
             string pathWszyscy = @"C:\Code\primary\20181218\cybermagic\karty-postaci";
             string result1 = @"C:\Code\primary\20181218\RegEx\RegEx\result1.txt";
             string result2 = @"C:\Code\primary\20181218\RegEx\RegEx\result2.txt";
-            StreamWriter result2File = new StreamWriter(result2);
 
-            //task 1
-            string fKomciur = File.ReadAllText(pathFKomciur);
-            TextParser tp = new TextParser();
+            string textKomciur = ReadSingleFile(pathFKomciur);
+            string nameKomciur = GetName(textKomciur);
+            string timeKomciur = GetTime(textKomciur);
+            string resultKomciur = CombineResults(nameKomciur, timeKomciur);
+            WriteResultToFile(result1, resultKomciur);
             
-            timeKomciur = tp.ExtractTimeToCreate(fKomciur);
-            nameKomciur = tp.ExtractProfileName(fKomciur);
-            File.WriteAllText(result1, nameKomciur + " byl budowany " + timeKomciur + " minuty");
-
             //task 2
-            foreach(var element in Directory.EnumerateFiles(pathWszyscy, "*.md"))
+            /*
+            StreamWriter result2File = new StreamWriter(result2);
+            foreach (var element in Directory.EnumerateFiles(pathWszyscy, "*.md"))
             {
                 string wszyscy = File.ReadAllText(element);
-                TextParser tp2 = new TextParser();                
+                TextParser tp2 = new TextParser();
                 timeWszyscy = tp2.ExtractTimeToCreate(wszyscy);
 
-                if(timeWszyscy != "")
+                if (timeWszyscy != "")
                 {
                     allTime += Int32.Parse(timeWszyscy);
                 }
             }
             result2File.WriteLine("Czas tworzenia wszystkich bahaterów to: {0} minuty", allTime);
             result2File.Close();
+            */
+        }
+
+        public static void WriteResultToFile(string fileResultPath, string result)
+        {
+            File.WriteAllText(fileResultPath, result);
+        }
+
+        public static string CombineResults(string name, string time)
+        {
+            string result = $"{name} byl budowany {time} minuty";
+            return result;
+            throw new NotImplementedException();
+        }
+
+        public static string GetName(string text)
+        {
+            TextParser tp = new TextParser();
+            string name = tp.ExtractProfileName(text);
+            return name;
+            throw new NotImplementedException();
+        }
+
+        public static string GetTime(string text)
+        {
+            TextParser tp = new TextParser();
+            string time = tp.ExtractTimeToCreate(text);
+            return time;
+            throw new NotImplementedException();
+        }
+
+        public static string ReadSingleFile(string filePath)
+        {
+            string textFromFile = File.ReadAllText(filePath);
+            return textFromFile;
+            throw new NotImplementedException();
         }
     }
 }
