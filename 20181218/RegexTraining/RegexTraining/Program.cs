04c0935db4ce2ec650817734ba7c975990ba7702
jankocon7@gmail.com <jankocon7@gmail.com>
2018-12-19 16:40:05 +0100

    Refactoring 1 & 2 Task

--- 20181218/RegexTraining/RegexTraining/Program.cs
+++ 20181218/RegexTraining/RegexTraining/Program.cs
@@ -11,28 +11,26 @@ namespace RegexTraining
     {
         public static void Task1(string inputPath,string outputPath)
         {
-            TextParser textParser = new TextParser();
-            string profilName = "";
-            string timeToCreate = "";
-            StringBuilder stringBuilder = new StringBuilder();
-            using (StreamReader streamReader = new StreamReader(inputPath))
+            // GIVEN : filePath, outputPath, profileName, result pattern
+            // getTimeToCreateFromFile(filePath) -> timeToCreate -int
+            // prepareStringToSave(result pattern -> stringToSave - string
+            // WriteFile(stringToSave,path) -> End
+            string profileName = "Fryderyk Komciur";
+            int timeToCreate = getTimeToCreateFromFile(inputPath);
+            string stringToSave = profileName + " był budowany " + timeToCreate + " minuty.";
+            WriteFile(stringToSave, outputPath);
+        }
+        public static void WriteFile(string content,string filePath)
+        {
+            using (StreamWriter streamWriter = new StreamWriter(filePath))
             {
-                string line = "";
-                while ((line = streamReader.ReadLine()) != null)
-                {
-                    stringBuilder.AppendLine(line);
-                }
+                streamWriter.WriteLine(content);
             }
-            profilName = textParser.ExtractProfileName(stringBuilder.ToString());
-            timeToCreate = textParser.ExtractTimeToCreate(stringBuilder.ToString());
-            using (StreamWriter streamWriter = new StreamWriter(outputPath))
-            {
-                streamWriter.WriteLine('"' + "{0} był budowany {1} minuty" + '"',profilName,timeToCreate);
-            }
-
-
-
-
+        }
+        public static string[] getFileList(string directoryPath)
+        {
+            string[] filePaths = Directory.GetFiles(directoryPath);
+            return filePaths;
 
         }
         public static int getTimeToCreateFromFile(string inputPath)
@@ -51,28 +49,35 @@ namespace RegexTraining
             int result = timeToCreateAsString.Equals(string.Empty) ? 0 : Int32.Parse(timeToCreateAsString);
             return result;
         }
-        public static void Task2(string inputDirectoryPath,string outputPath)
+        
+        public static void Task2(string directoryPath,string outputPath)
         {
-            // folder(lista plików) -> czas budowania wszystkich postaci (int)
-            string[] fileArray = Directory.GetFiles(inputDirectoryPath);
-            int counter = 0;
-            foreach(string file in fileArray)
-            {
-                counter += getTimeToCreateFromFile(file);
-            }
-            int hours = counter / 60;
-            int minutes = counter % 60;
-            using (StreamWriter streamWriter = new StreamWriter(outputPath))
-            {
-                streamWriter.WriteLine("Wszystkie postacie do tej pory budowane były {0} godzin {1} minut",hours,minutes);
-            }
-
+            // Zapisz do pliku, ile czasu zajęło budowanie WSZYSTKICH postaci z folderu
+            // GIVEN: DirectoryPath, result pattern
+            // getFileList(directorypath -> string[] filePaths - str array
+            // for each file in filePaths:
+            //      getTimeFromFile(file) 
+            //                           -> ListOfEachTimeToCreate -list
+            // Sum(listOfEachTimeToCreate) -> sumAllElements -int
+            // Calculate hours and minutes from sumAllElements -> hours,minutes
+            // CombineString(result pattern ,hour ,minutes) -> resultString -str
+            // WriteFile(resultString,outputPath) -> End
+            string[] filesInDirectory = getFileList(directoryPath);
+            List<int> timeToCreateForEachFile = new List<int>();
+            foreach(string filePath in filesInDirectory)
+                timeToCreateForEachFile.Add(getTimeToCreateFromFile(filePath));
+            int sumAllElements = timeToCreateForEachFile.Sum();
+            int hours = sumAllElements / 60;
+            int minutes = sumAllElements % 60;
+            string resultString = "Wszystkie postacie do tej pory budowane były " + hours + " godzin " + minutes + " minut";
+            WriteFile(resultString, outputPath);
         }
         static void Main(string[] args)
         {
             Task1(@"C:\Users\Lenovo\Desktop\Zajecia\primary\20181218\cybermagic\karty-postaci\1807-fryderyk-komciur.md", @"C:\Users\Lenovo\Desktop\Zajecia\primary\20181218\RegexTraining\RegexTraining\outputTask1.txt");
             Task2(@"C:\Users\Lenovo\Desktop\Zajecia\primary\20181218\cybermagic\karty-postaci\", @"C:\Users\Lenovo\Desktop\Zajecia\primary\20181218\RegexTraining\RegexTraining\outputTask2.txt");
             Console.ReadKey();
+            
         }
     }
 }
