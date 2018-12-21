293e0b773e52b8891cf5bd29989cc97ecba45ba0
piotr570 <piotr.rzeznik@edu.uekat.pl>
2018-12-21 18:11:48 +0100

    commited Task 5

--- 20181218/Task5/Task5/Program.cs
+++ 20181218/Task5/Task5/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Task5
 {
@@ -6,7 +7,27 @@ namespace Task5
     {
         public static void Main(string[] args)
         {
-            //ConfigFileReader(path)
+            //ConfigFileReader(path) -> string ReadConfig
+            string path = @"/Users/piotr/Desktop/Git/primary/20181218/Task5/conifgFile.txt";
+            List<string> configCommands = new ConfigFileReader().ConfigReader(path);
+
+            //configCommands -> list<int> intListCommands
+            //List<int> ListOfCommands = new CommandFinder().ReadCommands(configCommands);
+            //use Regex to extract commands -> List<int> Commands
+            //TaskOpener(Commands) -> 
+        }
+    }
+
+    class CommandFinder
+    {
+        public List<int> ReadCommands(string configCommands)
+        {
+            List<int> tempCommandList = new List<int>();
+            foreach (char item in configCommands)
+            {
+
+            }
+            return null;
         }
     }
 }
