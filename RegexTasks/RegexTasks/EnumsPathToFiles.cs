7bb1ec31ab41a384fc63c4144d1259e97bee22b6
--replace-all <luki990@onet.eu>
2018-12-22 08:45:34 +0100

    Task 5

---
+++ RegexTasks/RegexTasks/EnumsPathToFiles.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegexTasks
+{
+    class EnumsPathToFiles
+    {
+        //enum paths: string
+        //{
+        //    fryderyk_komciur = '..\\..\\..\\..\\..\\cybermagic\\karty-postaci\\1807-fryderyk-komciur.md';
+        //}
+    }
+}
