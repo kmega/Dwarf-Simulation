8f3e486888bc8eea772cc939b0f6116e0a86dab9
Karol Szelążek <charles.sz@protonmail.com>
2019-01-02 16:36:47 +0100

    Created project.

---
+++ 20190102/StringCalculator/StringCalculator/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringCalculator calculator = new StringCalculator();
+            int result = calculator.Sum(2, 2);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 20190102/StringCalculator/StringCalculator/StringCalculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCalculator
+{
+    class StringCalculator
+    {
+        internal int Sum(int v1, int v2)
+        {
+            return v1 + v2;
+        }
+    }
+}
