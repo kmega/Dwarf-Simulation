ece35d09a0ce2c666d47ee87fa480d4ea784795a
Marcin Pochrzest <pochrzest.marcin@gmail.com>
2019-01-02 16:31:04 +0100

    New StringCalculator project with unit testing

---
+++ 20190102/StringCalculator/StringCalculatorTest/CalculatorTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StringCalculatorTest
+{
+    [TestClass]
+    public class CalculatorTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ 20190102/StringCalculator/StringCalculatorTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("StringCalculatorTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("StringCalculatorTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("579344d6-f1b0-44b1-9cba-68d4abe2dab4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
