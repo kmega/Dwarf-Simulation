945232968cb61564e039e12d52a7442cc89c2edc
Michał Targiel <mtargiel@gmail.com>
2019-01-02 17:27:01 +0100

    add calc with tests

---
+++ StringCalc/StringCalc/Calculator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCalc
+{
+    public class Calculator
+    {
+        public int add(string values)
+        {
+
+            var numbers = values.Split(new[] { ",", "\n", "//;\n", ";"}, StringSplitOptions.None);
+            
+            int result = 0;
+            foreach (var col in numbers)
+            {
+                int.TryParse(col.Trim(), out int num);
+
+                if (num < 0)
+                    throw new ArgumentException("negatives not allowed");
+
+                result += num;
+            }
+            return result;
+
+        }
+    }
+}
