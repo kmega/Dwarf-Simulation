f0a08e542ed734b47842cdeb68c84f269f1c0d50
Karol Szelążek <charles.sz@protonmail.com>
2019-01-02 18:40:05 +0100

    Added validation for new-line and comma.

--- 20190102/StringCalculator/StringCalculator/Calculator.cs
+++ 20190102/StringCalculator/StringCalculator/Calculator.cs
@@ -8,10 +8,35 @@ namespace StringCalculator
         {
             if (numbers == "")
             {
-                numbers = "0";
+                return 0;
             }
-            int result = Convert.ToInt32(numbers);
-            return result;
+            else if (numbers.Length > 1)
+            {
+                int result = 0;
+                if (numbers.Contains(",") == true)
+                {
+                    numbers.Replace(",", " ");
+                    Console.WriteLine(numbers);
+                    numbers = numbers.Replace(",", " ");
+                    for (int i = 0; i < numbers.Split(' ').Length; i++)
+                    {
+                        result += Convert.ToInt32(numbers.Split(' ')[i]);
+                    }
+                    return result;
+                }
+                else
+                {
+                    for (int i = 0; i < numbers.Split('\n').Length; i++)
+                    {
+                        result += Convert.ToInt32(numbers.Split('\n')[i]);
+                    }
+                    return result;
+                }
+            }
+            else
+            {
+                return Convert.ToInt32(numbers);
+            }
         }
     }
 }
