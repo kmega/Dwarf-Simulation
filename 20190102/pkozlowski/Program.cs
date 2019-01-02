49fcabb97a01990abec2e52f8bb978b85b55d880
tetrael <kozlowskiprzemek@gmail.com>
2019-01-02 16:18:14 +0100

    gitignore

---
+++ 20190102/pkozlowski/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace pkozlowski
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
