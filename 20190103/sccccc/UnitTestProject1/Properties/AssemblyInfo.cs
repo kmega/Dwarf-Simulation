3892dae1c362bfa805eadf922a1045765f1bb0f6
Alicja Szweda <szwedaalicja@gmail.com>
2019-01-03 17:19:28 +0100

    string calculator w parach

---
+++ 20190103/sccccc/UnitTestProject1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9dc78414-dda4-4c92-ac95-4e7dac655926")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ 20190103/sccccc/sccccc/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sccccc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class StringCalculator
+    {
+        public int Add(string value, string[] separator)
+        {
+            List<string> temp = new List<string>();
+
+            //foreach (string s in value.Split(new[] { ",", Environment.NewLine }, StringSplitOptions.None))
+            foreach (string s in value.Split(separator, StringSplitOptions.None))
+            {
+                temp.Add(s.Replace("Bardzo", "").Replace("Lubie", "").Replace("Jendka", "").Replace("//[*][%]\n", "").Replace("]\n", ""));
+            }
+
+            int result = 0;
+            result += Extractor(temp);
+
+            return result;
+        }
+
+        public int Add(string value, string secondValue, string[] separator)
+        {
+            //string[] temp = value.Split(',');
+            List<string> temp = new List<string>();
+
+            foreach (string s in value.Split(separator, StringSplitOptions.None))
+            {
+                temp.Add(s.Replace("Bardzo", "").Replace("Lubie", "").Replace("Jendka", ""));
+            }
+
+            //string[] temp2 = secondValue.Split(',');
+
+            foreach (string s in secondValue.Split(separator, StringSplitOptions.None))
+            {
+                temp.Add(s.Replace("Make", "").Replace("DreamsComme", "").Replace("True", "").Replace("Day", ""));
+            }
+
+            int result = 0;
+            result += Extractor(temp);
+
+            return result;
+        }
+
+        private static int Extractor(string[] temp)
+        {
+            int returnValue = 0;
+            foreach (string stringNumber in temp)
+            {
+                int.TryParse(stringNumber, out int intNumber);
+                if(IsPositiveAndSmallerThan1000(intNumber))
+                {
+                    returnValue += intNumber;
+                }
+            }
+
+            return returnValue;
+        }
+       
+        private static int Extractor(List<string> temp)
+        {
+            int returnValue = 0;
+            foreach (string stringNumber in temp)
+            {
+                int.TryParse(stringNumber, out int intNumber);
+                if (IsPositiveAndSmallerThan1000(intNumber))
+                {
+                    returnValue += intNumber;
+                }
+            }
+
+            return returnValue;
+        }
+        private static bool IsPositiveAndSmallerThan1000(int intNumber)
+        {
+            if (intNumber >= 0 && intNumber <= 1000)
+                return true;
+            else throw new ArgumentOutOfRangeException(String.Format("Only positves numbers and smaller than 1000: =" + intNumber));
+        }
+    }
+}
