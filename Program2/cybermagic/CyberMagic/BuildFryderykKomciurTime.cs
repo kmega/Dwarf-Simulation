49d70b808f386102308d5bf39f7ecb2551b05fd7
Lukasz Radzik <radziklukasz@wp.pl>
2019-01-03 18:47:57 +0100

    second program

---
+++ Program2/cybermagic/CyberMagic/BuildFryderykKomciurTime.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CyberMagic
+{
+    public class BuildFryderykKomciurTime
+    {
+        public string BuildFryderykKomciur()
+        {
+            string filetxt;
+            string url = @"../../../karty-postaci/1807-fryderyk-komciur.md";
+
+            StringReturner returner = new StringReturner();
+            filetxt = returner.FileText(url);
+
+            TextParser parser = new TextParser();
+            string result = "Fryderyk Komciur był budowany " + parser.ExtractTimeToCreate(filetxt) + " minuty";
+
+            return result;
+        }
+    }
+}
---
+++ Program2/cybermagic/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CyberMagic;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class CyberMagicTests
+    {
+        [TestMethod]
+        public void RetrunFryderykKomciurBulitTime()
+        {
+            BuildFryderykKomciurTime createTmie = new BuildFryderykKomciurTime();
+            string result = createTmie.BuildFryderykKomciur();
+
+            Assert.AreEqual(result, "Fryderyk Komciur był budowany 23 minuty");
+        }
+
+        [TestMethod]
+        public void BulitAllCharactersTime()
+        {
+            BuildAllCharactersTime allCharactersTime = new BuildAllCharactersTime();
+            string result = allCharactersTime.BuildAllCharacters();
+
+            Assert.AreEqual(result, "");
+        }
+    }
+}
