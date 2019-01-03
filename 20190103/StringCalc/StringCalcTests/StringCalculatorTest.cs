7b176fe4655b035941ce21b4d3ec1a0d51c8aa54
Marcin Pochrzest <pochrzest.marcin@gmail.com>
2019-01-03 15:47:17 +0100

    Added test project

---
+++ 20190103/StringCalc/StringCalcTests/StringCalculatorTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StringCalcTests
+{
+    [TestClass]
+    public class StringCalculatorTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
