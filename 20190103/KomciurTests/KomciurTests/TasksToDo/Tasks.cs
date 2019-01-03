52c00d6d8363161df278887d4dc9313aaf4fac95
Paweł Howaniec <paolopek@gmail.com>
2019-01-03 18:39:06 +0100

    fsdfa

---
+++ 20190103/KomciurTests/KomciurTests/TasksToDo/Tasks.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using KomciurTests;
+
+namespace KomciurTests.TasksToDo
+{
+    public class Tasks
+    {
+        TextParser parser = new TextParser();
+        public string task1ForTest;
+
+        public void Task_1()
+        {
+            string path = @"C:\Code\primary\20190103\KomciurTests\cybermagic\karty-postaci\1807-fryderyk-komciur.md";
+            string file = File.ReadAllText(path);
+            string pathToSave = "result1.txt";
+
+            string time = parser.ExtractTimeToCreate(file);
+            this.task1ForTest = time;
+            File.AppendAllText(pathToSave, time);
+        }
+
+    }
+}
---
+++ 20190103/KomciurTests/UnitTestProject/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using KomciurTests.TasksToDo;
+
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TimeOfCreatingKomciur()
+        {
+            Tasks task_1 = new Tasks();
+
+            string result = task_1.task1ForTest;
+
+            Assert.AreEqual("23", result);
+        }
+    }
+}
