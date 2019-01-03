a04002ce1f7858348dee60130e4b2aaa825addfc
Marcin Pochrzest <pochrzest.marcin@gmail.com>
2019-01-03 15:59:55 +0100

    Added class

--- 20190103/StringCalc/StringCalc/StringCalculator.cs
+++ 20190103/StringCalc/StringCalc/StringCalculator.cs
@@ -10,10 +10,15 @@ namespace StringCalc
     {
         public int Add(string numbers)
         {
+            int result = 0;
             var separator = new string[] { "," };
+            var separatedLines = numbers.Split(new string[] { "/n" }, StringSplitOptions.None);
             var seperatedNumbers = numbers.Split(separator, StringSplitOptions.None);
-           // foreach(var number in )
-            return 0;
+            foreach(var number in seperatedNumbers)
+            {
+                result += Int32.Parse(number);
+            }
+            return result;
         }
     }
 }
