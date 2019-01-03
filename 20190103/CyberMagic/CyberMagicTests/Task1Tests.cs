162d3c9dc2fc745445f03b0b600d28f7f2cad1ce
Marcin Pochrzest <pochrzest.marcin@gmail.com>
2019-01-03 18:03:35 +0100

    New Cybermagic project

---
+++ 20190103/CyberMagic/CyberMagicTests/Task1Tests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CyberMagicTests
+{
+    [TestClass]
+    public class Task1Tests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
