cbb1cfa1908cf7253bda034ee0980bf83582ea09
Marcin Pochrzest <pochrzest.marcin@gmail.com>
2019-01-03 18:20:28 +0100

    Task 1 Completed

--- 20190103/CyberMagic/CyberMagic/Program.cs
+++ 20190103/CyberMagic/CyberMagic/Program.cs
@@ -10,6 +10,21 @@ namespace CyberMagic
     {
         static void Main(string[] args)
         {
+            Task1(@"cybermagic\karty-postaci\1807-fryderyk-komciur.md");
         }
+
+        private static void Task1(string fileFullPath)
+        {
+            //Task 1
+            //Wczytaj plik
+            string file = TextFileManager.ReadFile(fileFullPath);
+            //Wyfiltruj imie
+            string name = InformationExtractor.ExtractSingleName(file);
+            //Wyfiltruj czas
+            int time = InformationExtractor.ExtractSingleTime(file);
+            // Wyswietl wyniki
+            TextFileManager.WriteResults(new string[] { $"{name} był budowany {time} minuty." });
+        }
+
     }
 }
---
+++ 20190103/CyberMagic/CyberMagic/TextFileManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CyberMagic
+{
+    public static class TextFileManager
+    {
+        internal static string ReadFile(string fileName)
+        {
+            string text = File.ReadAllText(fileName);
+            return text;
+
+        }
+        public static void WriteResults(string[] toSave)
+        {
+            File.AppendAllLines("result.txt", toSave);
+        }
+
+    }
+}
