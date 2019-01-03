6539efa5248cb63a270ad124eb39170da866f137
Alicja Szweda <szwedaalicja@gmail.com>
2019-01-03 18:45:27 +0100

    Happy collection with tests

---
+++ 20190103/happypathpiotrekalicja/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using happypathpiotrekalicja;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class Task1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var Getter = new Operations();
+            string komciurPath = @"C:\Users\lysia\cUserslysia.ssh\primary\20181218\cybermagic\karty-postaci\1807-fryderyk-komciur.md";
+
+            string name = Getter.ReturnName(komciurPath); 
+            int time = Getter.ReturnTime(komciurPath);
+
+            int expectedTime = 23;
+            string expectedName = "Fryderyk Komciur";
+
+            Assert.AreEqual(time, expectedTime);
+            Assert.AreEqual(name, expectedName);
+        }
+    }
+}
---
+++ 20190103/happypathpiotrekalicja/happypathpiotrekalicja/Operations.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace happypathpiotrekalicja
+{
+    public class Operations
+    {
+        public string ReturnName(string komciurPath)
+        {
+            TextParser textParser = new TextParser();
+
+            string content = File.ReadAllText(komciurPath);
+            string name = textParser.ExtractProfileName(content);
+
+            return name;
+        }
+
+        public int ReturnTime(string komciurPath)
+        {
+            TextParser textParser = new TextParser();
+
+            string content = File.ReadAllText(komciurPath);
+            int time = Convert.ToInt16(textParser.ExtractTimeToCreate(content));
+
+            return time;
+        }
+    }
+}
