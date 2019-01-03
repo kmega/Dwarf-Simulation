7a8b1b28a1d7f56b0e24d00b3828733f5f3fcffe
Lukasz Radzik <radziklukasz@wp.pl>
2019-01-03 17:27:56 +0100

    string calculator with tests

---
+++ String_Calculator/String_Calculator/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String_Calculator
+{
+    public class Calculator
+    {
+
+
+        public int Add(string value)
+        {
+            int sum = 0;
+            string delimiter = "";
+            string[] values_almost;
+
+            if (value != "")
+            {
+                delimiter = CheckForDelimeters(value);
+
+                values_almost = value.Split(new string[] {
+     ",",
+     "\n",
+     "/",
+     "[",
+     "]",
+     delimiter
+    }, StringSplitOptions.None);
+                string[] values = values_almost.Except(new[] {
+     string.Empty, ""
+    }).ToArray();
+
+                sum = CheckForNegativesAndHigherThan1000(values);
+
+            }
+
+            return sum;
+        }
+
+        private int CheckForNegativesAndHigherThan1000(string[] values)
+        {
+            string negative_numbers = "";
+            bool flag = false;
+            int sum = 0;
+            foreach (var item in values)
+            {
+                try
+                {
+                    if (item[0] == '-')
+                    {
+                        negative_numbers += item + " ";
+                        flag = true;
+                    }
+                    int number = Int32.Parse(item);
+                    if (number < 1000)
+                    {
+                        sum += number;
+                    }
+                }
+                catch
+                {
+                    throw;
+                }
+            }
+
+            if (flag)
+            {
+                throw new ArgumentException("Negatives not allowed: " + negative_numbers);
+            }
+
+            return sum;
+        }
+
+        private string CheckForDelimeters(string value)
+        {
+            string delimiter = "";
+
+            if (value[0] == '/' && value[1] == '/')
+            {
+                if (value[2] == '[')
+                {
+                    for (int i = 3; i < value.Length; i++)
+                    {
+                        if (value[i] == ']')
+                        {
+                            break;
+                        }
+                        delimiter += value[i];
+                    }
+                }
+                else
+                {
+                    delimiter = value[2].ToString();
+                }
+
+            }
+            return delimiter;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ String_Calculator/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using String_Calculator;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class StringCalculatorTest
+    {
+        public int TestInput(string value)
+        {
+            Calculator calculator = new Calculator();
+            int result = calculator.Add(value);
+            return result;
+        }
+
+        [TestMethod]
+        public void ReturnValue0WhenStringIs0()
+        {
+            string value = "0";
+
+            int result = TestInput(value);
+
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void ReturnValue1WhenStringIs1()
+        {
+            string value = "1";
+
+            int result = TestInput(value);
+
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void ReturnValue3WhenStringIs1Comma2()
+        {
+            string value = "1,2";
+
+            int result = TestInput(value);
+
+            Assert.AreEqual(3, result);
+        }
+
+        [TestMethod]
+        public void ReturnValue6WhenStringIs1NewLine2Comma3()
+        {
+            string value = "1\n2,3";
+
+            int result = TestInput(value);
+
+            Assert.AreEqual(6, result);
+        }
+
+        [TestMethod]
+        public void ReturnValue0WhenStringIsEmpty()
+        {
+            string value = "";
+
+            int result = TestInput(value);
+
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(FormatException),
+    "Error")]
+        public void ShouldThrowExceptionWhenStringIs1CommaNewLine()
+        {
+            string value = "1,/n";
+            TestInput(value);
+        }
+
+        [TestMethod]
+        public void ReturnValue3WhenStringHasDelimiter()
+        {
+            string value = "//;\n1;2";
+
+            int result = TestInput(value);
+
+            Assert.AreEqual(3, result);
+        }
+
+        [TestMethod]
+        public void ReturnValue5WhenStringHasDelimiter()
+        {
+            string value = "//.\n2.3";
+
+            int result = TestInput(value);
+
+            Assert.AreEqual(5, result);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException),
+"Error")]
+        public void ShouldThrowExceptionWhenNegativeNumbers()
+        {
+            string value = "-1,-2";
+            TestInput(value);
+        }
+
+        [TestMethod]
+        public void ReturnValue2WhenStringHasHighValue()
+        {
+            string value = "2,1001";
+
+            int result = TestInput(value);
+
+            Assert.AreEqual(2, result);
+        }
+
+        [TestMethod]
+        public void ReturnValue6WhenStringHasLongDelimiter()
+        {
+            string value = "//[***]\n1***2***3";
+
+            int result = TestInput(value);
+
+            Assert.AreEqual(6, result);
+        }
+    }
+}
