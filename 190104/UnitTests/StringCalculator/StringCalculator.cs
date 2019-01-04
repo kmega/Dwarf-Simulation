3aeeb84fdc4815bd641591bdc784b74b87918237
tetrael <kozlowskiprzemek@gmail.com>
2019-01-04 17:44:53 +0100

    without task 5 from string controller

--- 190104/UnitTests/StringCalculator/StringCalculator.cs
+++ 190104/UnitTests/StringCalculator/StringCalculator.cs
@@ -1,9 +1,44 @@
 ﻿using System;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using System.Linq;
 
-namespace StringCalculator
+namespace StringCalculatorNS
 {
-    class StringCalculator
+    public class StringCalculator
     {
+        // Given 
+        // String that has "", 1 or 1,2
+        public string _numbers;
+
+        // Then
+        // Parse numbers from string
+        
+
+        // Expected
+        // sum of numbers from string
+        public int Add(string numbers)
+        {
+            if (numbers.Equals(""))
+            {
+                return 0;
+            }
+
+            List<int> _numbers = new List<int>();
+            MatchCollection _matches = Regex.Matches(numbers, @"\d+", RegexOptions.Multiline);
+
+            foreach(var match in _matches)
+            {
+                Int32 tmpInt = Convert.ToInt32(match.ToString());
+                if (tmpInt < 0){
+                    throw new ArgumentException("negatives not allowed", tmpInt.ToString());
+                }
+                
+                _numbers.Add(tmpInt);            
+            }
+            return _numbers.Sum();
+        }
+
         static void Main(string[] args)
         {
             
--- 190104/UnitTests/StringCalculatorTests/StringCalculatorTests.cs
+++ 190104/UnitTests/StringCalculatorTests/StringCalculatorTests.cs
@@ -1,13 +1,70 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StringCalculatorNS;
+using System;
 
-namespace StringCalculatorTests
+namespace StringCalculatorTestNS
 {
     [TestClass]
     public class StringCalculatorTests
     {
-        [TestMethod]
-        public void TestMethod1()
+        private readonly StringCalculator _stringCalculator;
+
+        public StringCalculatorTests()
         {
+            _stringCalculator = new StringCalculator();
+        }
+
+        [TestMethod]
+        public void GivenStringIsEmptyThenResult0()
+        {
+            var result = _stringCalculator.Add("");
+            var expected = 0;
+
+            Assert.AreEqual(result, expected);
+        }
+
+        [TestMethod]
+        public void GivenStringHas1ThenResult1()
+        {
+            var result = _stringCalculator.Add("1");
+            var expected = 1;
+
+            Assert.AreEqual(result, expected);
+        }
+
+        [TestMethod]
+        public void GivenStringHas12ThenResult3()
+        {
+            var result = _stringCalculator.Add("1,2");
+            var expected = 3;
+
+            Assert.AreEqual(result, expected);
+        }
+
+        [TestMethod]
+        public void GivenStringHas1n2ThenResult6()
+        {
+            var result = _stringCalculator.Add("1\n2,3");
+            var expected = 6;
+
+            Assert.AreEqual(result, expected);
+        }
+
+        [TestMethod]
+        public void GivenStringHasDifferentDelimiterThenResult3()
+        {
+            var result = _stringCalculator.Add("//;\n1;2");
+            var expected = 3;
+
+            Assert.AreEqual(result, expected);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException),
+        "negatives not allowed")]
+        public void GivenStringHasNegativeNumberThenThrowExeption()
+        {
+            _stringCalculator.Add("-2");
         }
     }
 }
