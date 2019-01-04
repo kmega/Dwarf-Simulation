9dcd9262d447933fdd887cafb5fcb418b177c61b
Krzysztof Gebski <krzysztof.gebski@gmail.com>
2019-01-04 23:56:22 +0100

    Created solution String calculator with two projects: StringCalculator and

---
+++ StringCalculator/StringCalculatorTests/StringCalculatorTests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace StringCalculatorTests
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            Assert.Fail();
+        }
+    }
+}
