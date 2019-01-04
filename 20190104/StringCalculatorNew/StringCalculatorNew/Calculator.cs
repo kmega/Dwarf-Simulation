b32f35bcaeadfa8e2870f6125113b86216f758c3
Michał C <michalczyrnek93@gmail.com>
2019-01-04 17:06:43 +0100

    Something was delete, but task 5 done

---
+++ 20190104/StringCalculatorNew/StringCalculatorNew/Calculator.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCalculatorNew
+{
+    public class Calculator
+    {
+        public int Sum(string input)
+        {
+            int result = 0;
+            List<string> delimeterlist = new List<string>();
+           public List<int> exception = new List<int>();
+
+            if (input.StartsWith("//"))
+            {
+                string [] parts = input.Split('\n');
+                delimeterlist.Add(parts[0].Remove(0, 2));
+            }
+
+
+            else
+            {
+                delimeterlist.Add(",");
+                delimeterlist.Add("\n");
+                
+
+            }
+
+            string[] delimeter = delimeterlist.ToArray();
+            string[] numbers = input.Split(delimeter, StringSplitOptions.None);
+
+
+            foreach (var item in numbers)
+    {
+                if (Int32.TryParse(item, out int n))
+                {
+                    if (n < 0)
+                    {
+                        exception.Add(n);
+
+
+                    }
+                    else
+                    {
+                        result += n;
+                    }
+                }
+    }
+
+            if (exception.Count>0)
+            {
+                throw new Exception("Nie wolno używac ujemnych" + String.Join(", ", exception));
+            }
+            return result;
+
+        }
+    }
+
---
+++ 20190104/StringCalculatorNew/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StringCalculatorNew;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+        Calculator calc = new Calculator();
+        [TestMethod]
+        public void EmptyisZero()
+        {
+            string input = "";
+           int result =calc.Sum(input);
+
+            Assert.AreEqual(0, result);
+
+        }
+        [TestMethod]
+        public void shouldReturn3beacause1and2()
+        {
+            string input = "1,2";
+            int result = calc.Sum(input);
+
+            Assert.AreEqual(3, result);
+
+        }
+
+        [TestMethod]
+        public void shouldReturn6withnewline()
+        {
+            string input = "1\n2,3";
+            int result = calc.Sum(input);
+
+            Assert.AreEqual(6, result);
+
+        }
+        [TestMethod]
+        public void shouldReturn3withdeclaredelimeter()
+        {
+            string input = "//;\n1;2";
+            int result = calc.Sum(input);
+
+            Assert.AreEqual(3, result);
+
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception),"Nie wolno ujemnych")]
+        public void shouldReturnException()
+        {
+            string input = "-1";
+            int result = calc.Sum(input);
+            
+
+        }
+
+        [TestMethod]
+      
+        public void shouldReturnListExeption()
+        {
+            string input = "-1,-2";
+            int resulttemp = calc.Sum(input);
+            int result = calc.exception.Count;
+
+            Assert.IsTrue(result > 1);
+
+        }
+
+
+
+    }
+}
