e7b3453bd6595a3c680c8e643b84d24ff10afad5
hinren <projekt.pierscien@gmail.com>
2019-01-04 18:39:03 +0100

    Save projects

---
+++ TeaFast/TeaPrograms/TeaTest/UnitTest1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TeaPrograms;
+
+namespace TeaTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestSortTeaInformation()
+        {
+            List<ListTeaClass> informatioAboutTea = new List<ListTeaClass>();
+            informatioAboutTea.Add(new ListTeaClass
+            {
+                teaName = "Gunpowder Czarny",
+                typeTea = "czarna",
+                boilingpoint = "90",
+                boilingTime = "3"
+            });
+
+            informatioAboutTea.Add(new ListTeaClass
+            {
+                teaName = "Gunpowder Zielony",
+                typeTea = "zielona",
+                boilingpoint = "70",
+                boilingTime = "3"
+            });
+
+            var newList = informatioAboutTea.OrderBy(x => x.typeTea).ToList();
+            Assert.AreEqual(newList[0].typeTea, "czarna");
+        }
+
+        [TestMethod]
+        public void TestReverseTeaInformation()
+        {
+            DataTestInformation testingData = new DataTestInformation();
+            var informatioAboutTea = testingData.InformationTest();
+
+            informatioAboutTea.Reverse();
+            Assert.AreEqual(informatioAboutTea[0].typeTea, "napar");
+        }
+
+        [TestMethod]
+        public void TestMakeATeaWhenMintHave96Temperatureand300secondsresultPerfect()
+        {
+            DataTestInformation testingData = new DataTestInformation();
+            var informatioAboutTea = testingData.InformationTest();
+
+            MakeATeaTask make = new MakeATeaTask();
+
+            string quality = make.QualityTea(informatioAboutTea, 96, 300);
+            Assert.AreEqual(quality, "Perfect");
+        }
+
+        [TestMethod]
+        public void TestMakeATeaWhenMintHave91Temperatureand310secondsresultPerfect()
+        {
+            DataTestInformation testingData = new DataTestInformation();
+            var informatioAboutTea = testingData.InformationTest();
+
+            MakeATeaTask make = new MakeATeaTask();
+
+            string quality = make.QualityTea(informatioAboutTea, 91, 310);
+            Assert.AreEqual(quality, "Perfect");
+        }
+
+        [TestMethod]
+        public void TestMakeATeaWhenMintHave80Temperatureand300secondsresultweak()
+        {
+            DataTestInformation testingData = new DataTestInformation();
+            var informatioAboutTea = testingData.InformationTest();
+
+            MakeATeaTask make = new MakeATeaTask();
+
+            string quality = make.QualityTea(informatioAboutTea, 91, 310);
+            Assert.AreEqual(quality, "Weak");
+        }
+
+        [TestMethod]
+        public void TestMakeATeaWhenMintHave80Temperatureand350secondsresultyucky()
+        {
+            DataTestInformation testingData = new DataTestInformation();
+            var informatioAboutTea = testingData.InformationTest();
+
+            MakeATeaTask make = new MakeATeaTask();
+
+            string quality = make.QualityTea(informatioAboutTea, 91, 310);
+            Assert.AreEqual(quality, "Yucky");
+        }
+    }
+}
---
+++ TryCalcIn40/Calc/Calc/Calculator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calc
+{
+    public class Calculator
+    {
+        public int add(string values)
+        {
+
+            var text = values.Split('/');
+            char split = text[2][0];
+
+            var numbersFromDelimeter = values.Split(split);
+
+            var numbers = values.Split(new[] { ",", "\n", "/", ";", "*", "%" }, StringSplitOptions.RemoveEmptyEntries);
+
+
+            int result = 0;
+            foreach (var col in numbers)
+            {
+                int.TryParse(col.Trim(), out int num);
+                if (num < 1000)
+                {
+                    result += num;
+                }
+                else if (num > 0)
+                {
+                    result += num;
+                }
+                
+            }
+            return result;
+        }
+    }
+}
