1add0b61a02727bcc52fc5e9907a649c018bec7f
Krzysztof Gebski <krzysztof.gebski@gmail.com>
2019-01-06 22:54:00 +0100

    added all tests to Add method

--- StringCalculatorKataTDD/SC/StringCalculator.cs
+++ StringCalculatorKataTDD/SC/StringCalculator.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace StringCalculatorKata
 {
@@ -10,11 +8,32 @@ namespace StringCalculatorKata
     {
         public int Add(string numbers)
         {
-            if (numbers.Length == 0)
+            if (string.IsNullOrEmpty(numbers))
             {
                 return 0;
             }
-            return int.Parse(numbers);
+
+            var numbersStringArray = numbers.Replace('\n', ',').Split(',');
+
+            if (numbersStringArray[0].StartsWith("//"))
+            {
+                var delimeters = numbersStringArray[0].Remove(0,2).Distinct();
+                foreach (var delimeter in delimeters)
+                {
+                    numbersStringArray[1] = numbersStringArray[1].Replace(delimeter, ',');
+                }
+                numbersStringArray = numbersStringArray[1].Split(',');
+            }
+
+            var numbersArray = numbersStringArray.Where(s => !string.IsNullOrEmpty(s)).Select(s => int.Parse(s));
+
+            if (numbersArray.Any(p => p < 0))
+            {
+                throw new Exception($"Negatives not allowed {string.Join(" ", numbersArray.Where(p => p < 0))}");
+            }
+
+            var sum = numbersArray.Where(s => s <= 100).Sum();
+            return sum;
         }
     }
 }
--- StringCalculatorKataTDD/StringCalculatorKataTests/StringCalculatorTests.cs
+++ StringCalculatorKataTDD/StringCalculatorKataTests/StringCalculatorTests.cs
@@ -1,29 +1,55 @@
 ﻿using NUnit.Framework;
 using StringCalculatorKata;
+using System;
 
 namespace StringCalculatorKataTests
 {
     [TestFixture]
     public class StringCalculatorTests
     {
-        [Test]
-        public void Add_EmptyString_ReturnsZero()
+        public StringCalculator sc = new StringCalculator();
+
+        [TestCase(0, "")]
+        public void Add_EmptyString_ReturnsZero(int expected, string numbers)
         {
-            StringCalculator sc = new StringCalculator();
+            var result = sc.Add(numbers);
 
-            int result = sc.Add("");
+            Assert.That(expected, Is.EqualTo(result));
+        }
 
-            Assert.That(0, Is.EqualTo(result));
+        [TestCase(14, "14")]
+        [TestCase(0, "0")]
+        public void Add_SingleNumber_ReturnsThatNumber(int expected, string numbers)
+        {
+            var result = sc.Add(numbers);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestCase(3, "1,2")]
+        [TestCase(9, "1,2,6")]
+        [TestCase(10, "5,0,5")]
+        [TestCase(6, "1\n2,3")]
+        [TestCase(3, "//;\n1;2")]
+        [TestCase(2, "2,1001")]
+        [TestCase(6, "//***\n1***2***3")]
+        [TestCase(6, "//*****\n1*****2*****3")]
+        [TestCase(6, "//*%\n1*2%3")] 
+        public void Add_MultipleNumbers_ReturnsSum(int expected, string numbers)
+        {
+            var result = sc.Add(numbers);
+
+            Assert.AreEqual(expected, result);
         }
 
         [Test]
-        public void Add_SingleNumber_ReturnsThatNumber()
+        public void Add_NegativeNumbers_ThrowsException()
         {
-            StringCalculator sc = new StringCalculator();
-
-            int result = sc.Add("1");
-
-            Assert.AreEqual(1, result);
+            string number = "1\n2,-3";
+            var exception = Assert.Throws<Exception>(() => sc.Add(number));
+            Assert.AreEqual("Negatives not allowed -3", exception.Message);
         }
+
+        
     }
 }
