0d67b6fc58393717fd0c358162ecf9beff282500
Karol Szelążek <charles.sz@protonmail.com>
2019-01-07 17:48:09 +0100

    Started happy planes project

---
+++ 20190107/ConsoleApp4/ConsoleApp4/Airport.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+[assembly: InternalsVisibleTo("UnitTestProject1")]
+
+namespace ConsoleApp4
+{
+    internal class Airport
+    {
+        internal void Simulation(int turns, int planes, bool[] landingPlaces)
+        {
+            for (int i = 0; i < turns; i++)
+            {
+                bool planeCanLand = FindClearLandingPlace(landingPlaces);
+            }
+        }
+
+        internal bool FindClearLandingPlace(bool[] landingPlaces)
+        {
+            bool planeCanLand = false;
+            for (int i = 0; i < landingPlaces.Length; i++)
+            {
+                if (landingPlaces[i] == false)
+                {
+                    landingPlaces[i] = true;
+                    planeCanLand = true;
+                }
+            }
+            return planeCanLand;
+        }
+    }
+}
---
+++ 20190107/ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int turns = 1000;
+            int planes = 50;
+            bool[] landingPlaces = {
+                false,
+                false,
+                false,
+                false,
+                false,
+                false,
+                false,
+                false,
+                false,
+                false
+            };
+            
+            Airport airport = new Airport();
+            airport.Simulation(turns, planes, landingPlaces);
+        }
+    }
+}
