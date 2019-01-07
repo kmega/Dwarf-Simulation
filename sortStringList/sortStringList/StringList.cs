e5a2dd81b6b24b22d7c2932d91f78dc2e0477bd8
Krzysztof Gebski <krzysztof.gebski@gmail.com>
2019-01-07 18:17:30 +0100

    sortStringList exercise with tests

---
+++ sortStringList/sortStringList/StringList.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace sortStringList
+{
+    public class StringList
+    {
+        public string SortStringList(string beforeSort)
+        {
+            var stringList = beforeSort.Split(',');
+
+            Regex letter = new Regex("^[a-zA-Z]$");
+            Regex number = new Regex("^[0-9]*$");
+
+            var oddLettersList = stringList
+                .Where((c, i) => i % 2 == 0)
+                .Where(s => letter.Match(s).Success)
+                .OrderBy(s => s)
+                .ToList();
+
+            var evenLettersList = stringList
+                .Where((c, i) => i % 2 != 0)
+                .Where(s => letter.Match(s).Success)
+                .OrderBy(s => s)
+                .ToList();
+
+            var numberList = stringList
+                .Where(s => int.TryParse(s, out int result))
+                .Select(s => int.Parse(s))
+                .OrderBy(s => s)
+                .Select(s => s.ToString())
+                .ToList();
+
+            List<string> resultString = new List<string>();
+
+            resultString.AddRange(oddLettersList);
+            resultString.AddRange(numberList);
+            resultString.AddRange(evenLettersList);
+
+            return string.Join(",", resultString);
+        }
+    }
+}
---
+++ sortStringList/sortStringListTests/StringListTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using sortStringList;
+
+namespace sortStringListTests
+{
+    [TestFixture]
+    public class StringListTests
+    {
+        [TestCase("a,b,1,7,4,c,9,d,14,k,a,1,2", "a,a,1,1,2,4,7,9,14,b,c,d,k")]
+        public void SortString(string beforeSort, string afterSort)
+        {
+            StringList st = new StringList();
+
+            string result = st.SortStringList(beforeSort);
+
+            Assert.AreEqual(afterSort, result);
+        }
+    }
+}
