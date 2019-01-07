4a2ddbde9e901857acc81e5546d811d78e1c1048
Krzysztof Gebski <krzysztof.gebski@gmail.com>
2019-01-07 18:57:21 +0100

    Added FlightControl project

---
+++ FlightControl/FlightControl/Runway.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightControl
+{
+    class Runway
+    {
+        public bool IsOccupied { get; set; }
+        public static bool CheckIsOccupied { get; internal set; }
+
+        public Runway(bool areOccupied)
+        {
+            IsOccupied = areOccupied;
+        }
+    }
+}
