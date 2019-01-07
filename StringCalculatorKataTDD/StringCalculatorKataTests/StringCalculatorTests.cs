5ee68f7014a8b1379dbd4d73a4730f35936335e1
Krzysztof Gebski <krzysztof.gebski@gmail.com>
2019-01-07 21:44:51 +0100

    simple refactor

--- StringCalculatorKataTDD/StringCalculatorKataTests/StringCalculatorTests.cs
+++ StringCalculatorKataTDD/StringCalculatorKataTests/StringCalculatorTests.cs
@@ -7,6 +7,7 @@ namespace StringCalculatorKataTests
     [TestFixture]
     public class StringCalculatorTests
     {
+
         public StringCalculator sc = new StringCalculator();
 
         [TestCase(0, "")]
