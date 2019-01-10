9c3f9d45df3c3d51e39f0143e410aeee8222eb3b
tetrael <kozlowskiprzemek@gmail.com>
2019-01-10 15:56:12 +0100

    tested Visual Studio git control

--- 20190110/barcos/barcos/Program.cs
+++ 20190110/barcos/barcos/Program.cs
@@ -6,7 +6,7 @@ namespace barcos
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello World!");
         }
     }
 }
