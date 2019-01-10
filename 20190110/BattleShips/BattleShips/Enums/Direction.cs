86f245cb751870fbc8d58bcdf195c14fdc053a7e
Marcin Pochrzest <pochrzest.marcin@gmail.com>
2019-01-10 17:55:48 +0100

    Classes Ship and Player added

---
+++ 20190110/BattleShips/BattleShips/Enums/Direction.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BattleShips.Enums
+{
+    public enum Direction
+    {
+        Left,Right,Up,Down
+    }
+}
--- 20190110/BattleShips/BattleShips/Ship.cs
+++ 20190110/BattleShips/BattleShips/Ship.cs
@@ -7,7 +7,14 @@ namespace BattleShips
 {
     public class Ship
     {
-        public ShipType Type { get; set; }
-        public List<string> OccupiedPositions { get; set; }
+        public Ship(ShipType type, List<string> positions, Player player)
+        {
+            Type = type;
+            OccupiedPositions = positions;
+            player.RegisterShip(this);
+        }
+        public ShipType Type { get; private set; }
+        public List<string> OccupiedPositions { get; private set; }
+        public List<string> DamagedPositions { get; set; }
     }
 }
