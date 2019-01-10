8acc02cc7260608a2be8dd465535d444a28c65ab
Marcin Pochrzest <pochrzest.marcin@gmail.com>
2019-01-10 17:24:40 +0100

    First edition of Ship class

---
+++ 20190110/BattleShips/BattleShips/Ship.cs
@@ -0,0 +1,13 @@
+﻿using BattleShips.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BattleShips
+{
+    public class Ship
+    {
+        public ShipType Type { get; set; }
+        public List<string> OccupiedPositions { get; set; }
+    }
+}
