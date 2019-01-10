15b4d13e171f96e491e939d136bce4d4b33bb67b
Jan Kocon <jankocon7@gmail.com>
2019-01-10 18:47:12 +0100

    Added Player Hit method

--- 20190110/Socallship/Socallship/BoardGame.cs
+++ 20190110/Socallship/Socallship/BoardGame.cs
@@ -4,10 +4,46 @@ using System.Text;
 
 namespace Socallship
 {
-   public class Board
+    public class BoardGame
     {
+        char[,] Board = new char[10, 10];
+        public BoardGame()
+        {
+            InitializeBoard();
+        }
 
-        //char[][] Board = new char[10][10];
-
+        public void ShowBoard()
+        {
+            for (int i = 0; i < Board.GetLength(0); i++)
+            {
+                for (int j = 0; j < Board.GetLength(1); j++)
+                {
+                    Console.Write(Board[i,j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+        private void InitializeBoard()
+        {
+            for(int i=0;i<Board.GetLength(0);i++)
+            {
+                for (int j = 0; j < Board.GetLength(1); j++)
+                {
+                    Board[i, j] = 'O';
+                }
+            }
+        }
+        public bool CheckPositon(int x,int y)
+        {
+            if (Board[x,y] == 'O')
+                return true;
+            else
+                return false;
+        }
+        public void UpdateBoard(int x,int y)
+        {
+            Board[x, y] = 'X';
+        }
+   
     }
 }
--- 20190110/Socallship/Socallship/Program.cs
+++ 20190110/Socallship/Socallship/Program.cs
@@ -6,7 +6,11 @@ namespace Socallship
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Player p1 = new Player();
+            Player p2 = new Player();
+            p1.TryToHit(p2.Board);
+            Console.ReadKey();
+
         }
     }
 }
