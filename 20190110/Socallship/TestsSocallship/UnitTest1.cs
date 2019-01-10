6114d10a15a1315ad992220370b21c1fcf736b26
piotr <piotrrzeznik@gmail.com>
2019-01-10 17:15:13 +0100

    Started project with tests

---
+++ 20190110/Socallship/TestsSocallship/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestsSocallship
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
