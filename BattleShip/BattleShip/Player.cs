60f0af763367ad3110f69ac06d39ab274338fefa
Lukasz Radzik <radziklukasz@wp.pl>
2019-01-10 17:31:24 +0100

    Add first few class

---
+++ BattleShip/BattleShip/Player.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleShip
+{
+    class Player
+    {
+        Board Player_Board;
+        int ID;
+        Board Opponent_Board;
+        List<Ship> ships;
+    }
+}
--- BattleShip/Test/UnitTest1.cs
+++ BattleShip/Test/UnitTest1.cs
@@ -8,7 +8,7 @@ namespace Test
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestMethod1()                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     
         {
         }
     }
