5271edad9db2976f44175c92eabbe188a08a1cfc
Michał C <michalczyrnek93@gmail.com>
2019-01-10 17:54:13 +0100

    CheckCoordinates done, not tested

---
+++ 20190110/BattleshipsWar/BattleshipWarTest/WarTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using BattleshipsWar;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BattleshipWarTest
+{
+    [TestClass]
+    public class WarTest
+    {
+        [TestMethod]
+        public void IsCooridnatesInTable()
+        {
+            string input = "A1";
+            int[,] warmap = new int[2, 2];
+
+
+            War war = new War();
+           bool result=  war.CheckCoordinates(input, warmap);
+
+            Assert.IsTrue(result);
+        }
+    }
+}
---
+++ 20190110/BattleshipsWar/BattleshipsWar/War.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleshipsWar
+{
+    public class War
+    {
+
+        public bool CheckCoordinates (string input, int[,] warmap)
+        {
+            int[] tempcoordinates = new int[2];
+            bool answer = false;
+           InputParser ip = new InputParser();
+           tempcoordinates = ip.ChangeCordsToArrayIndex(input);
+
+            if (tempcoordinates[0] <= warmap.GetLength(0) && tempcoordinates[0] >= 0)
+            {
+
+                if (tempcoordinates[1] <= warmap.GetLength(1) && tempcoordinates[1] >= 0)
+                {
+                    answer = true;
+                }
+            }
+
+            return answer;
+        }
+
+    }
+}
