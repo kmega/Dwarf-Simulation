47838b25ccbb10a0554a7d50c05a5fad96bce8f1
Jan Kocon <jankocon7@gmail.com>
2019-01-10 17:51:59 +0100

    added class Board

---
+++ 20190110/Socallship/Socallship/Board.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Socallship
+{
+   public class Board
+    {
+
+        //char[][] Board = new char[10][10];
+
+    }
+}
