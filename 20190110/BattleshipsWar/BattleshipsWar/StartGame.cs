e4f30d88e39217ce767ec6ed70134f23838052bc
Karol Szelążek <charles.sz@protonmail.com>
2019-01-10 18:14:00 +0100

    Added placeShips function.

--- 20190110/BattleshipsWar/BattleshipsWar/StartGame.cs
+++ 20190110/BattleshipsWar/BattleshipsWar/StartGame.cs
@@ -8,15 +8,26 @@ namespace BattleshipsWar
 {
     class StartGame
     {
-        internal void CreateShip()
+        internal void PlaceShips()
         {
+            object[,] board = MakeBoard();
+
             List<int> ships = new List<int>
             {
                 2, 2, 3, 3, 4, 4, 6
             };
 
+            string userInput = "";
+            while (true)
+            {
+                Console.WriteLine("Choose starting coordinates:");
+                userInput = Console.ReadLine();
+                board = PlaceShipsOnBoard(board, ships);
+            }
+        }
 
-
+        private object[,] MakeBoard()
+        {
             object[,] Board = new object[10, 10];
 
             for (int i = 0; i < Board.GetLength(i); i++)
@@ -26,8 +37,22 @@ namespace BattleshipsWar
                     Board[i, j] = CellProperty.Empty;
                 }
             }
-            
 
+            return Board;
+        }
+
+        private object[,] PlaceShipsOnBoard(object[,] board, List<int> ships)
+        {
+            bool allPlaces = false;
+            while (allPlaces == false)
+            {
+                
+                if (ships.Count == 0)
+                {
+                    allPlaces = true;
+                }
+            }
+            return board;
         }
     }
 }
