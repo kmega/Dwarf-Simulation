5793c5b0b159aa564e68eb2b1bab37c0689d0002
Dawid1992 <dawidlogan@gmail.com>
2019-01-10 18:13:39 +0100

    second push

---
+++ BattleShip/BattleShip/PreperePlayer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleShip
+{
+    public class PreperePlayer
+    {
+
+        public void ShowShips(Player player)
+        {
+            foreach (var ship in player.ships)
+            {
+                Console.WriteLine(ship.Id.ToString() + " " + ship.Lenght);
+            }
+        }
+    }
+}
