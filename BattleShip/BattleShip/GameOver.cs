e950311c31e9c82e930c11ddcc362da85428bdcd
Lukasz Radzik <radziklukasz@wp.pl>
2019-01-11 16:30:25 +0100

    t

---
+++ BattleShip/BattleShip/GameOver.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleShip
+{
+    public static class GameOver
+    {
+       static public bool ShipsAreDestroyed(Board board)
+        {
+            bool result = true;
+
+            foreach (var field in board.Fields)
+            {
+                if (field == Field.F)
+                {
+                    result = false;
+                }
+            }
+            return result;
+        }
+
+    }
+}
