890602bd6f732196a40fc77ba370baf0a181aa76
Matthew-1 <matikm2@gmail.com>
2019-01-11 16:32:56 +0100

    added comment phase 1 in Main and UI as ASCII

--- 20190110/barcos/barcos.tests/UnitTest1.cs
+++ 20190110/barcos/barcos.tests/UnitTest1.cs
@@ -1,6 +1,7 @@
 using NUnit.Framework;
 using barcos;
 using System.Collections.Generic;
+using barcos.Enums;
 
 namespace Tests
 {
@@ -27,23 +28,23 @@ namespace Tests
 
         }
 
-        //[Test]
-        //public void BoardHas100EmptyFields1()
-        //{
-        //    //Given
-        //    Board testedBoard = new Board();
-        //    int boardLength = testedBoard.Fields.Length;
-        //    int boardFieldCounter = 0;
+        [Test]
+        public void BoardHas100EmptyFields1()
+        {
+            //Given
+            Board testedBoard = new Board();
+            int boardLength = testedBoard.Fields.Length;
+            int boardFieldCounter = 0;
 
-        //    Assert.IsTrue(testedBoard.Fields[56] == FieldsStatus.empty);
+            Assert.IsTrue(testedBoard.Fields[56] == FieldsStatus.empty);
 
-        //    while (boardFieldCounter < boardLength)
-        //    {
-        //        Assert.AreEqual(
-        //            testedBoard.Fields[boardFieldCounter], 
-        //            FieldsStatus.empty);
-        //        boardFieldCounter++;
-        //    }
-        //}
+            while (boardFieldCounter < boardLength)
+            {
+                Assert.AreEqual(
+                    testedBoard.Fields[boardFieldCounter],
+                    FieldsStatus.empty);
+                boardFieldCounter++;
+            }
+        }
     }
 }
