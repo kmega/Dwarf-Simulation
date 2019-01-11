e8f7809026ef954acc2cc390a81b286243535b4f
Dawid1992 <dawidlogan@gmail.com>
2019-01-11 15:32:15 +0100

    tttt

--- BattleShip/BattleShip/Program.cs
+++ BattleShip/BattleShip/Program.cs
@@ -10,6 +10,16 @@ namespace BattleShip
     {
         static void Main(string[] args)
         {
+<<<<<<< HEAD
+=======
+            Player player1 = new Player(1);
+          
+            player1.ShowShips(player1);
+           
+            player1.ShowBoard(player1);
+           
+            Console.ReadKey();
+>>>>>>> 10bf8b231fc3d1c744ab560d251f9c361ac1b182
 
         }
     }
