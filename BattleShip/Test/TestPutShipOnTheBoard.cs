f300c2ffa25a949d53d6c31b8582d3ef12e9bfe8
Lukasz Radzik <radziklukasz@wp.pl>
2019-01-11 18:12:13 +0100

    Add test ToPutShipOnTheBoard

--- BattleShip/Test/TestPutShipOnTheBoard.cs
+++ BattleShip/Test/TestPutShipOnTheBoard.cs
@@ -8,7 +8,7 @@ namespace Test
     public class TestPutShipOnTheBoard
     {
         [TestMethod]
-        public void CParserCoordinateMethodShouldReturn1And2WhenA1()
+        public void ParserCoordinateMethodShouldReturn1And2WhenA1()
         {
             //Given
             PutShipOnTheBoard putship = new PutShipOnTheBoard();
@@ -21,7 +21,7 @@ namespace Test
         }
 
         [TestMethod]
-        public void CParserCoordinateMethodShouldReturn2And10WhenA1()
+        public void ParserCoordinateMethodShouldReturn2And10WhenA1()
         {
             //Given
             PutShipOnTheBoard putship = new PutShipOnTheBoard();
@@ -32,8 +32,9 @@ namespace Test
             Assert.AreEqual(result[0], 2);
             Assert.AreEqual(result[1], 10);
         }
+
         [TestMethod]
-        public void CParserCoordinateMethodShouldReturn4And5WhenA1()
+        public void ParserCoordinateMethodShouldReturn4And5WhenA1()
         {
             //Given
             PutShipOnTheBoard putship = new PutShipOnTheBoard();
@@ -44,5 +45,66 @@ namespace Test
             Assert.AreEqual(result[0], 4);
             Assert.AreEqual(result[1], 5);
         }
+
+        [TestMethod]
+        public void ShoudChangeFieldB2ToS()
+        {
+            //Given
+            Player player = new Player(1);
+            PutShipOnTheBoard put = new PutShipOnTheBoard();
+            string coordinates = "A1";
+            bool horizontal = true;
+            int id_ship = 1;
+
+            Assert.AreEqual(player.Player_Board.Fields[0,0], Field.O);
+            //When
+            put.PutShip(player, coordinates, horizontal, id_ship);
+            //Then
+
+            Assert.AreEqual(player.Player_Board.Fields[0, 0], Field.S);
+        }
+
+        [TestMethod]
+        public void ShoudChangeFieldB2AndB3ToS()
+        {
+            //Given
+            Player player = new Player(1);
+            PutShipOnTheBoard put = new PutShipOnTheBoard();
+            string coordinates = "B2";
+            bool horizontal = true;
+            int id_ship = 2;
+
+            Assert.AreEqual(player.Player_Board.Fields[1, 1], Field.O);
+            Assert.AreEqual(player.Player_Board.Fields[2, 1], Field.O);
+            //When
+            put.PutShip(player, coordinates, horizontal, id_ship);
+            //Then
+
+            Assert.AreEqual(player.Player_Board.Fields[1, 1], Field.S);
+            Assert.AreEqual(player.Player_Board.Fields[2, 1], Field.S);
+        }
+
+        [TestMethod]
+        public void ShoudChangeFieldA2AndB2AndC2ToS()
+        {
+            //Given
+            Player player = new Player(1);
+            PutShipOnTheBoard put = new PutShipOnTheBoard();
+            string coordinates = "A2";
+            bool horizontal = false;
+            int id_ship = 3;
+
+            Assert.AreEqual(player.Player_Board.Fields[0, 1], Field.O);
+            Assert.AreEqual(player.Player_Board.Fields[0, 2], Field.O);
+            Assert.AreEqual(player.Player_Board.Fields[0, 3], Field.O);
+            //When
+            put.PutShip(player, coordinates, horizontal, id_ship);
+            //Then
+
+            Assert.AreEqual(player.Player_Board.Fields[0, 1], Field.S);
+            Assert.AreEqual(player.Player_Board.Fields[0, 2], Field.S);
+            Assert.AreEqual(player.Player_Board.Fields[0, 3], Field.S);
+        }
+
     }
 }
