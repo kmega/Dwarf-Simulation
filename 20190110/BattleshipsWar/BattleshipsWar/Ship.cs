6eaab5e90de5f2d9934e851126d62d8d9b487881
--replace-all <luki990@onet.eu>
2019-01-11 15:47:48 +0100

    Start create ship

---
+++ 20190110/BattleshipsWar/BattleshipsWar/Ship.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleshipsWar
+{
+    public class Ship
+    {
+        public List<int> Coords { get; set; }
+
+        public Ship(KindOfShip kindOfShip, int startCoords, Directions direction)
+        {
+            switch (kindOfShip)
+            {
+                case KindOfShip.Two:
+                    {
+                        BuildShip(direction);
+                        break;
+                    }
+                    
+                case KindOfShip.Three:
+                    {
+                        BuildShip(direction);
+                        break;
+                    }
+                case KindOfShip.Four:
+                    {
+                        BuildShip(direction);
+                        break;
+                    }
+                case KindOfShip.Six:
+                    {
+                        BuildShip(direction);
+                        break;
+                    }
+                default:
+                    break;
+            }
+        }
+    }
+}
