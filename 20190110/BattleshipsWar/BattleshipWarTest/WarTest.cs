ecedb01641c9862590e3a153fb0a2457ab030759
Michał C <michalczyrnek93@gmail.com>
2019-01-11 16:20:47 +0100

    ShipCanBeHited

---
+++ 20190110/BattleshipsWar/BattleshipWarTest/WarTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using BattleshipsWar;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BattleshipWarTest
+{
+    [TestClass]
+    public class WarTest
+    {
+        [TestMethod]
+        public void IsShipCanHit()
+        {
+            
+            int[] coordinates = new int[] { 0, 0, };
+            CellProperty[,] warmap = new CellProperty[1, 1] { { CellProperty.Occupied } };
+
+            War war = new War();
+            war.Shoot(coordinates, warmap);
+
+
+            Assert.IsTrue(warmap[0, 0] == CellProperty.Hit);
+        }
+    }
+
+
+}
---
+++ 20190110/BattleshipsWar/BattleshipsWar/War.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleshipsWar
+{
+    public class War
+    {
+
+        public void Shoot(int[] tempcoordinates, CellProperty[,] warmap)
+        {
+            Scanner sc = new Scanner();
+            string result;
+            bool isCoorinatesCorrect = false;
+            CellProperty option;
+
+
+            if (sc.CheckCoordinatesCorrectness(tempcoordinates, warmap))
+            {
+                option = sc.CheckCellStatus(warmap, tempcoordinates);
+                isCoorinatesCorrect = true;
+
+                switch (option)
+                {
+                    case CellProperty.Hit:
+
+                        CellIsHit();
+                        break;
+
+
+                    case CellProperty.Empty:
+
+                         CellISsEmpty();
+                        break;
+
+
+                    case CellProperty.Occupied:
+
+                        CellIsOccupied(warmap, tempcoordinates);
+                        break;
+
+
+                    case CellProperty.Blocked:
+
+                        CellIsBlocked();
+                        break;
+
+                    default:
+                       Console.WriteLine( "Złe koordynaty, spróbuj jeszcze raz") ;
+                        break;
+
+                }
+
+            }
+            else
+            {
+                result = "Złe koordynaty, spróbuj jeszcze raz";
+                Console.WriteLine(result); 
+            }
+        }
+
+
+
+
+        private void CellIsBlocked()
+        {
+            string result = "Złe koordynaty, spróbuj jeszcze raz";
+            Console.WriteLine(result);
+        }
+
+        private void CellIsOccupied(CellProperty[,] warmap, int[] tempcoordinates)
+        {
+            string result;
+            warmap[tempcoordinates[0], tempcoordinates[1]] = CellProperty.Hit;
+            result = "Trafiłeś";
+            Console.WriteLine(result); ;
+        }
+
+        private void CellISsEmpty()
+        {
+            string result = "Nie trafiłeś";
+                Console.WriteLine(result);
+        }
+
+        private void CellIsHit()
+        {
+            string result = "Złe koordynaty, spróbuj jeszcze raz";
+            Console.WriteLine(result);
+
+        }
+    }
+}
