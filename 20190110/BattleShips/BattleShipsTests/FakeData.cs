3d2e3d1b90d5ba610564a4d2d51c4472f385ba57
Krzysztof Gebski <krzysztof.gebski@gmail.com>
2019-01-11 18:25:04 +0100

    Added GameTests

--- 20190110/BattleShips/BattleShipsTests/FakeData.cs
+++ 20190110/BattleShips/BattleShipsTests/FakeData.cs
@@ -1,12 +1,27 @@
 ﻿using BattleShips;
-using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace BattleShipsTests
 {
-    class FakeData
+    internal class FakeData
     {
-        
+        public static List<Player> CreateFakeData()
+        {
+            Player player1 = new Player();
+            Player player2 = new Player();
+            List<Player> players = new List<Player>();
+            player1.FieldWithShips.Add("A1");
+            player1.FieldWithShips.Add("A2");
+            player1.FieldWithShips.Add("A3");
+
+            player2.FieldWithShips.Add("B1");
+            player2.FieldWithShips.Add("B2");
+            player2.FieldWithShips.Add("B3");
+
+            players.Add(player1);
+            players.Add(player2);
+
+            return players;
+        }
     }
 }
