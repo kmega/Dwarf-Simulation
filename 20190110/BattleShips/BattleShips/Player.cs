70bc11970ae2266bf7b5e39736b6e3f098bded6e
Marcin Pochrzest <pochrzest.marcin@gmail.com>
2019-01-11 16:12:14 +0100

    Player ctor added

--- 20190110/BattleShips/BattleShips/Player.cs
+++ 20190110/BattleShips/BattleShips/Player.cs
@@ -4,6 +4,13 @@ namespace BattleShips
 {
     public class Player
     {
+        public Player()
+        {
+            FieldUsedByPlayer = new List<string>();
+            FieldWithDestroyedShips = new List<string>();
+            FieldWithShips = new List<string>();
+        }
+        public string PlayerName { get; set; }
         public List<string> FieldUsedByPlayer { get; set; }
         public List<string> FieldWithDestroyedShips { get; set; }
         public List<string> FieldWithShips { get; set; }
