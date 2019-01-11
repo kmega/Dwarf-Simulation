24584a229a50a4312e7645ab105477fc0b9d67ab
Michał C <michalczyrnek93@gmail.com>
2019-01-11 18:25:23 +0100

    Add MakeTurn

---
+++ 20190110/BattleshipsWar/BattleshipsWar/PlayerTurn.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleshipsWar
+{
+    public class PlayerTurn
+    {
+
+        public void MakeTurn(List<Ship> listofenemyships, CellProperty[,] enemywarmap, string input)
+        {
+            bool breakloop = false;
+            do
+            {
+                Console.WriteLine();
+                Console.WriteLine("Podaj koordynaty");
+                input = Console.ReadLine();
+
+                int[] coordinates = new InputParser().ChangeCordsToIndexes(input);
+                War war = new War();
+               breakloop= war.Shoot(coordinates, enemywarmap, listofenemyships);
+
+
+            } while (breakloop == false);
+
+        }
+    }
+}
