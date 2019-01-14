9fe3d9c90adf4e80a8be1fa722cffc6e25631e46
tetrael <kozlowskiprzemek@gmail.com>
2019-01-14 18:08:02 +0100

    T10 - T13_1 are green

--- 20190114/FoodTrack/FoodTracks/Model/CashRegister.cs
+++ 20190114/FoodTrack/FoodTracks/Model/CashRegister.cs
@@ -4,6 +4,51 @@ namespace FoodTracks.Model
 {
     public class CashRegister
     {
+        private bool isCheapBurger(Burger burger)
+        {
+            if (burger.Meet.Type.Equals(MeetType.None) &&
+                burger.Cheeseness.Equals(Cheeseness.None) &&
+                burger.Addons.Contains(AddonType.None))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private bool isCheeseBurger(Burger burger)
+        {
+            if (!burger.Cheeseness.Equals(Cheeseness.None))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private bool isEnglishBurger(Burger burger)
+        {
+            if (burger.Meet.Type.Equals(MeetType.Full) &&
+                burger.Cheeseness.Equals(Cheeseness.Single) &&
+                burger.Addons.Contains(AddonType.Halapenio) &&
+                burger.Addons.Contains(AddonType.Egg))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private bool isVegeBurger(Burger burger)
+        {
+            if (burger.Meet.Type.Equals(MeetType.None))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
         private readonly IDiscountCalculator _discountCalculator;
 
         public CashRegister(IDiscountCalculator discountCalculator = null)
@@ -13,12 +58,33 @@ namespace FoodTracks.Model
       
         public decimal HowMuch(Burger burger)
         {
+            if (_discountCalculator == null)
+            {
+                return ValueBurger(burger);
+            }
+
             return ValueBurger(burger) + _discountCalculator.Calculate();
         }
 
         private decimal ValueBurger(Burger burger)
         {
+            if (isCheapBurger(burger)) { return 0; }
+            if (isVegeBurger(burger)) { return 5; }
+            if (isEnglishBurger(burger)) { return 25; }
+            if (isCheeseBurger(burger))
+            {
+                switch (burger.Cheeseness)
+                {
+                    case Cheeseness.Single:
+                        return 10;
+                    case Cheeseness.Double:
+                        return 20;
+                }
+            }
+
             return 0;
         }
+
+        public CashRegister() { }
     }
 }
