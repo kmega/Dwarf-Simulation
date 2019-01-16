29067b44f9d4eaea2f16fb9217720d235e8d97eb
Marcin Pochrzest <pochrzest.marcin@gmail.com>
2019-01-16 16:11:30 +0100

    Started working on input validation

---
+++ 20190115/BattleShips/BattleShips/Models/InputValidator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BattleShips.Models
+{
+    public static class InputValidator
+    {
+        public static bool CheckShipType(string message)
+        {
+            return false;
+        }
+
+        public static bool CheckPosition(string message)
+        {
+            return false;
+        }
+        public static bool CheckDirection(string message)
+        {
+            return false;
+        }
+    }
+}
