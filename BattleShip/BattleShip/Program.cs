49aa7bfe1afae67a984ec8d87642196e10e65081
Dawid1992 <dawidlogan@gmail.com>
2019-01-16 15:56:03 +0100

    16012019 working code

--- BattleShip/BattleShip/Program.cs
+++ BattleShip/BattleShip/Program.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.IO;
 
 namespace BattleShip
 {
@@ -11,96 +10,13 @@ namespace BattleShip
     {
         static void Main(string[] args)
         {
+            Player player1 = new Player(1);
+
+            GameOver.DisplayPlayerPutsShipsOnBoard(player1);
+
+          
          
-
-                Game game = new Game();
-                Player player1 = new Player(1);
-                Player player2 = new Player(2);
-                PutShipOnTheBoard putShipOnTheBoard = new PutShipOnTheBoard();
-                int i = 1;
-                string coordinates = "";
-                bool horizontal;
-                string horizontal_string = "";
-                foreach (var ship in player1.ships)
-                {
-                    Console.WriteLine("Gracz 1");
-                    player1.ShowShips(player1);
-                    Console.WriteLine();
-                    player1.ShowBoard(player1.Player_Board);
-                    Console.WriteLine();
-                    Console.WriteLine("Rozmieść " + i + " statek");
-                    Console.WriteLine("Podaj koordynaty np. A1");
-                    coordinates = Console.ReadLine();
-                    Console.WriteLine("Rozmieścić statek poziomo: 1, pionowo: 2");
-                    horizontal_string = Console.ReadLine();
-                    if (horizontal_string == "1")
-                    {
-                        horizontal = true;
-                    }
-                    else { horizontal = false; };
-
-                    putShipOnTheBoard.PutShip(player1, coordinates, horizontal, i);
-                    Console.Clear();
-                    i++;
-
-                }
-                Console.WriteLine("Rozmieszczone statki");
-                player1.ShowBoard(player1.Player_Board);
-                Console.WriteLine("Naciśnij enter");
-                Console.ReadKey();
-                Console.Clear();
-                i = 1;
-
-
-                foreach (var ship in player2.ships)
-                {
-                    Console.WriteLine("Gracz 2");
-                    player1.ShowShips(player2);
-                    Console.WriteLine();
-                    player1.ShowBoard(player2.Player_Board);
-                    Console.WriteLine();
-                    Console.WriteLine("Rozmieść " + i + " statek");
-                    Console.WriteLine("Podaj koordynaty np. A1");
-                    coordinates = Console.ReadLine();
-                    Console.WriteLine("Rozmieścić statek poziomo: 1, pionowo: 2");
-                    horizontal_string = Console.ReadLine();
-                    if (horizontal_string == "1")
-                    {
-                        horizontal = true;
-                    }
-                    else { horizontal = false; };
-
-                    putShipOnTheBoard.PutShip(player2, coordinates, horizontal, i);
-                    Console.Clear();
-                    i++;
-<<<<<<< HEAD
-
-                }
-
-            Console.WriteLine("Rozmieszczone statki");
-            player1.ShowBoard(player2.Player_Board);
-            Console.WriteLine("Naciśnij enter");
             Console.ReadKey();
-            Console.Clear();
-
-=======
-
-                }
-
-            Console.WriteLine("Rozmieszczone statki");
-            player1.ShowBoard(player2.Player_Board);
-            Console.WriteLine("Naciśnij enter");
-            Console.ReadKey();
-            Console.Clear();
-
->>>>>>> 04bf77e93525bbf5fdadb16d4fbf1fd644afd1fd
-            do
-            {
-                string cordinatesShipPlaye = "a1";
-                bool horitontal = true;
-                int id_ship = 1;
-                putShipOnTheBoard.PutShip(player2, cordinatesShipPlaye, horitontal, id_ship);
->>>>>>> 04bf77e93525bbf5fdadb16d4fbf1fd644afd1fd
 
             //bool result;
             //do
@@ -121,15 +37,7 @@ namespace BattleShip
             //    string coordinatePlayer1 = "a1";
             //    string coordinatePlayer2 = "a1";
 
-<<<<<<< HEAD
-<<<<<<< HEAD
             //    game.TryHitShip(player1, player2, coordinatePlayer1, coordinatePlayer2);
-=======
-=======
->>>>>>> 04bf77e93525bbf5fdadb16d4fbf1fd644afd1fd
-                
-                
->>>>>>> 04bf77e93525bbf5fdadb16d4fbf1fd644afd1fd
 
             //    result = GameOver.ShipsAreDestroyed(player1.Opponent_Board,1);
 
---
+++ BattleShip/Test/DawidTests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BattleShip;
+
+namespace Test
+{
+    [TestClass]
+    public class DawidTests
+    {
+        [TestMethod]
+        public void ShouldReturnFalseWhenBoardHaveShips()
+        {
+            //Given
+            Board board = new Board();
+
+            //When
+            board.Fields[0, 1] = Field.F;
+            board.Fields[0, 2] = Field.F;
+            board.Fields[0, 3] = Field.F;
+
+            board.Fields[4, 5] = Field.F;
+            board.Fields[4, 6] = Field.F;
+            board.Fields[4, 7] = Field.F;
+
+            bool result = GameOver.ShipsAreDestroyed(board);
+
+            //Then
+            Assert.AreEqual(false, result);
+
+        }
+
+        [TestMethod]
+        public void ShouldReturnTrueWhenAllShipsOnBoardAreDestroyed()
+        {
+            //Given
+            Board board = new Board();
+
+            //When
+            board.Fields[4, 5] = Field.F;
+            board.Fields[4, 6] = Field.F;
+            board.Fields[4, 7] = Field.F;
+
+            board.Fields[4, 5] = Field.H;
+            board.Fields[4, 6] = Field.H;
+            board.Fields[4, 7] = Field.H;
+
+            bool result = GameOver.ShipsAreDestroyed(board);
+
+            //Then
+            Assert.AreEqual(true, result);
+
+
+        }
+
+
+    }
+}
