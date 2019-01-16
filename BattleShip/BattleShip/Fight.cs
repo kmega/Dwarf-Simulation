3dcb68e9820f5aacd75a9143a7719aca37625922
Lukasz Radzik <radziklukasz@wp.pl>
2019-01-16 16:35:52 +0100

    Repaired class Fight

--- BattleShip/BattleShip/Fight.cs
+++ BattleShip/BattleShip/Fight.cs
@@ -14,9 +14,9 @@ namespace BattleShip
         {
             //Player 1 try hit player 2 ships
             player1TryHitShip = player1TryHitShip.ToLower();
-            int lenghtBoard = player1TryHitShip[0] % 97;
-            int heightBoard = (player1TryHitShip[1] - 48) - 1;
-
+            int heightBoard = player1TryHitShip[0] % 97;
+            int lenghtBoard = (player1TryHitShip[1] - 48) - 1;
+            
             if (player2.Player_Board.Fields[lenghtBoard,heightBoard] == Field.O)
             {
                 player1.Opponent_Board.Fields[lenghtBoard, heightBoard] = Field.F;
