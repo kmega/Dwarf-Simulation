735f52663a4a9bf2a5cafbb9d22a77f1077862a9
Marcin Pochrzest <pochrzest.marcin@gmail.com>
2019-01-16 16:30:50 +0100

    Validation of shipType works, next step direction

--- 20190115/BattleShips/BattleShips/Models/InputValidator.cs
+++ 20190115/BattleShips/BattleShips/Models/InputValidator.cs
@@ -8,16 +8,39 @@ namespace BattleShips.Models
     {
         public static bool CheckShipType(string message)
         {
-            return false;
+            string validationInfo = message.ToLower();
+            switch(validationInfo)
+            {
+                case "c":
+                case "d":
+                case "b":
+                case "s":
+                case "p":
+                    return true;
+                default:
+                    throw new Exception("Incorrect ship type inserted. Please try again!");
+            }
         }
 
         public static bool CheckPosition(string message)
         {
+            string validationInfo = message.ToUpper();
+            
             return false;
         }
         public static bool CheckDirection(string message)
         {
-            return false;
+            string validationInfo = message.ToLower();
+            switch(validationInfo)
+            {
+                case "u":
+                case "r":
+                case "l":
+                case "d":
+                    return true;
+                default:
+                    throw new Exception("Incorrect direction inserted, please try again!");
+            }
         }
     }
 }
---
+++ 20190115/BattleShips/BattleShipsTests/InputValidationTests.cs
@@ -0,0 +1,32 @@
+﻿using BattleShips.Models;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BattleShipsTests
+{
+    public class InputValidationTests
+    {
+        [Test]
+        [TestCase("C")]
+        [TestCase("c")]
+        [TestCase("d")]
+        [TestCase("D")]
+        [TestCase("c")]
+        public void T01_ShouldReturnTrueWhenCorrectInputForShipType(string input)
+        {
+            //Then
+            Assert.IsTrue(InputValidator.CheckShipType(input));
+        }
+        [Test]
+        [TestCase("Cd")]
+        public void T02_ShouldThrowExceptionWhenIncorrectInputForShipType(string input)
+        {
+            //Then
+            string message = Assert.Throws<Exception>(
+                () => InputValidator.CheckShipType(input)).Message;
+            Assert.AreEqual("Incorrect ship type inserted. Please try again!", message);
+        }
+    }
+}
