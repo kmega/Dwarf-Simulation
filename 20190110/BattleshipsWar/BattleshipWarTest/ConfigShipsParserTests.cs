afcb00ddbaabaebea7fb8fe72eff5e1072d06335
--replace-all <luki990@onet.eu>
2019-01-21 17:14:00 +0100

    Test to ConfigParser

---
+++ 20190110/BattleshipsWar/BattleshipWarTest/ConfigShipsParserTests.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.IO;
+using BattleshipsWar;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace BattleshipWarTest
+{
+    [TestClass]
+    class ConfigShipsParserTests
+    {
+        [TestMethod]
+        public void ListShipsShouldBeEmptyWhenStringIsEmpty()
+        {
+            //Given
+            string text = "";
+            
+
+            //When
+            List<KindOfShip> result = ShipInputParser.costam(text);
+
+            //Then
+            Assert.AreEqual(result.Count, 0);
+
+        }
+        [TestMethod]
+        public void ShouldReturnTwoShipsKindOfOne()
+        {
+            //Given
+            string text = "1:2";
+
+            //When
+            List<KindOfShip> result = ShipInputParser.costam(text, listastatkow);
+
+            //Then
+            Assert.AreEqual(result[0], KindOfShip.One);
+            Assert.AreEqual(result[1], KindOfShip.One);
+            Assert.AreEqual(result.Count, 2);
+
+        }
+        [TestMethod]
+        public void LastShipShouldBeKindFive()
+        {
+            //Given
+            string text = "1:2\n3:1\n5:2";
+
+            //When
+            List<KindOfShip> result = ShipInputParser.costam(text, listastatkow);
+
+            //Then
+            Assert.AreEqual(result[result.Count-1], KindOfShip.Five);
+
+        }
+        [TestMethod]
+        public void ShipsShouldContainsThreeShipsKindOf4()
+        {
+            //Given
+            string text = "1:2\n3:1\n5:2\n4:3";
+
+            //When
+            List<KindOfShip> result = ShipInputParser.costam(text, listastatkow);
+
+            var ShipsKindOfFour = result.Where(x => x == KindOfShip.Four).ToList();
+
+            //Then
+            Assert.AreEqual(ShipsKindOfFour.Count, 3);
+
+        }
+
+    }
+        
+}
