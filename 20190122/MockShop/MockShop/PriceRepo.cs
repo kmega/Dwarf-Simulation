ee4018c27b98ee2d7721d64179015c3864999d48
Marcin Pochrzest <pochrzest.marcin@gmail.com>
2019-01-22 18:54:22 +0100

    Simulation of PriceRepo added

---
+++ 20190122/MockShop/MockShop/PriceRepo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MockShop
+{
+    public class PriceRepo : IPriceRepo
+    {
+        public double GetProductPrice(ProductType productType)
+        {
+            if(productType == ProductType.Clothes)
+            {
+                return 10.0;
+            }
+            if(productType == ProductType.TurtleOrthopedicShoes)
+            {
+                return 15.0;
+            }
+            return 0;
+        }
+    }
+}
