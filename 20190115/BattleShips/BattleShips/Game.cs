ca0f5bf28a441691c36971dfbb36efe256509317
Krzysztof Gebski <krzysztof.gebski@gmail.com>
2019-01-22 18:37:04 +0100

    refactor

--- 20190115/BattleShips/BattleShips/Game.cs
+++ 20190115/BattleShips/BattleShips/Game.cs
@@ -9,6 +9,35 @@ namespace BattleShips
     {
         public List<Player> Players { get; set; }
 
+        public static void ChangeActivePlayer(List<Player> players)
+        {
+            var index = players.FindIndex(p => p.IsActive == true);
+            players[index].IsActive = false;
+            if (index < players.Count() - 1)
+            {
+                players[index + 1].IsActive = true;
+            }
+            else
+            {
+                players[0].IsActive = true;
+            }
+        }
+
+        public static bool ShipIsHit(string field, List<Player> players)
+        {
+            bool result = false;
+            foreach (var player in players)
+            {
+                result = CheckFieldOnOccupiedList(field, player.Ships);
+            }
+            return result;
+        }
+
+        public static bool WhetherInactivePlayersHasShips(List<Player> players)
+        {
+            return players.Where(p => p.IsActive == false).Select(p => p.Ships).Select(p => p.Count > 0).Any();
+        }
+
         public void StartGame()
         {
             UI.GameRules();
@@ -18,7 +47,7 @@ namespace BattleShips
             while (WhetherInactivePlayersHasShips(Players))
             {
                 var field = UI.InputFieldToAttack();
-                
+
                 Turn(Players, field);
                 ShowPlayersBoardsWithChoosenFields(Players);
             }
@@ -26,13 +55,24 @@ namespace BattleShips
             Console.WriteLine($"The winner is: " + activePlayer.IsActive);
         }
 
-        private void ShowPlayersBoardsWithChoosenFields(List<Player> players)
+        private static bool CheckFieldOnOccupiedList(string field, List<IShip> ships)
         {
-            foreach (var player in players)
+            foreach (var ship in ships)
             {
-                CreatePlayerBoardWithChoosenField(player);
-                Console.WriteLine("");
+                foreach (var position in ship.OccupiedPositions)
+                {
+                    if (position.Equals(field))
+                    {
+                        return true;
+                    }
+                }
             }
+            return false;
+        }
+
+        private List<Player> CreateInactivePlayersList(List<Player> players)
+        {
+            return players.Where(p => p.IsActive == false).ToList();
         }
 
         private void CreatePlayerBoardWithChoosenField(Player player)
@@ -44,14 +84,53 @@ namespace BattleShips
             ShowBoard(board);
         }
 
+        private void MoveFieldFromOneListToAnother(string field, List<string> sourceList, List<string> destinationList)
+        {
+            sourceList.Remove(field);
+            destinationList.Add(field);
+        }
+
+        private void SetActivePlayer(List<Player> players)
+        {
+            players.First(p => p.IsActive = true);
+        }
+
+        private void ShowPlayersBoardsWithChoosenFields(List<Player> players)
+        {
+            foreach (var player in players)
+            {
+                CreatePlayerBoardWithChoosenField(player);
+                Console.WriteLine("");
+            }
+        }
         #region CreateBoard
 
+        public static void CreateEmptyBoard(string[,] board)
+        {
+            var symbol = "- ";
+            for (int i = 0; i < board.GetLength(0); i++)
+            {
+                for (int j = 0; j < board.GetLength(1); j++)
+                {
+                    board[i, j] = symbol;
+                }
+            }
+        }
 
         public static void ShowPlayersBoards(List<Player> players)
         {
             foreach (var player in players)
             {
                 CreatePlayerBoard(player);
+                Console.WriteLine("");
+            }
+        }
+
+        public static void ShowPlayersBoardsWithShips(List<Player> players)
+        {
+            foreach (var player in players)
+            {
+                CreatePlayerBoardWithShips(player);
                 Console.WriteLine("");
             }
         }
@@ -65,17 +144,12 @@ namespace BattleShips
             ShowBoard(board);
         }
 
-        private static void FillArrayPlayerShips(Player player, string[,] board)
+        private static void CreatePlayerBoardWithShips(Player player)
         {
-            foreach (var ship in player.Ships)
-            {
-                foreach (var field in ship.OccupiedPositions)
-                {
-                    int x, y;
-                    ParseFieldToInt(field, out x, out y);
-                    board[y, x] = "X ";
-                }
-            }
+            string[,] board = new string[10, 10];
+            CreateEmptyBoard(board);
+            FillArrayPlayerShips(player, board);
+            ShowBoard(board);
         }
 
         private static void FillArrayPlayerChoosenFields(Player player, string[,] board)
@@ -102,23 +176,18 @@ namespace BattleShips
             }
         }
 
-        public static void ShowPlayersBoardsWithShips(List<Player> players)
+        private static void FillArrayPlayerShips(Player player, string[,] board)
         {
-            foreach (var player in players)
+            foreach (var ship in player.Ships)
             {
-                CreatePlayerBoardWithShips(player);
-                Console.WriteLine("");
+                foreach (var field in ship.OccupiedPositions)
+                {
+                    int x, y;
+                    ParseFieldToInt(field, out x, out y);
+                    board[y, x] = "X ";
+                }
             }
         }
-
-        private static void CreatePlayerBoardWithShips(Player player)
-        {
-            string[,] board = new string[10, 10];
-            CreateEmptyBoard(board);
-            FillArrayPlayerShips(player, board);
-            ShowBoard(board);
-        }
-
         private static void ParseFieldToInt(string field, out int x, out int y)
         {
             x = field[0] - 65;
@@ -144,30 +213,13 @@ namespace BattleShips
                 Console.WriteLine("");
             }
         }
-
-        public static void CreateEmptyBoard(string[,] board)
-        {
-            var symbol = "- ";
-            for (int i = 0; i < board.GetLength(0); i++)
-            {
-                for (int j = 0; j < board.GetLength(1); j++)
-                {
-                    board[i, j] = symbol;
-                }
-            }
-        }
-        #endregion
-
-        private List<Player> CreateInactivePlayersList(List<Player> players)
-        {
-            return players.Where(p => p.IsActive == false).ToList();
-        }
-
+        #endregion CreateBoard
         private void Turn(List<Player> players, string field)
         {
             var inactivePlayers = CreateInactivePlayersList(players);
             if (ShipIsHit(field, inactivePlayers))
             {
+                Console.WriteLine("Ship has been hit");
                 foreach (var player in inactivePlayers)
                 {
                     foreach (var ship in player.Ships)
@@ -176,7 +228,7 @@ namespace BattleShips
                         {
                             MoveFieldFromOneListToAnother(field, ship.OccupiedPositions, ship.DamagedPositions);
                         }
-                        
+
                         if (ship.OccupiedPositions.Count() == 0)
                         {
                             Console.WriteLine($"The ship {ship.Type} has been destroyed");
@@ -188,61 +240,6 @@ namespace BattleShips
             {
                 ChangeActivePlayer(players);
             }
-        }
-
-        private void MoveFieldFromOneListToAnother(string field, List<string> sourceList, List<string> destinationList)
-        {
-            sourceList.Remove(field);
-            destinationList.Add(field);
-        }
-
-        public static bool ShipIsHit(string field, List<Player> players)
-        {
-            bool result = false;
-            foreach (var player in players)
-            {
-                result = CheckFieldOnOccupiedList(field, player.Ships);
-            }
-            return result;
-        }
-
-        private static bool CheckFieldOnOccupiedList(string field, List<IShip> ships)
-        {
-            foreach (var ship in ships)
-            {
-                foreach (var position in ship.OccupiedPositions)
-                {
-                    if (position.Equals(field))
-                    {
-                        return true;
-                    }
-                }
-            }
-            return false;
-        }
-
-        public static void ChangeActivePlayer(List<Player> players)
-        {
-            var index = players.FindIndex(p => p.IsActive == true);
-            players[index].IsActive = false;
-            if (index < players.Count() - 1)
-            {
-                players[index + 1].IsActive = true;
-            }
-            else
-            {
-                players[0].IsActive = true;
-            }
-        }
-
-        private void SetActivePlayer(List<Player> players)
-        {
-            players.First(p => p.IsActive = true);
-        }
-
-        public static bool WhetherInactivePlayersHasShips(List<Player> players)
-        {
-            return players.Where(p => p.IsActive == false).Select(p => p.Ships).Select(p => p.Count > 0).Any();
         }
     }
 }
