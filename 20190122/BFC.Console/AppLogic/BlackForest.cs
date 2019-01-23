fedc85f1546127527792499e839d939c1defe7f8
Jan Kocon <jankocon7@gmail.com>
2019-01-23 18:39:32 +0100

    Added project

--- 20190122/BFC.Console/AppLogic/BlackForest.cs
+++ 20190122/BFC.Console/AppLogic/BlackForest.cs
@@ -1,6 +1,7 @@
 ﻿using BFC.Console.Animals;
 using BFC.Console.Heroes;
 using BFC.Console.Presentation;
+using System.Collections.Generic;
 
 namespace BFC.Console.AppLogic
 {
@@ -8,6 +9,8 @@ namespace BFC.Console.AppLogic
     {       
         private IOutputWritter _presenter;
         private Person _person;
+        private List<Animal> branch = new List<Animal>();
+        TimeOfDay timeOfDay;
 
         public BlackForest(IOutputWritter presenter)
         {
@@ -16,27 +19,70 @@ namespace BFC.Console.AppLogic
 
         public void SetTimeOfDay(TimeOfDay timeOfDay)
         {
-            throw new System.NotImplementedException();
+            this.timeOfDay = timeOfDay;
+            if(timeOfDay == TimeOfDay.Fire)
+            {
+                if(_person is Romantic)
+                {
+                    if (CheckIfBranchContainsAnimalType(AnimalTypes.Child))
+                        _presenter.WriteLine("Child will be rescued by Romantic.");
+                }
+                ActivateFireman();
+            }
+        }
+        public bool CheckIfBranchContainsAnimalType(AnimalTypes type)
+        {
+            foreach(Animal animal in branch)
+            {
+                if (animal.AnimalType == type) return true;
+            }
+            return false;
         }
 
         public void SitOnTheTree(AnimalTypes animalType)
         {
-            throw new System.NotImplementedException();
+            bool isPossibleToSit = true;
+            if (timeOfDay == TimeOfDay.Fire)
+                isPossibleToSit = false;
+            if (animalType == AnimalTypes.Cat && timeOfDay != TimeOfDay.Night)
+                isPossibleToSit = false;
+            else if (animalType == AnimalTypes.Bird && timeOfDay != TimeOfDay.Day)
+                isPossibleToSit = false;
+            branch.Add(new Animal(animalType.ToString(), animalType, true));
+            string information = isPossibleToSit ? animalType.ToString() + " sit on the Tree." : animalType.ToString() + " doesn't sit on the Tree.";
+            _presenter.WriteLine(information);
         }
 
         public void SitOnTheTree(AnimalTypes[] animalType)
         {
-            throw new System.NotImplementedException();
+            if (timeOfDay != TimeOfDay.Fire)
+            {
+                foreach (AnimalTypes animal in animalType)
+                    SitOnTheTree(animal);
+            }
+            else
+            { 
+                _presenter.WriteLine("Bird, Cat, Child doesn't sit on the Tree");
+                _presenter.WriteLine("Nobody will be rescued by Romantic.");
+            }
         }
 
         public void ActivateFireman()
         {
             _person = new Fireman();
+            if (CheckIfBranchContainsAnimalType(AnimalTypes.Bird))
+                _presenter.WriteLine("Fireman generated alarm sound.");
+            if (branch.Count == 1 && CheckIfBranchContainsAnimalType(AnimalTypes.Bird))
+                _presenter.WriteLine("Nobody will be rescued by Fireman.");
+            if (CheckIfBranchContainsAnimalType(AnimalTypes.Child) && CheckIfBranchContainsAnimalType(AnimalTypes.Cat))
+                _presenter.WriteLine("Child, Cat will be rescued by Fireman.");
+            _person.RescuAnimals(branch);
+          
         }
 
         public void ActivateRomantic()
         {
-            _person = new Romantic();
+            _person = new Romantic();            
         }
     }
 }
