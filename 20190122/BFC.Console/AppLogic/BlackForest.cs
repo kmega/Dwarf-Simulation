55cea2395c109f083ca251deeeb58d71bf908b20
tetrael <kozlowskiprzemek@gmail.com>
2019-01-23 17:54:36 +0100

    Romantic tests are passing

--- 20190122/BFC.Console/AppLogic/BlackForest.cs
+++ 20190122/BFC.Console/AppLogic/BlackForest.cs
@@ -1,6 +1,9 @@
 ﻿using BFC.Console.Animals;
 using BFC.Console.Heroes;
 using BFC.Console.Presentation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace BFC.Console.AppLogic
 {
@@ -9,6 +12,7 @@ namespace BFC.Console.AppLogic
         private IOutputWritter _presenter;
         private Person _person;
         private TimeOfDay _timeOfDay;
+        private List<Animal> _animals = new List<Animal>();
 
         public BlackForest(IOutputWritter presenter)
         {
@@ -17,13 +21,32 @@ namespace BFC.Console.AppLogic
 
         public void SetTimeOfDay(TimeOfDay timeOfDay)
         {
-            //throw new System.NotImplementedException();
             _timeOfDay = timeOfDay;
+
+            switch (_timeOfDay)
+            {
+                case TimeOfDay.Fire:
+                    ActivateFireman();
+                    _person.RescuAnimals(_animals);
+                    if (new Random().Next(0, 2) == 1)
+                    {
+                        ActivateRomantic();
+                        _person.RescuAnimals(_animals);
+                        _presenter.WriteLine("Child will be rescued by Romantic.");
+                    }
+                    break;
+                case TimeOfDay.Day:
+                    break;
+                case TimeOfDay.Night:
+                    break;
+            }
         }
 
         public void SitOnTheTree(AnimalTypes animalType)
         {
-            //throw new System.NotImplementedException();
+            List<AnimalTypes> animals = new List<AnimalTypes>();
+            _animals.Add(new Animal(animalType.ToString(), animalType));
+
             switch (_timeOfDay)
             {
                 case TimeOfDay.Day:
@@ -73,11 +96,25 @@ namespace BFC.Console.AppLogic
 
         public void SitOnTheTree(AnimalTypes[] animalType)
         {
-            //throw new System.NotImplementedException();
-            // "Bird, Cat, Child doesn't sit on the Tree"
+            List<AnimalTypes> animals = animalType.ToList();
+
+            foreach (var animal in animals)
+            {
+                _animals.Add(new Animal(animal.ToString(), animal));
+            }
+
             switch (_timeOfDay)
             {
                 case TimeOfDay.Fire:
+                    animals.ForEach(animal =>
+                    {
+                        switch (animal)
+                        {
+                            case AnimalTypes.Child:
+                                _presenter.WriteLine("Child will be rescued by Romantic.");
+                                break;
+                        }
+                    });
                     _presenter.WriteLine("Bird, Cat, Child doesn't sit on the Tree");
                     break;
                 case TimeOfDay.Day:
