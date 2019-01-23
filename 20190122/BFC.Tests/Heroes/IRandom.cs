c1f066899106be3f1831383c182726858958bd04
Jan Kocon <jankocon7@gmail.com>
2019-01-23 18:55:41 +0100

    started additional task

---
+++ 20190122/BFC.Tests/Heroes/IRandom.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BFC.Tests.Heroes
+{
+    public interface IRandom
+    {
+        int Rand();
+    }
+}
---
+++ 20190122/BFC.Tests/Scenarios/Fireman and Romantic love story/WhenRomanticAndFiremanCameIntoForrest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BFC.Console.Animals;
+using BFC.Console.AppLogic;
+using BFC.Console.Presentation;
+using Moq;
+using NUnit.Framework;
+
+namespace BFC.Tests.Scenarios.Fireman_and_Romantic_love_story
+{
+    public class WhenRomanticAndFiremanCameIntoForrest
+    {
+        private BlackForest _blackForest;
+
+        private Mock<IOutputWritter> _presenter;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _presenter = new Mock<IOutputWritter>();
+            _blackForest = new BlackForest(_presenter.Object);
+        }
+         
+    }
+}
