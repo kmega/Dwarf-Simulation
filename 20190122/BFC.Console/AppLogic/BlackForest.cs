b3cfe29498372f06218087e6f4ef1e28364d296e
tetrael <kozlowskiprzemek@gmail.com>
2019-01-23 15:54:25 +0100

    Test with animals in the forest are green

--- 20190122/BFC.Console/AppLogic/BlackForest.cs
+++ 20190122/BFC.Console/AppLogic/BlackForest.cs
@@ -8,6 +8,7 @@ namespace BFC.Console.AppLogic
     {       
         private IOutputWritter _presenter;
         private Person _person;
+        private TimeOfDay _timeOfDay;
 
         public BlackForest(IOutputWritter presenter)
         {
@@ -16,12 +17,58 @@ namespace BFC.Console.AppLogic
 
         public void SetTimeOfDay(TimeOfDay timeOfDay)
         {
-            throw new System.NotImplementedException();
+            //throw new System.NotImplementedException();
+            _timeOfDay = timeOfDay;
         }
 
         public void SitOnTheTree(AnimalTypes animalType)
         {
-            throw new System.NotImplementedException();
+            //throw new System.NotImplementedException();
+            switch (_timeOfDay)
+            {
+                case TimeOfDay.Day:
+                    switch (animalType)
+                    {
+                        case AnimalTypes.Bird:
+                            _presenter.WriteLine("Bird sit on the Tree.");
+                            break;
+                        case AnimalTypes.Cat:
+                            _presenter.WriteLine("Cat doesn't sit on the Tree.");
+                            break;
+                        case AnimalTypes.Child:
+                            _presenter.WriteLine("Child sit on the Tree.");
+                            break;
+                    }
+                    break;
+                case TimeOfDay.Night:
+                    switch (animalType)
+                    {
+                        case AnimalTypes.Bird:
+                            _presenter.WriteLine("Bird doesn't sit on the Tree.");
+                            break;
+                        case AnimalTypes.Cat:
+                            _presenter.WriteLine("Cat sit on the Tree.");
+                            break;
+                        case AnimalTypes.Child:
+                            _presenter.WriteLine("Child sit on the Tree.");
+                            break;
+                    }
+                    break;
+                case TimeOfDay.Fire:
+                    switch (animalType)
+                    {
+                        case AnimalTypes.Bird:
+                            _presenter.WriteLine("Bird doesn't sit on the Tree.");
+                            break;
+                        case AnimalTypes.Cat:
+                            _presenter.WriteLine("Cat doesn't sit on the Tree.");
+                            break;
+                        case AnimalTypes.Child:
+                            _presenter.WriteLine("Child doesn't sit on the Tree.");
+                            break;
+                    }
+                    break;
+            }
         }
 
         public void SitOnTheTree(AnimalTypes[] animalType)
