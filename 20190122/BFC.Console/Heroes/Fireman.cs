9ac0c99b5796482a7278dafb1812175ce78633f2
tetrael <kozlowskiprzemek@gmail.com>
2019-01-23 16:52:40 +0100

    Fireman tests are green

--- 20190122/BFC.Console/Heroes/Fireman.cs
+++ 20190122/BFC.Console/Heroes/Fireman.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using BFC.Console.Animals;
+using System.Linq;
 
 namespace BFC.Console.Heroes
 {
@@ -9,7 +10,20 @@ namespace BFC.Console.Heroes
     {
         public void RescuAnimals(IList<Animal> branch)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            List<Animal> rescuBranch = new List<Animal>();
+            foreach (var animal in branch)
+            {
+                rescuBranch.Add(animal);
+            }
+
+            rescuBranch.ForEach(animal =>
+            {
+                if (animal.AnimalType != AnimalTypes.Bird)
+                {
+                    branch.Remove(animal);
+                }
+            });
         }
     }
 }
