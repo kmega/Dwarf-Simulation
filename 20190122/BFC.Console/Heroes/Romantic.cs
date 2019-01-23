7dcd8914703edccc6e18ac91e420c04e78310680
tetrael <kozlowskiprzemek@gmail.com>
2019-01-23 16:56:24 +0100

    Romantic tests are green

--- 20190122/BFC.Console/Heroes/Romantic.cs
+++ 20190122/BFC.Console/Heroes/Romantic.cs
@@ -8,7 +8,21 @@ namespace BFC.Console.Heroes
     {
         public void RescuAnimals(IList<Animal> branch)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+
+            List<Animal> rescuBranch = new List<Animal>();
+            foreach (var animal in branch)
+            {
+                rescuBranch.Add(animal);
+            }
+
+            rescuBranch.ForEach(animal =>
+            {
+                if (animal.AnimalType.Equals(AnimalTypes.Child))
+                {
+                    branch.Remove(animal);
+                }
+            });
         }
     }
 }
