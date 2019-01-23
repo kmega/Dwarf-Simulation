3158ffe77ae5b837aeedc79b9bfa519583013d13
Karol Szelążek <charles.sz@protonmail.com>
2019-01-23 15:06:30 +0100

    Initial commit.

---
+++ 23012019/BFC.Console/Heroes/Romantic.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using BFC.Console.Animals;
+
+namespace BFC.Console.Heroes
+{
+    public class Romantic : Person
+    {
+        public void RescuAnimals(IList<Animal> branch)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ 23012019/BFC.Tests/Heroes/FiremanTests.cs
@@ -0,0 +1,53 @@
+﻿using BFC.Console.Animals;
+using BFC.Console.Heroes;
+using NUnit.Framework;
+
+namespace BFC.Tests.Heroes
+{
+    [TestFixture]
+    public class FiremanTests
+    {
+        private readonly Fireman _fireman = new Fireman();
+        [Test]
+        public void ShouldBeAbleToRescueAnimalFromBranchWhenTimeOfDayIsFireAndAnimalTypeIsChild()
+        {
+            // given 
+            var branch = BranchHelper.PutAnimalOnBranch(AnimalTypes.Child);
+
+            // when 
+            _fireman.RescuAnimals(branch);
+
+            // then
+            const int expected = 0;
+            Assert.That(branch.Count, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void ShouldBeAbleToRescueAnimalFromBranchWhenTimeOfDayIsFireAndAnimalTypeIsCat()
+        {
+            // given 
+            var branch = BranchHelper.PutAnimalOnBranch(AnimalTypes.Cat);
+
+            // when 
+            _fireman.RescuAnimals(branch);
+
+            // then
+            const int expected = 0;
+            Assert.That(branch.Count, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void ShouldNotBeAbleToRescueAnimalFromBranchWhenTimeOfDayIsFireAndAnimalTypeIsBird()
+        {
+            // given 
+            var branch = BranchHelper.PutAnimalOnBranch(AnimalTypes.Bird);
+
+            // when 
+            _fireman.RescuAnimals(branch);
+
+            // then
+            const int expected = 1;
+            Assert.That(branch.Count, Is.EqualTo(expected));
+        }
+    }
+}
