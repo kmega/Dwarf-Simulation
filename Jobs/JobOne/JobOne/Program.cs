e2c90cd69f8853c1a6a4ebfe12b2f1aa9b109a84
Dariusz <opcjonalni@gmail.com>
2019-01-24 19:39:20 +0100

    Introductory tasks (first and second)

---
+++ Jobs/JobOne/JobOne/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace JobOne
+{
+    class Reverse
+    {
+        private static void Main()
+        {
+            // Read each line of the file into a string array. Each element
+            // of the array is one line of the file.
+
+            string[] data = System.IO.File.ReadAllLines(@"./tea-data.txt"); // the txt file must be in the same directory as the executable file ( EXE ) 
+         
+            // Display the file contents by using a foreach loop.
+            System.Console.WriteLine("Contents of tea-data.txt after loading  = ");
+           foreach (string line in data)
+             {
+                Console.WriteLine(line);  // writing line by line in console 
+             }
+            string[]  detaReverse = data;
+            Array.Reverse(detaReverse); //Reverse of the arra
+
+            System.Console.WriteLine("\n\t" + "Contents of tea-data.txt after before ( result-2.txt )  = ");
+            foreach (string line in detaReverse)
+            {
+                Console.WriteLine("\t" + line);
+            }
+            System.IO.File.WriteAllLines(@"./result-2.txt", data); // writing line by line in console 
+
+            // Keep the console window open in debug mode.
+            Console.WriteLine("\n" + "Press any key to exit.");
+            System.Console.ReadKey();
+        }
+    }
+}
---
+++ Jobs/JobTwo/JobTwo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+namespace JobOne
+{
+    class Reverse
+    {
+        private static void Main()
+        {
+            int rows = 8;
+            int columns = 5;
+            // Read each line of the file into a string array. Each element
+            // of the array is one line of the file.
+            //string[] dniTygodnia = { "poniedziałek", "wtorek", "śr", "cz", "pi", "so", "ni" };
+            string[] plik = System.IO.File.ReadAllLines(@"./tea-data.txt"); // the txt file must be in the same directory as the executable file ( EXE ) 
+            int[,] matrix = new int[rows, columns];                                                                //string test = "TEST test TEST ";
+                                                                            // Display the file contents by using a foreach loop.
+                                                                            //System.Console.WriteLine("TEst " + data[8,2]);
+
+            for (int i = 0; i < plik.Length; i++)
+            {
+                //bierzemy pierwsza linie
+                //robimy tymczasowa tablice - dzielimy string
+                string[] tmp = plik[i].Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+                for (int j = 0; j < tmp.Length; j++)
+                {
+                    //zapelniamy macierz wartosciami
+                    matrix[i, j] = int.Parse(tmp[j]);
+                }
+            }
+
+            Console.WriteLine("\n" + "Press any key to exit.");
+            System.Console.ReadKey();
+        }
+    }
+}
