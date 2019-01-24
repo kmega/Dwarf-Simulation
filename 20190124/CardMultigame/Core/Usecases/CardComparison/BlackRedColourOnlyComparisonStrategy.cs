43367b20627dfa269b5de64579f40a4cff9106ae
Michał Czyrnek <e.s.michael.c@gmail.cok>
2019-01-24 23:13:37 +0100

    Task 113 done

--- 20190124/CardMultigame/Core/Usecases/CardComparison/BlackRedColourOnlyComparisonStrategy.cs
+++ 20190124/CardMultigame/Core/Usecases/CardComparison/BlackRedColourOnlyComparisonStrategy.cs
@@ -9,9 +9,38 @@ namespace Core.Usecases.CardComparison
 {
     public class BlackRedColourOnlyComparisonStrategy : ICardComparisonStrategy
     {
+
+        // Spades, Clubs (S, C) -> black
+        // Hearts, Diamonds (H, D) -> red
+        string card1color;
+        string card2color;
+
         public bool AreTheSame(Card card1, Card card2)
         {
-            throw new NotImplementedException("Test 103");
+
+          if (card1.Colour() == "S" || card1.Colour() == "C")
+
+            {
+                card1color = "black";
+            }
+          else
+            {
+                card1color = "red";
+            }
+
+            if (card2.Colour() == "S" || card2.Colour() == "C")
+            {
+                card2color = "black";
+            }
+            else
+            {
+                card2color = "red";
+            }
+            if (card1color == card2color)
+            {
+                return true;
+            }
+            return false;
         }
     }
 }
