e566c39966ec50431280634cd916fb8fd53030ee
tetrael <kozlowskiprzemek@gmail.com>
2019-01-27 21:58:00 +0100

    I really don't know why T210 won't works

--- 20190124/CardMultigame/Core/GameManagers/SelectedRules/CreationCommands/SelectGame.cs
+++ 20190124/CardMultigame/Core/GameManagers/SelectedRules/CreationCommands/SelectGame.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 using Core.Usecases.GameActions;
 using Core.Entities.Games.Guessing;
 using Core.Entities.Games;
+using Core.Usecases.GameConditions;
+using Core.Usecases.CardComparison;
+using Core.Entities.GameStates;
 
 namespace Core.Containers.GameRules.CreationCommands
 {
@@ -15,7 +18,32 @@ namespace Core.Containers.GameRules.CreationCommands
 
         public void ChangeGameRuleset(GameManagerInternalsBuilder builder, string parameters)
         {
-            throw new NotImplementedException("Implement this for T210 BuilderRevealsItself");
+            //throw new NotImplementedException("Implement this for T210 BuilderRevealsItself");
+            //IGameImplementation gameImplementation = SelectGameImplementation(parameters);
+
+            //builder.SetName(gameImplementation.Title());
+            //builder.SetAvailableActions(gameImplementation.AvailableActions());
+            //builder.SetVictoryConditions(gameImplementation.VictoryConditions());
+            //builder.SetGameStopConditions(gameImplementation.GameStopConditions());
+            //builder.SetDeck(gameImplementation.CardDeck());
+            //builder.SetInitialGameState(gameImplementation.InitialGameState());
+            //builder.SetMaxTurns(5);
+            //builder.SetCardComparisonStrategy(gameImplementation.CardComparisonStrategy());
+
+            switch (parameters)
+            {
+                case "GuessACard":
+                    builder.SetAvailableActions(new List<IGameAction>() { new GuessCardAction() });
+                    builder.SetInitialGameState(new GameState());
+                    builder.SetVictoryConditions(new List<IGameCondition>() { new DidGuessACard() });
+                    builder.SetGameStopConditions(new List<IGameCondition>() { new DidTurnsExpire() });
+                    builder.SetMaxTurns(5);
+                    builder.SetCardComparisonStrategy(new StrictCardComparisonStrategy());
+                    builder.SetName("");
+                    break;
+            }
+
+
         }
 
         public bool ShouldReactTo(string outerCommandName)
