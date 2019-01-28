2cd84b530f226e396128124c17f10525c161ccaf
tetrael <kozlowskiprzemek@gmail.com>
2019-01-28 16:40:40 +0100

    Added new Deck to play Blackjack

---
+++ 20190124/CardMultigame/Instructions/Batch_300.cs
@@ -0,0 +1,43 @@
+﻿using Core.Containers.GameManagers.Rules.CreationCommands;
+using Core.Containers.GameRules;
+using Core.Containers.GameRules.CreationCommands;
+using Core.Entities.Cards;
+using Core.Entities.Decks;
+using Core.Entities.Games;
+using Core.Entities.GameStates;
+using Core.GameManagers.SelectedRules;
+using Core.Usecases.CardComparison;
+using Core.Usecases.GameActions;
+using Core.Usecases.GameConditions;
+using Core.Usecases.InfluenceState;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TrainingTests
+{
+
+    public class Batch_300
+    {
+
+        [Test]
+        public void T_301_GenerateDeckForBlackjack()
+        {
+            // Given
+            CardDeck createdDeck = new CardDeckFactory().Simple9ToKWith4Colours();
+
+            // Expected
+            CardDeck expectedDeck = new CardDeckFactory()
+                                .FromGivenCards("9S 9H 9D 9C 10S 10H 10D 10C JS JH JD JC " +
+                                                "QS QH QD QC KS KH KD KC");
+
+            // When
+
+
+            // Then
+            Assert.AreSame(createdDeck, expectedDeck);
+
+        }
+    }
+
+}
