8a1cb1b72e4fb80f067c977710eeeea3d824b95c
Paweł Howaniec <paolopek@gmail.com>
2019-01-28 18:22:08 +0100

    trying to start solving Black Jack

---
+++ 20190124/CardMultigame/Core/Entities/Games/BlackJack/BlackJack.cs
@@ -0,0 +1,52 @@
+﻿using Core.Entities.Games.Guessing;
+using System.Collections.Generic;
+using Core.Entities.Decks;
+using Core.Usecases.GameActions;
+using Core.Usecases.GameConditions;
+using Core.Entities.GameStates;
+using Core.Usecases.CardComparison;
+using System;
+
+namespace Core.Entities.Games
+{
+	public class BlackJack : IGameImplementation
+	{
+		public CardDeck CardDeck()
+		{
+			return new CardDeckFactory().Simple9ToAWith4Colours();
+		}
+
+		public List<IGameAction> AvailableActions()
+		{
+			return new List<IGameAction> { new GuessCardAction() };
+		}
+
+		public List<IGameCondition> GameStopConditions()
+		{
+			return new List<IGameCondition> { new DidTurnsExpire() };
+		}
+
+		public List<IGameCondition> VictoryConditions()
+		{
+			return new List<IGameCondition> { new DidGuessACard() };
+		}
+
+		public GameState InitialGameState()
+		{
+			return new GameState()
+			{
+				{ GameStateKeys.MaxTurns, 5 }
+			};
+		}
+
+		public string Title()
+		{
+			return "Black Jack Bitches!";
+		}
+
+		public ICardComparisonStrategy CardComparisonStrategy()
+		{
+			return new StrictCardComparisonStrategy();
+		}
+	}
+}
