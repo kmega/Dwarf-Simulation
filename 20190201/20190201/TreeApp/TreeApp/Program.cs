47b140b803b4dc34e01065f2e7553bf52b69d1ca
Jan Kocon <jankocon7@gmail.com>
2019-02-01 18:55:57 +0100

    Not yet..

---
+++ 20190201/20190201/TreeApp/TreeApp/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Linq;
+
+namespace TreeApp
+{
+   
+    class Program
+    { 
+        static string extractPath(List<Record> records,int depth)
+        {
+            string result = "";
+            int i = 0;
+            while(i <= depth)
+            {
+                foreach(Record record in records)
+                {
+                    if(record.depth == i)
+                    {
+                        result += record.name + "/";
+                        break;
+                    }
+                }
+                i++;
+            }
+            return result;
+        }
+        public static List<Record> ReadTreeFromFile(string filePath)
+        {
+            string[] lines = File.ReadAllLines(filePath);
+            List<Record> records = new List<Record>();
+            foreach (string line in lines)
+            {
+                int indexOfBeginningRecord = line.IndexOf("1. ");
+                int depth = indexOfBeginningRecord / 4;
+                string name = line.Substring(indexOfBeginningRecord + 3);
+                string path = extractPath(records, depth);
+                records.Add(new Record(name, depth, path));
+            }
+            return records;
+        }
+        static void Main(string[] args)
+        {
+            List<Record> firstTree = ReadTreeFromFile(@"C:\Users\Lenovo\Desktop\a.txt");
+            List<Record> secondTree = ReadTreeFromFile(@"C:\Users\Lenovo\Desktop\b.txt");
+            
+            Console.WriteLine("RESULT");
+            firstTree.AddRange(secondTree);
+            List<Record> result = new List<Record>();
+            for(int i=0;i<firstTree.Count;i++)
+            {
+                if (!result.Contains(firstTree[i]))
+                    result.Add(firstTree[i]);
+            }
+            printTree(result);
+            
+            
+            
+
+
+
+
+            Console.ReadLine();
+        }
+
+        private static void printTree(List<Record> tree)
+        {
+            foreach(Record record in tree)
+                Console.WriteLine(record);
+        }
+    }
+}
---
+++ 20190201/20190201/TreeApp/TreeApp/Record.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TreeApp
+{
+    class Record
+    {
+        public string name { get; set; }
+        public int depth { get; set; }
+        public string path { get; set; }
+
+        public Record(string name, int depth, string path)
+        {
+            this.name = name;
+            this.path = path;
+            this.depth = depth;
+        }
+        public override string ToString()
+        {
+            string result = "";
+            for (int i = 0; i < this.depth; i++)
+            {
+                result += "--";
+            }
+            result += ">";
+            return result + name;
+        }
+        public override bool Equals(object obj)
+        {
+            Record other = obj as Record;
+            if (this.path != other.path) return false;
+
+            return true;
+        }
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(path);
+        }
+
+    }
+}
