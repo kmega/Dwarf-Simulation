f4aecbc95221b67c0002b2d6d4682f519a78d1ad
Michał Czyrnek <e.s.michael.c@gmail.cok>
2019-02-01 18:54:47 +0100

    Path builder not work

---
+++ 20190201/20190201/Drzewa/Drzewa/Location.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Drzewa
+{
+    public class Location
+    {
+
+      public  int depth;
+       public string name;
+       public string path;
+
+        public Location (string record, string[] parse, int iterator)
+        {
+            depth = 0;
+            for (int i = 0; i < record.Length; i++)
+                if (!(Char.IsLetter(record.ToCharArray()[i])))
+                {
+                    depth++;
+                }
+            name = record.TrimStart(' ');
+
+            Path(parse, iterator);
+
+        }
+
+        void Path(string[] parse, int iterator)
+        {
+            int itemdepth = 0;
+            int pathiterator = 1;
+            for (int i = iterator - 1; i >= 0; i--)
+            {
+                for (int j = 0; j < parse[i].Length; j++)
+                    if (!(Char.IsLetter(parse[i].ToCharArray()[j])))
+                    {
+                        itemdepth++;
+                    }
+
+               if (itemdepth == depth-pathiterator)
+                {
+                    path = parse[i].TrimStart(' ') +"|"+ path;
+                    pathiterator++;
+                    itemdepth = 0;
+                }
+
+
+            }
+
+
+        }
+
+
+    }
+}
---
+++ 20190201/20190201/Drzewa/Drzewa/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Drzewa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Location> lista = new List<Location>();
+          
+            string test = "Świat|" +
+                " Primus|" +
+                "  Astoria|" +
+                "   Szczeliniec|" +
+                "    Powiat Pustogórski|" +
+                "     Pustogor|" +
+                "      Gabinet Pięknotki|" +
+                "     Zaczęstwo|" +
+                "      Cyberszkoła|" +
+                "      Osiedle Ptasie|" +
+                "     Trzęsawisko Zjawosztup|" +
+                "      Głodna Ziemia";
+            string test2 = "Świat| Estrinus";
+           string  test3 = "Świat| Estrinus";
+
+
+
+           string[] parse = test.Split('|');
+            for (int i = 0; i < parse.Length; i++)
+            {
+                Location temp = new Location(parse[i],parse, i);
+                if (lista.Count == 0)
+                {
+                    lista.Add(temp);
+
+
+                }
+                else
+                {
+                    for (int j=0;j<lista.Count;j++)
+                      
+                        if (!(lista.Exists(k => k.name == temp.name)))
+                        {
+                            lista.Add(temp);
+                        }
+
+
+                    }
+                }
+            
+
+         parse = test2.Split('|');
+            for (int i = 0; i<parse.Length; i++)
+            {
+                Location temp = new Location(parse[i], parse, i);
+
+                    for (int j=0;j<lista.Count;j++)
+                      
+                        if (!(lista.Exists(k => k.name == temp.name)))
+                        {
+                            lista.Add(temp);
+                        }
+
+
+                    }
+                
+
+            parse = test3.Split('|');
+            for (int i = 0; i < parse.Length; i++)
+            {
+                Location temp = new Location(parse[i],parse, i);
+
+                
+                    for (int j = 0; j < lista.Count; j++)
+
+                        if (!(lista.Exists(k => k.name == temp.name)))
+                        {
+                            lista.Add(temp);
+                        }
+                
+            }
+
+            
+           foreach (var item in lista)
+            {
+                Console.WriteLine(item.depth + " " + item.name + "     " + item.path);
+                    
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
