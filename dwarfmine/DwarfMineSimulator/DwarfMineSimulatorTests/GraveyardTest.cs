c07462d68a89c3f23ebf62b701c41a87f84779a0
Lukasz Radzik <radziklukasz@wp.pl>
2019-02-05 17:56:41 +0100

    Add graveyard with tests

--- dwarfmine/DwarfMineSimulator/DwarfMineSimulatorTests/GraveyardTest.cs
+++ dwarfmine/DwarfMineSimulator/DwarfMineSimulatorTests/GraveyardTest.cs
@@ -1,18 +1,102 @@
 ﻿using NUnit.Framework;
+using DwarfMineSimulator;
+using System.Collections.Generic;
 
 namespace Tests
 {
     public class GraveyardTest
     {
-        [SetUp]
-        public void Setup()
+        public List<Dwarf> Setup()
         {
+            List<Dwarf> DwarfsPopulation = new List<Dwarf>()
+            {
+                new Dwarf() { Alive = true, Type = DwarfTypes.Father, Money = 0, MoneyEarndedThisDay = 0 },
+                new Dwarf() { Alive = true, Type = DwarfTypes.Father, Money = 0, MoneyEarndedThisDay = 0 },
+                new Dwarf() { Alive = false, Type = DwarfTypes.Father, Money = 0, MoneyEarndedThisDay = 0 },
+                new Dwarf() { Alive = true, Type = DwarfTypes.Father, Money = 0, MoneyEarndedThisDay = 0 }
+            };
+            return DwarfsPopulation;
+        }
+
+        public List<Dwarf> Setup1()
+        {
+            List<Dwarf> DwarfsPopulation = new List<Dwarf>()
+            {
+                new Dwarf() { Alive = true, Type = DwarfTypes.Father, Money = 0, MoneyEarndedThisDay = 0 },
+                new Dwarf() { Alive = false, Type = DwarfTypes.Father, Money = 0, MoneyEarndedThisDay = 0 },
+                new Dwarf() { Alive = false, Type = DwarfTypes.Father, Money = 0, MoneyEarndedThisDay = 0 },
+                new Dwarf() { Alive = false, Type = DwarfTypes.Father, Money = 0, MoneyEarndedThisDay = 0 }
+            };
+            return DwarfsPopulation;
+        }
+
+        public List<Dwarf> Setup2()
+        {
+            List<Dwarf> DwarfsPopulation = new List<Dwarf>()
+            {
+                new Dwarf() { Alive = false, Type = DwarfTypes.Father, Money = 0, MoneyEarndedThisDay = 0 },
+                new Dwarf() { Alive = false, Type = DwarfTypes.Father, Money = 0, MoneyEarndedThisDay = 0 },
+                new Dwarf() { Alive = false, Type = DwarfTypes.Father, Money = 0, MoneyEarndedThisDay = 0 },
+                new Dwarf() { Alive = false, Type = DwarfTypes.Father, Money = 0, MoneyEarndedThisDay = 0 }
+            };
+            return DwarfsPopulation;
+        }
+
+
+        [Test]
+        public void DeleteOneDeadDwarfFromList()
+        {
+            //Given
+            List<Dwarf> DwarfsPopulation = Setup();
+            Graveyard graveyard = new Graveyard();
+
+            //When
+            DwarfsPopulation = graveyard.DeleteDeadDwarfFromList(DwarfsPopulation);
+
+            //Then
+            foreach (var dwarf in DwarfsPopulation)
+            {
+                Assert.AreEqual(dwarf.Alive, true);
+            }
+
+            Assert.AreEqual(graveyard.HowManyDead(), 1);
         }
 
         [Test]
-        public void Test1()
+        public void DeleteManyDeadDwarfFromList()
         {
-            Assert.Pass();
+            //Given
+            List<Dwarf> DwarfsPopulation = Setup1();
+            Graveyard graveyard = new Graveyard();
+
+            //When
+            DwarfsPopulation = graveyard.DeleteDeadDwarfFromList(DwarfsPopulation);
+
+            //Then
+            foreach (var dwarf in DwarfsPopulation)
+            {
+                Assert.AreEqual(dwarf.Alive, true);
+            }
+            Assert.AreEqual(graveyard.HowManyDead(), 3);
         }
+
+        [Test]
+        public void DeleteAllDeadDwarfFromList()
+        {
+            //Given
+            List<Dwarf> DwarfsPopulation = Setup2();
+            Graveyard graveyard = new Graveyard();
+
+            //When
+            DwarfsPopulation = graveyard.DeleteDeadDwarfFromList(DwarfsPopulation);
+
+            //Then
+            foreach (var dwarf in DwarfsPopulation)
+            {
+                Assert.AreEqual(dwarf.Alive, true);
+            }
+            Assert.AreEqual(graveyard.HowManyDead(), 4);
+        }
+
     }
 }
