71aa6ee62561947ec775e4e0e39b62db42efb873
Jan <jankocon7@gmail.com>
2019-02-05 18:28:40 +0100

    Shop functionality added

---
+++ DwarfsCity/DwarfCityTests/ShopTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DwarfsCity.DwarfContener;
+using DwarfsCity;
+
+namespace DwarfCityTests
+{
+    [TestClass]
+    public class ShopTests
+    {
+        [TestMethod]
+        public void ShouldDwarfPayHalfMoneyWhenDwarfTypeIsFatherOrSingle()
+        {
+            //given
+            List<Dwarf> dwarfs = new List<Dwarf>();
+            Dwarf dwarfFather = new Dwarf();
+            dwarfFather.Attrybute = DwarfsCity.DwarfContener.Type.Father;
+            dwarfFather.Backpack.Moneys = 1000;
+            Dwarf dwarfSingle = new Dwarf();
+            dwarfSingle.Attrybute = DwarfsCity.DwarfContener.Type.Father;
+            dwarfSingle.Backpack.Moneys = 1000;
+            dwarfs.Add(dwarfFather);
+            dwarfs.Add(dwarfSingle);
+            Shop shop = new Shop();
+
+            decimal expectedFatherMoney = dwarfFather.Backpack.Moneys / 2;
+            decimal expectedSingleMoney = dwarfSingle.Backpack.Moneys / 2;
+            //when
+            shop.PerformShopping(dwarfs);
+
+            // then
+            decimal actualFatherMoney = dwarfFather.Backpack.Moneys;
+            decimal actualSingleMoney = dwarfSingle.Backpack.Moneys;
+            Assert.AreEqual(expectedFatherMoney, actualFatherMoney);
+            Assert.AreEqual(expectedSingleMoney, actualSingleMoney);
+
+        }
+    }
+}
--- DwarfsCity/DwarfsCity/Shop/Shop.cs
+++ DwarfsCity/DwarfsCity/Shop/Shop.cs
@@ -1,6 +1,21 @@
-﻿namespace DwarfsCity
+﻿using DwarfsCity.DwarfContener;
+using System.Collections.Generic;
+namespace DwarfsCity
 {
     public class Shop
     {
+        public void PerformShopping(List<Dwarf> dwarfs)
+        {
+            foreach(Dwarf dwarf in dwarfs)
+            {
+                if(dwarf.Attrybute == Type.Father || dwarf.Attrybute == Type.Single)
+                {
+                    decimal price = dwarf.Backpack.Moneys / 2;
+                    dwarf.Backpack.Moneys /= 2;
+                    string product = dwarf.Attrybute == Type.Father ? "Alcohol" : "Food";
+                    // Log.Shop(@"Dwarf bought {product}");
+                }
+            }
+        }
     }
 }
