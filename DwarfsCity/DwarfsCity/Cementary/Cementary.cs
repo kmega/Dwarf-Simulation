89f0b20d974817a2c6b366de3f6e2f29705d3721
Matthew-1 <matikm2@gmail.com>
2019-02-05 18:38:19 +0100

    Implementet basics of Cementary class - BurryDeadDwarfs

--- DwarfsCity/DwarfsCity/Cementary/Cementary.cs
+++ DwarfsCity/DwarfsCity/Cementary/Cementary.cs
@@ -1,6 +1,15 @@
-﻿namespace DwarfsCity
+﻿using DwarfsCity.DwarfContener;
+using System.Collections.Generic;
+
+namespace DwarfsCity
 {
     public class Cementary
     {
+        public void BurryDeadDwarfs(List<Dwarf> deadDwarfs)
+        {
+            deadDwarfs.Clear();
+
+            //give report it the shape of log/logger 
+        }
     }
 }
