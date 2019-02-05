8dee0e96dd08c647f01b3a4f57de175d4e1d5cfd
--replace-all <luki990@onet.eu>
2019-02-05 15:56:53 +0100

    Begining DwarfCity, make solution

---
+++ DwarfsCity/DwarfsCity/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DwarfsCity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
