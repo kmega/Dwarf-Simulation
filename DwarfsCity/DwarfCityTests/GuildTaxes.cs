a8d665cfd89e27eb9e86e3002b56731c89171d1d
Alicja Szweda <szwedaalicja@gmail.com>
2019-02-05 23:23:06 +0100

    Guild is working

---
+++ DwarfsCity/DwarfCityTests/GuildTaxes.cs
@@ -0,0 +1,25 @@
+﻿using DwarfsCity;
+using DwarfsCity.DwarfContener;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DwarfCityTests
+{
+    [TestClass]
+    public class GuildTaxes
+    {
+        [TestMethod]
+        public void ShouldReturnTaxesForOneDwarf()
+        {
+            List<Dwarf> Dwarfs = new List<Dwarf>();
+            Dwarfs.Add(new Dwarf());
+            Dwarfs[0].Backpack.Moneys = 4;        
+            Guild guild = new Guild();
+            decimal expected = 1;
+            decimal result = guild.GetTaxesofAllDwarfs(Dwarfs);
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
--- DwarfsCity/DwarfsCity/Finanse/Guild.cs
+++ DwarfsCity/DwarfsCity/Finanse/Guild.cs
@@ -1,6 +1,28 @@
-﻿namespace DwarfsCity
+﻿using DwarfsCity.DwarfContener;
+using System;
+using System.Collections.Generic;
+
+namespace DwarfsCity
 {
     public class Guild
     {
+        //public Guild()
+        //{ //konstruktor na wypadek jakby był potrzebny
+        //    City city = new City();
+        //    List<Dwarf> Dwarfs = city.GetDwarfs();
+        //    GetTaxesofAllDwarfs(Dwarfs);           
+        //}
+        public decimal GetTaxesofAllDwarfs(List<Dwarf> dwarfs)
+        {
+            decimal TheSumOfTaxes = 0;
+            foreach (Dwarf dwarf in dwarfs)
+            {
+                decimal EarnedMoney = dwarf.Backpack.Moneys;
+                decimal TaxesOfGuild = 0.25m * dwarf.Backpack.Moneys;
+                EarnedMoney -= TaxesOfGuild;
+                TheSumOfTaxes += TaxesOfGuild;
+            }
+            return TheSumOfTaxes;          
+        }
     }
 }
