95c77ef7414d11d9ad6cff33140a19bbe637ac10
piotr <piotrrzeznik@gmail.com>
2019-02-05 18:34:04 +0100

    Localy merged with marcnis Dwarf.cs

--- 20190205/MiningSimulatorByKPMM/MiningSimulatorByKPMM/DwarfsTypes/Dwarf.cs
+++ 20190205/MiningSimulatorByKPMM/MiningSimulatorByKPMM/DwarfsTypes/Dwarf.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using MiningSimulatorByKPMM.Enums;
 using MiningSimulatorByKPMM.Interfaces;
 
@@ -6,17 +7,46 @@ namespace MiningSimulatorByKPMM.DwarfsTypes
 {
     public class Dwarf
     {
-        public E_DwarfType DwarfType { get; set; }
-        public decimal BankAccount { get; set; }
-        public decimal DailySalary { get; set; }
-        public List<E_Minerals> PersonalEquipment { get; set; }
-        public IMineAction WorkingAction { get; set; }
-        public List<IShopAction> ShoppingActions { get; set; }
+        public E_DwarfType DwarfType { get;private set; }
+        public decimal BankAccount { get;private set; }
+        public decimal DailySalary { get;private set; }
+        public List<E_Minerals> PersonalEquipment { get;private set; }
+        public IMineAction WorkingActions { get;private set; }
+        public IShopAction ShoppingActions { get;private set; }
 
-        public Dwarf()
+        public Dwarf(E_DwarfType e_DwarfType, IMineAction mineAction, IShopAction shopAction)
+        {
+            SetInitialList();
+            SetInitialProperties(e_DwarfType, mineAction, shopAction);            
+        }
+
+        private void SetInitialProperties(E_DwarfType e_DwarfType, IMineAction mineAction, IShopAction shopAction)
+        {
+            DwarfType = e_DwarfType;
+            WorkingActions = mineAction;
+            ShoppingActions = shopAction;
+            BankAccount = 0.0m;
+            DailySalary = 0.0m;
+        }
+        private void SetInitialList()
         {
             PersonalEquipment = new List<E_Minerals>();
-            ShoppingActions = new List<IShopAction>();
+        }
+        public void AddMinedMaterial(E_Minerals e_Mineral)
+        {
+            PersonalEquipment.Add(e_Mineral);
+        }
+        public void RemoveSingleMaterialFromEquipment(E_Minerals e_Mineral)
+        {
+            PersonalEquipment.Remove(e_Mineral);
+        }
+        public void EarnMoney(decimal earnedMoney)
+        {
+            DailySalary = earnedMoney;
+        }
+        public void SpendMoney(decimal spentMoney)
+        {
+            DailySalary -= spentMoney;
         }
     }
 }
