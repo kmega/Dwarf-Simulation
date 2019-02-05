ea40094d84b49c61b7437b9d98e1b13de7a39f0c
Dawid1992 <dawidlogan@gmail.com>
2019-02-05 18:37:44 +0100

    initialize new dwarf minerals

--- dwarfmine/DwarfMineSimulator/DwarfMineSimulator/Dwarf.cs
+++ dwarfmine/DwarfMineSimulator/DwarfMineSimulator/Dwarf.cs
@@ -15,7 +15,19 @@ namespace DwarfMineSimulator
 
         internal Dictionary<Minerals, int> MineralsMined { get; set; }
 
-
+        public Dwarf()
+        {
+            Alive = true;
+            Money = 0;
+            MoneyEarndedThisDay = 0;
+            MineralsMined = new Dictionary<Minerals, int>()
+            {
+                [Minerals.Mithril] = 0,
+                [Minerals.Gold] = 0,
+                [Minerals.Silver] = 0,
+                [Minerals.TaintedGold] = 0
+            };
+        }
 
     }
 }
