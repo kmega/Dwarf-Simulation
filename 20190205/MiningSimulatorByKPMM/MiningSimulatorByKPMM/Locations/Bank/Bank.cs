4f8c402b7a3d8a1603a6c5105d606284916f33e2
Michał Czyrnek <e.s.michael.c@gmail.cok>
2019-02-06 18:03:46 +0100

    Add part of Bank class

---
+++ 20190205/MiningSimulatorByKPMM/MiningSimulatorByKPMM/Locations/Bank/Bank.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MiningSimulatorByKPMM.Locations.Bank
+{
+    public class Bank
+    {
+       private decimal BankAccount;
+
+        public static void PayIntoYourAccount (decimal bankaccount, decimal value)
+        {
+            bankaccount += value;
+        }
+
+        public static void PayTax (decimal receipt)
+        {
+           
+
+
+
+        }
+
+
+    }
+}
