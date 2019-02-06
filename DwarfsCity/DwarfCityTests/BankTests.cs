56bc0dcbad53221410928f9c1e6a700edcc6e0f8
Jan <jankocon7@gmail.com>
2019-02-06 18:41:18 +0100

    Added Bank functionally

---
+++ DwarfsCity/DwarfCityTests/BankTests.cs
@@ -0,0 +1,31 @@
+﻿using DwarfsCity.DwarfContener;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DwarfsCity.DwarfContener.DwarfEquipment;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using DwarfsCity;
+
+namespace DwarfCityTests
+{
+    [TestClass]
+    public class BankTests
+    {
+        [TestMethod]
+        public void ShouldChangeItemsToMoneyWhenDwarfComeToBank()
+        {
+            // given
+            Dwarf dwarf = new Dwarf();
+            dwarf.Backpack.Items.Add(Item.DirtyGold);
+            List<Dwarf> dwarfs = new List<Dwarf>();
+            dwarfs.Add(dwarf);
+            Bank bank = new Bank();
+
+            // when
+            bank.ExchangeItemsToMoney(dwarfs);
+            decimal actual = dwarf.Backpack.Money;
+            // then
+            Assert.IsTrue(actual >= 1 && actual <= 5);
+        }
+    }
+}
