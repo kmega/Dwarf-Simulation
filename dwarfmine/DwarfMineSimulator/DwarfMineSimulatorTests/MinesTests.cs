aa51e3cc780a1c6b31e8ae0f7c2cf590fb9921d8
Karol Szelążek <charles.sz@protonmail.com>
2019-02-06 16:16:55 +0100

    Corrected suicider behavior. Created three more tests for mines.

--- dwarfmine/DwarfMineSimulator/DwarfMineSimulatorTests/MinesTests.cs
+++ dwarfmine/DwarfMineSimulator/DwarfMineSimulatorTests/MinesTests.cs
@@ -7,7 +7,7 @@ namespace DwarfMineSimulatorTests
     public class MinesTests
     {
         [Test]
-        public void x()
+        public void OneMinerShouldMineMinerals()
         {
             // For
             Mines mines = new Mines();
@@ -16,16 +16,16 @@ namespace DwarfMineSimulatorTests
             List<Shaft> shafts = new List<Shaft>();
 
             // Given
-            dwarfs.Add(new Dwarf({ Type = DwarfTypes.Father });
+            dwarfs.Add(new Dwarf() { Type = DwarfTypes.Father });
             shafts.Add(new Shaft());
 
             dwarfs = mines.MineInShafts(dwarfs, shafts);
-            Dictionary<Minerals, int> results = dwarfs[0].MineralsMined;
 
             // Assert
+            Dictionary<Minerals, int> results = dwarfs[0].MineralsMined;
             List<int> listOfValues = new List<int>();
-            listOfValues.AddRange(results.Values);
 
+            listOfValues.AddRange(results.Values);
             int mineralsMined = 0;
 
             for (int i = 0; i < results.Count; i++)
@@ -34,6 +34,111 @@ namespace DwarfMineSimulatorTests
             }
 
             Assert.IsTrue(mineralsMined > 0);
+        }
+
+        [Test]
+        public void TenMinerShouldMineMinerals()
+        {
+            // For
+            Mines mines = new Mines();
+
+            List<Dwarf> dwarfs = new List<Dwarf>();
+            List<Shaft> shafts = new List<Shaft>();
+
+            // Given
+            for (int i = 0; i < 10; i++)
+            {
+                dwarfs.Add(new Dwarf() { Type = DwarfTypes.Father });
+            }
+            shafts.Add(new Shaft());
+
+            dwarfs = mines.MineInShafts(dwarfs, shafts);
+
+            // Assert
+            Dictionary<Minerals, int> results;
+            List<int> listOfValues = new List<int>();
+
+            int mineralsMined = 0;
+
+            for (int i = 0; i < dwarfs.Count; i++)
+            {
+                results = dwarfs[i].MineralsMined;
+                listOfValues.AddRange(results.Values);
+
+                for (int j = 0; j < results.Count; j++)
+                {
+                    mineralsMined += listOfValues[j];
+                }
+
+                Assert.IsTrue(mineralsMined > 0);
+                mineralsMined = 0;
+            }
+        }
+
+        [Test]
+        public void TwentyMinerShouldMineMineralsInTwoShafts()
+        {
+            // For
+            Mines mines = new Mines();
+
+            List<Dwarf> dwarfs = new List<Dwarf>();
+            List<Shaft> shafts = new List<Shaft>();
+
+            // Given
+            for (int i = 0; i < 10; i++)
+            {
+                dwarfs.Add(new Dwarf() { Type = DwarfTypes.Father });
+            }
+            shafts.Add(new Shaft());
+            shafts.Add(new Shaft());
+
+            dwarfs = mines.MineInShafts(dwarfs, shafts);
+
+            // Assert
+            Dictionary<Minerals, int> results;
+            List<int> listOfValues = new List<int>();
+
+            int mineralsMined = 0;
+
+            for (int i = 0; i < dwarfs.Count; i++)
+            {
+                results = dwarfs[i].MineralsMined;
+                listOfValues.AddRange(results.Values);
+
+                for (int j = 0; j < results.Count; j++)
+                {
+                    mineralsMined += listOfValues[j];
+                }
+
+                Assert.IsTrue(mineralsMined > 0);
+                mineralsMined = 0;
+            }
+        }
+
+        [Test]
+        public void SuiciderShouldKillEveryoneInShaftAndCollapseShaft()
+        {
+            // For
+            Mines mines = new Mines();
+
+            List<Dwarf> dwarfs = new List<Dwarf>();
+            List<Shaft> shafts = new List<Shaft>();
+
+            // Given
+            for (int i = 0; i < 4; i++)
+            {
+                dwarfs.Add(new Dwarf() { Type = DwarfTypes.Father });
+            }
+            dwarfs.Add(new Dwarf() { Type = DwarfTypes.Suicider });
+            shafts.Add(new Shaft());
+
+            dwarfs = mines.MineInShafts(dwarfs, shafts);
+
+            // Assert
+            for (int i = 0; i < dwarfs.Count; i++)
+            {
+                Assert.IsTrue(dwarfs[i].Alive == false);
+            }
         }
     }
 }
