b806fa68b7eb907fb9b45479d36956bd3211a9bf
Krzysztof Gebski <krzysztof.gebski@gmail.com>
2019-02-06 23:22:38 +0100

    Added Hospital, Mine and Canteen methods to SimulationEngine class, simple refactor

--- 20190205/MiningSimulatorByKPMM/MiningSimulatorByKPMM/ApplicationLogic/SimulationEngine.cs
+++ 20190205/MiningSimulatorByKPMM/MiningSimulatorByKPMM/ApplicationLogic/SimulationEngine.cs
@@ -1,7 +1,7 @@
-﻿using MiningSimulatorByKPMM.DwarfsTypes;
-using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using MiningSimulatorByKPMM.Locations.Canteen;
+using MiningSimulatorByKPMM.Locations.Guild;
+using MiningSimulatorByKPMM.Locations.Hospital;
+using System.Linq;
 
 namespace MiningSimulatorByKPMM.ApplicationLogic
 {
@@ -16,17 +16,32 @@ namespace MiningSimulatorByKPMM.ApplicationLogic
 
         public void Start()
         {
+            Hospital hospital = new Hospital();
+            Guild guild = new Guild();
+            Canteen canteen = new Canteen();
             for (int i = 0; i < 30; i++)
             {
                 //Hospital.TryToBorn(currentSimulationState) -> Dwarf or Null;
+                var dwarf = hospital.TryToCreateDwarf();
+                if (dwarf != null)
+                {
+                    _currentSimulationState.Dwarves.Add(dwarf);
+                }
 
                 //Mine.Work(List<Backpack>,List<dwarfType>) -> aktualizacja Backpack;
+                var backpacks = _currentSimulationState.Dwarves.Select(p => p.Backpack).ToList();
+                var dwarfTypes = _currentSimulationState.Dwarves.Select(p => p.DwarfType).ToList();
+
                 //Guild.PayWorkers(List<BankAccount> bankAccounts, backpacks);
+
                 // int value;
                 // Bank.PutIn(bankAccount, value);
                 // Bank.Putin(GuildAccount, valuezarobionie25%);
 
                 //Canteen(numberOfWorkersToday);
+                canteen.GiveFoodRations(_currentSimulationState.Dwarves.Count);
+                canteen.OrderFoodRations();
+
                 //Shop.BuyStaff(List<BankAccount> bankAccounts, List<DwarfType> dwarftypes);
                 // int ostatniaWpłata = Bank.GetLastInput(bankaccount);
                 //int paragon = ostatniawplata /2
