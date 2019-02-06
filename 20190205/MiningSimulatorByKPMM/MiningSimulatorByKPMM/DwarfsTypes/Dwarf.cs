bd635a33117002b5d434335ab81a88a87d0f72d8
Marcin Pochrzest <pochrzest.marcin@gmail.com>
2019-02-06 18:37:56 +0100

    Added 3 tests for hospital working, module finished

--- 20190205/MiningSimulatorByKPMM/MiningSimulatorByKPMM/DwarfsTypes/Dwarf.cs
+++ 20190205/MiningSimulatorByKPMM/MiningSimulatorByKPMM/DwarfsTypes/Dwarf.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using MiningSimulatorByKPMM.Enums;
-using MiningSimulatorByKPMM.Interfaces;
-using MiningSimulatorByKPMM.Locations.Mine.Miningoutputs;
 using MiningSimulatorByKPMM.PersonalItems;
 
 namespace MiningSimulatorByKPMM.DwarfsTypes
