4a810ef0ac091a384c167f7badcea49014782f09
Matthew-1 <matikm2@gmail.com>
2019-02-06 16:28:47 +0100

    Created Inteface IReport, class Report which is responsible to analise reports from main classes. Created class UI

---
+++ DwarfsCity/DwarfsCity/UI/DisplayReport.cs
@@ -0,0 +1,18 @@
+﻿using DwarfsCity.Reports;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DwarfsCity.UI
+{
+    public class DisplayReport
+    {
+        public void Display(Report reports)
+        {
+            foreach (string report in reports.GetReportsToDisplay())
+            {
+                Console.WriteLine(report);
+            }
+        }
+    }
+}
