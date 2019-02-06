d46a14ab1bd45a541b64801a4f4b9c9576e9225d
Marcin Pochrzest <pochrzest.marcin@gmail.com>
2019-02-06 17:56:04 +0100

    High level logic Added, solved Bank place

---
+++ 20190205/MiningSimulatorByKPMM/MiningSimulatorByKPMM/ApplicationLogic/SimulationEngine.cs
@@ -0,0 +1,38 @@
+﻿using MiningSimulatorByKPMM.DwarfsTypes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MiningSimulatorByKPMM.ApplicationLogic
+{
+    public class SimulationEngine
+    {
+        SimulationState currentSimulationState;
+        public SimulationEngine()
+        {
+            currentSimulationState = new SimulationState();
+        }
+
+        public void Start()
+        {
+            for(int i = 0; i<30; i++)
+            {
+                //Hospital.TryToBorn(currentSimulationState) -> Dwarf or Null;
+                //Mine.Work(Backpack, dwarfType) -> aktualizacja Backpack;
+                //Guild.PayWorkers(List<BankAccount> bankAccounts, backpacks);
+                    // int value;
+                    // Bank.PutIn(bankAccount, value);
+                    // Bank.Putin(GuildAccount, valuezarobionie25%);
+
+                //Canteen(numberOfWorkersToday);
+                //Shop.BuyStaff(List<BankAccount> bankAccounts, List<DwarfType> dwarftypes);
+                    // int ostatniaWpłata = Bank.GetLastInput(bankaccount);
+                    //int paragon = ostatniawplata /2 
+                    // int result = Bank.PayTax(paragon);
+                    // Bank.Putin(shopBankAccount, result);
+                //Bank.SumDay(List<bankAccount>);
+            //Bankaccount -> overallMoney, lastInput
+            }
+        }
+    }
+}
---
+++ 20190205/MiningSimulatorByKPMM/MiningSimulatorByKPMM/ApplicationLogic/SimulationState.cs
@@ -0,0 +1,12 @@
+﻿using MiningSimulatorByKPMM.DwarfsTypes;
+using System.Collections.Generic;
+
+namespace MiningSimulatorByKPMM.ApplicationLogic
+{
+    public class SimulationState
+    {
+        public List<Dwarf> SocietyMembers { get; set; }
+        public int NumberOfDeadDwarves { get; set; }
+        public bool ShouldSimulationBreak { get; set; }
+    }
+}
