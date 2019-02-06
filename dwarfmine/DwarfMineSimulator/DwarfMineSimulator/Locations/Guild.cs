280bd465645fbc19910ae5ceb64d2d1b2383c7f6
hinren <projekt.pierscien@gmail.com>
2019-02-06 17:45:39 +0100

    Guild tax and view information

--- dwarfmine/DwarfMineSimulator/DwarfMineSimulator/Locations/Guild.cs
+++ dwarfmine/DwarfMineSimulator/DwarfMineSimulator/Locations/Guild.cs
@@ -8,6 +8,8 @@ namespace DwarfMineSimulator
     {
         public List<Dwarf> HowMuchDwarfEarnedMoney(List<Dwarf> DwarfsPopulation)
         {
+            decimal sumMoney = 0;
+            decimal sumTax = 0;
             for (int i = 0; i < DwarfsPopulation.Count; i++)
             {
                 Random priceForRawMaterials = new Random();
@@ -16,15 +18,20 @@ namespace DwarfMineSimulator
                 
                 //mithril 15 - 25j, zoto 10 - 20j, srebro 5 - 15, brudne zoto 2
                 decimal earnedMoney = HowMuchMonetDwarfsGet(priceForRawMaterials, howMuchGold, howMuchMithril, howMuchTrainedGold, howMuchSilver);
-
+                decimal taxMoney = earnedMoney * 0.25m;
+                earnedMoney -= taxMoney;
+                Simulation.TotalMoneyEarned += earnedMoney;
+                Simulation.TaxedMoney += taxMoney;
+                sumMoney += earnedMoney;
+                sumTax += taxMoney;
                 DwarfsPopulation[i].MoneyEarndedThisDay = earnedMoney;
                 DwarfsPopulation[i].Money += earnedMoney;
 
                 SetMaterialstozero(DwarfsPopulation, i);
             }
             Console.WriteLine("##Money and taxed from guild");
-            Console.WriteLine("Total money earned:" + Simulation.TotalMoneyEarned);
-            Console.WriteLine("Total money from tax guild" + Simulation.TaxedMoney);
+            Console.WriteLine("Total money earned:" + sumMoney);
+            Console.WriteLine("Total money from tax guild" + sumTax);
             return DwarfsPopulation;
         }
 
@@ -34,11 +41,7 @@ namespace DwarfMineSimulator
             int priceGold = priceForRawMaterials.Next(10, 21);
             int priceSilver = priceForRawMaterials.Next(5, 16);
             decimal earnedMoney = (howMuchGold * priceGold) + (howMuchMithril * priceMithril)
-                + (howMuchSilver * priceSilver) + (howMuchTrainedGold * 2);
-            Simulation.TotalMoneyEarned += earnedMoney;
-            decimal taxMoney = earnedMoney * 0.25m;
-            Simulation.TaxedMoney += taxMoney;
-            earnedMoney -= taxMoney;
+                + (howMuchSilver * priceSilver) + (howMuchTrainedGold * 2);        
             return earnedMoney;
         }
 
