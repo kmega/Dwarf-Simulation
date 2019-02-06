ff05a7a64da2a25eaafadfb0f6d8b99846906f80
PatrycjuszL <patrycjusz4@gazeta.pl>
2019-02-06 16:15:03 +0100

    Refactorising Project

--- DwarfsCity/DwarfCityTests/ShopTests.cs
+++ DwarfsCity/DwarfCityTests/ShopTests.cs
@@ -4,6 +4,7 @@ using System.Text;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using DwarfsCity.DwarfContener;
 using DwarfsCity;
+using DwarfsCity.ShopContener;
 
 namespace DwarfCityTests
 {
@@ -17,22 +18,22 @@ namespace DwarfCityTests
             List<Dwarf> dwarfs = new List<Dwarf>();
             Dwarf dwarfFather = new Dwarf();
             dwarfFather.Attribute = DwarfsCity.DwarfContener.Type.Father;
-            dwarfFather.Backpack.Moneys = 1000;
+            dwarfFather.Backpack.Money = 1000;
             Dwarf dwarfSingle = new Dwarf();
             dwarfSingle.Attribute = DwarfsCity.DwarfContener.Type.Father;
-            dwarfSingle.Backpack.Moneys = 1000;
+            dwarfSingle.Backpack.Money = 1000;
             dwarfs.Add(dwarfFather);
             dwarfs.Add(dwarfSingle);
             Shop shop = new Shop();
 
-            decimal expectedFatherMoney = dwarfFather.Backpack.Moneys / 2;
-            decimal expectedSingleMoney = dwarfSingle.Backpack.Moneys / 2;
+            decimal expectedFatherMoney = dwarfFather.Backpack.Money / 2;
+            decimal expectedSingleMoney = dwarfSingle.Backpack.Money / 2;
             //when
             shop.PerformShopping(dwarfs);
 
             // then
-            decimal actualFatherMoney = dwarfFather.Backpack.Moneys;
-            decimal actualSingleMoney = dwarfSingle.Backpack.Moneys;
+            decimal actualFatherMoney = dwarfFather.Backpack.Money;
+            decimal actualSingleMoney = dwarfSingle.Backpack.Money;
             Assert.AreEqual(expectedFatherMoney, actualFatherMoney);
             Assert.AreEqual(expectedSingleMoney, actualSingleMoney);
 
