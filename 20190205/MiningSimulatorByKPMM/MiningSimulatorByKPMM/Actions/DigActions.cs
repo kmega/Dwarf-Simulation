c1897c2bd1b2503bee3c5557d7a5d325c5da37dc
Krzysztof Gebski <krzysztof.gebski@gmail.com>
2019-02-06 16:14:17 +0100

    Added DigActions and ExplodedActions class

---
+++ 20190205/MiningSimulatorByKPMM/MiningSimulatorByKPMM/Actions/DigActions.cs
@@ -0,0 +1,19 @@
+﻿using MiningSimulatorByKPMM.Interfaces;
+using MiningSimulatorByKPMM.Locations.Mine.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MiningSimulatorByKPMM.Actions
+{
+    public class DigActions : IMineAction
+    {
+        public void Work(E_MiningSchaftStatus SchaftStatus)
+        {
+            if (SchaftStatus == E_MiningSchaftStatus.Operational)
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
---
+++ 20190205/MiningSimulatorByKPMM/MiningSimulatorByKPMM/Actions/ExplodeActions.cs
@@ -0,0 +1,18 @@
+﻿using MiningSimulatorByKPMM.Interfaces;
+using MiningSimulatorByKPMM.Locations.Mine.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MiningSimulatorByKPMM.Actions
+{
+    internal class ExplodeActions : IMineAction
+    {
+        public void Work(E_MiningSchaftStatus SchaftStatus)
+        {
+            if (SchaftStatus == E_MiningSchaftStatus.Operational)
+            {
+            }
+        }
+    }
+}
