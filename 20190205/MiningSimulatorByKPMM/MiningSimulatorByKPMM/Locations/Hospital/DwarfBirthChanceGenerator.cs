3b35200578bbfdb37ee2e29b80972f00ccd693f7
Krzysztof <Gebski>
2019-02-06 18:28:52 +0100

    Resolved conflicts

--- 20190205/MiningSimulatorByKPMM/MiningSimulatorByKPMM/Locations/Hospital/DwarfBirthChanceGenerator.cs
+++ 20190205/MiningSimulatorByKPMM/MiningSimulatorByKPMM/Locations/Hospital/DwarfBirthChanceGenerator.cs
@@ -1,13 +1,1 @@
-﻿using MiningSimulatorByKPMM.Interfaces;
-using System;
-
-namespace MiningSimulatorByKPMM.Locations.Hospital
-{
-    internal class DwarfBirthChanceGenerator : IRandomGenerator
-    {
-        public int GenerateSignleRandomNumber()
-        {
-            return new Random().Next(0, 99);
-        }
-    }
-}
+﻿
