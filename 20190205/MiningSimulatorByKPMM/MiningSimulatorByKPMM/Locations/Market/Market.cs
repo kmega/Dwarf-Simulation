62dde41a1e06f5bc7cf920161db71ef09696ab96
Paweł Howaniec <paolopek@gmail.com>
2019-02-07 15:36:25 +0100

    added another test

--- 20190205/MiningSimulatorByKPMM/MiningSimulatorByKPMM/Locations/Market/Market.cs
+++ 20190205/MiningSimulatorByKPMM/MiningSimulatorByKPMM/Locations/Market/Market.cs
@@ -3,20 +3,20 @@ using MiningSimulatorByKPMM.Enums;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using MiningSimulatorByKPMM.Locations.Bank;
 
 namespace MiningSimulatorByKPMM.Locations.Market
 {
 	public class Market 
 	{
+		public BankAccount shopMoneyAccount = new BankAccount();
+		public Dictionary<E_ProductsType, decimal> marketState = new Dictionary<E_ProductsType, decimal>();
+
 		public Market()
 		{
 			marketState.Add(E_ProductsType.Food, 0);
 			marketState.Add(E_ProductsType.Alcohol, 0);
-			shopBankAccount = 0;
 		}
-
-		private decimal shopBankAccount;
-		public Dictionary<E_ProductsType, decimal> marketState = new Dictionary<E_ProductsType, decimal>();
 		
 		public void PerformShopping(List<Dwarf> customers)
 		{
@@ -24,23 +24,25 @@ namespace MiningSimulatorByKPMM.Locations.Market
 			{
 				if (customer.DwarfType == E_DwarfType.Dwarf_Single)
 				{
-					BuyProdcutsFromMarket(dailySalary, this, E_ProductsType.Alcohol);
+					BuyProdcutsFromMarket(customer, E_ProductsType.Alcohol);
 				}
 				else if (customer.DwarfType == E_DwarfType.Dwarf_Father)
 				{
-					BuyProdcutsFromMarket(dailySalary, this, E_ProductsType.Food);
+					BuyProdcutsFromMarket(customer, E_ProductsType.Food);
 				}
 			}
 		}
 
-		public void BuyProdcutsFromMarket(decimal dailySalary, Market market, E_ProductsType productType, Bank bank)
+		public void BuyProdcutsFromMarket(Dwarf customer, E_ProductsType productType)
 		{
-			decimal spentMoney = dailySalary / 2;
-			decimal amountOfProduct = market.marketState[productType];
-			amountOfProduct += spentMoney;
-			market.marketState[productType] = amountOfProduct;
-			decimal tax = bank.PayTax(spentMoney);
-			bank.PayIntoYourAccount(shopBankAccount, spentMoney - tax);
+			decimal recipe = customer.BankAccount.LastInput / 2;
+
+			decimal amountOfProduct = marketState[productType];
+			amountOfProduct += recipe;
+			marketState[productType] = amountOfProduct;
+			
+			shopMoneyAccount.ReceivedMoney(recipe);
+			shopMoneyAccount.CalculateOverallAccount();
 		}
 	}
 }
--- 20190205/MiningSimulatorByKPMM/SimulationTests/Market Tests/MarketTests.cs
+++ 20190205/MiningSimulatorByKPMM/SimulationTests/Market Tests/MarketTests.cs
@@ -1,1 +1,65 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MiningSimulatorByKPMM.Enums;
+using MiningSimulatorByKPMM.Locations.Market;
+using MiningSimulatorByKPMM.Locations.Bank;
+using MiningSimulatorByKPMM.PersonalItems;
+using NUnit.Framework;
+using MiningSimulatorByKPMM.DwarfsTypes;
+
+namespace SimulationTests.Market_Tests
+{
+	public class MarketTests
+	{
+		Market market;
+
+		[SetUp]
+		public void Setup()
+		{
+			market = new Market();
+		}
+
+		[Test]
+		public void ShouldBe0eachProductInMerket()
+		{
+			//given
+
+			//when
+			decimal result1 = market.marketState[E_ProductsType.Alcohol];
+			decimal result2 = market.marketState[E_ProductsType.Food];
+
+			//then
+			Assert.AreEqual(0, result1);
+			Assert.AreEqual(0, result2);
+		}
+
+		[Test]
+		public void ShopAccountShouldBe0()
+		{
+			//given
+			//when
+			decimal result = market.shopMoneyAccount.OverallAccount;
+			//then
+			Assert.AreEqual(0, result);
+		}
+
+		[Test]
+		public void MarketShouldHave50foodSoldInMarketState()
+		{
+			//given
+			List<Dwarf> dwarfsList = new List<Dwarf>();
+			Dwarf Andrzej = new Dwarf(E_DwarfType.Dwarf_Father);
+			Andrzej.BankAccount.ReceivedMoney(100);
+			dwarfsList.Add(Andrzej);
+
+			//when
+			market.PerformShopping(dwarfsList);
+			decimal result = market.marketState[E_ProductsType.Food];
+
+			//then
+			Assert.AreEqual(50, result);
+		}
+
+	}
+}
