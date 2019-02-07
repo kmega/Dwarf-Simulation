b065d1a8ab719e2b521d770b22920af486968c9d
Paweł Howaniec <paolopek@gmail.com>
2019-02-07 16:26:14 +0100

    bank works with market, count tax and added changes to accounts

--- 20190205/MiningSimulatorByKPMM/MiningSimulatorByKPMM/Locations/Market/Market.cs
+++ 20190205/MiningSimulatorByKPMM/MiningSimulatorByKPMM/Locations/Market/Market.cs
@@ -18,30 +18,32 @@ namespace MiningSimulatorByKPMM.Locations.Market
 			marketState.Add(E_ProductsType.Alcohol, 0);
 		}
 		
-		public void PerformShopping(List<Dwarf> customers)
+		public void PerformShopping(List<Dwarf> customers, GeneralBank bank)
 		{
 			foreach (var customer in customers)
 			{
 				if (customer.DwarfType == E_DwarfType.Dwarf_Single)
 				{
-					BuyProdcutsFromMarket(customer, E_ProductsType.Alcohol);
+					BuyProdcutsFromMarket(customer, E_ProductsType.Alcohol, bank);
 				}
 				else if (customer.DwarfType == E_DwarfType.Dwarf_Father)
 				{
-					BuyProdcutsFromMarket(customer, E_ProductsType.Food);
+					BuyProdcutsFromMarket(customer, E_ProductsType.Food, bank);
 				}
 			}
 		}
 
-		public void BuyProdcutsFromMarket(Dwarf customer, E_ProductsType productType)
+		public void BuyProdcutsFromMarket(Dwarf customer, E_ProductsType productType, GeneralBank bank)
 		{
 			decimal recipe = customer.BankAccount.LastInput / 2;
 
 			decimal amountOfProduct = marketState[productType];
 			amountOfProduct += recipe;
 			marketState[productType] = amountOfProduct;
-			
-			shopMoneyAccount.ReceivedMoney(recipe);
+
+			bank.PayTax(recipe);
+
+			shopMoneyAccount.ReceivedMoney(recipe * 0.77m);
 			shopMoneyAccount.CalculateOverallAccount();
 		}
 	}
---
+++ 20190205/MiningSimulatorByKPMM/SimulationTests/Market Tests/FakeDwarfsListForTest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MiningSimulatorByKPMM.Enums;
+using MiningSimulatorByKPMM.Locations.Market;
+using MiningSimulatorByKPMM.Locations.Bank;
+using MiningSimulatorByKPMM.PersonalItems;
+using NUnit.Framework;
+using MiningSimulatorByKPMM.DwarfsTypes;
+
+namespace SimulationTests.Market_Tests
+{
+	public class FakeDwarfsListForTest
+	{
+		public List<Dwarf> FakeList()
+		{
+			List<Dwarf> dwarfsListTest = new List<Dwarf>();
+			Dwarf Andrzej = new Dwarf(E_DwarfType.Dwarf_Father);
+			Dwarf Andrzej2 = new Dwarf(E_DwarfType.Dwarf_Father);
+			Dwarf Andrzej3 = new Dwarf(E_DwarfType.Dwarf_Father);
+			Dwarf Andrzej4 = new Dwarf(E_DwarfType.Dwarf_Single);
+			Dwarf Andrzej5 = new Dwarf(E_DwarfType.Dwarf_Single);
+			Dwarf Andrzej6 = new Dwarf(E_DwarfType.Dwarf_Single);
+			Dwarf Andrzej7 = new Dwarf(E_DwarfType.Dwarf_Sluggard);
+			Dwarf Andrzej8 = new Dwarf(E_DwarfType.Dwarf_Sluggard);
+			Dwarf Andrzej9 = new Dwarf(E_DwarfType.Dwarf_Suicide);
+
+			Andrzej.BankAccount.ReceivedMoney(200);
+			Andrzej2.BankAccount.ReceivedMoney(200);
+			Andrzej3.BankAccount.ReceivedMoney(200);
+			Andrzej4.BankAccount.ReceivedMoney(200);
+			Andrzej5.BankAccount.ReceivedMoney(200);
+			Andrzej6.BankAccount.ReceivedMoney(200);
+			Andrzej7.BankAccount.ReceivedMoney(100);
+			Andrzej8.BankAccount.ReceivedMoney(100);
+			Andrzej9.BankAccount.ReceivedMoney(0);
+
+			dwarfsListTest.Add(Andrzej);
+			dwarfsListTest.Add(Andrzej2);
+			dwarfsListTest.Add(Andrzej3);
+			dwarfsListTest.Add(Andrzej4);
+			dwarfsListTest.Add(Andrzej5);
+			dwarfsListTest.Add(Andrzej6);
+			dwarfsListTest.Add(Andrzej7);
+			dwarfsListTest.Add(Andrzej8);
+			dwarfsListTest.Add(Andrzej9);
+
+			return dwarfsListTest;
+		}
+	}
+}
