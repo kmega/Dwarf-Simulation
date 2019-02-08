175135fd6e2f5d727e9adc744737bcf0d9c0f819
Michał Czyrnek <e.s.michael.c@gmail.cok>
2019-02-08 15:56:42 +0100

    FakeGuild prepared

---
+++ 20190205/MiningSimulatorByKPMM/SimulationTests/E2ESimulationTests/FakeGuildFactory.cs
@@ -0,0 +1,18 @@
+﻿
+using MiningSimulatorByKPMM.Locations.Guild;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimulationTests.E2ESimulationTests
+{
+    internal class FakeGuildFactory
+    {
+        internal static Guild Create(ICreateOreValue value)
+        {
+            return new Guild(value);
+        }
+
+    }
+}
