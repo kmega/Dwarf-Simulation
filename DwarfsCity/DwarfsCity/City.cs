5a81bbfb83b6e9efbd452428ff626b2591236f9b
Matthew-1 <matikm2@gmail.com>
2019-02-08 18:57:12 +0100

    two different scenarios before main loop

--- DwarfsCity/DwarfsCity/City.cs
+++ DwarfsCity/DwarfsCity/City.cs
@@ -26,7 +26,8 @@ namespace DwarfsCity
             Shop shop = new Shop();
             Cementary cementary = new Cementary();                      
             hospital.InitialNumberOfDwarfs(dwarfs, 10);
-            dwarfs.AddRange(DwarfFactory.CreateMultipleDwarfs(2,Type.Saboteur));
+            dwarfs.InsertRange(2, DwarfFactory.CreateMultipleDwarfs(2, Type.Saboteur));
+            dwarfs.InsertRange(7, DwarfFactory.CreateMultipleDwarfs(2, Type.Saboteur));
 
             for (int i = 0; i < 30; i++)
             {
