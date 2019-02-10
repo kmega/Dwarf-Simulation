172c818578cb86eaf9507e462e0b95fdabb763c9
tetrael <kozlowskiprzemek@gmail.com>
2019-02-10 19:45:52 +0100

    Graveyard can now burry dead dwarfs

--- dwarfmine/DwarfMineSimulator/DwarfLife/Buildings/Graveyard/Graveyard.cs
+++ dwarfmine/DwarfMineSimulator/DwarfLife/Buildings/Graveyard/Graveyard.cs
@@ -1,10 +1,23 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using DwarfLife.Dwarfs;
+
 namespace DwarfLife.Buildings.Graveyard
 {
     public class Graveyard
     {
+        public List<IDwarf> DeadDwarfs { get; private set; }
+
         public Graveyard()
         {
+            DeadDwarfs = new List<IDwarf>();
+        }
+
+        public void BurryDeadDwarfs(List<IDwarf> dwarfs)
+        {
+            DeadDwarfs = dwarfs.Where(dead => dead.Alive == false).ToList();
+            DeadDwarfs.ForEach(dead => dwarfs.Remove(dead));
         }
     }
 }
