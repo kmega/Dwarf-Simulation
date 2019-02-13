d6d47d5bd1558f1b37a0946fca64795b16590cb4
Krzysztof Gebski <krzysztof.gebski@gmail.com>
2019-02-13 16:35:03 +0100

    Added fields to Dwarf model

--- DwarfTown/Dwarf.Town/Models/BackPack.cs
+++ DwarfTown/Dwarf.Town/Models/BackPack.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Dwarf.Town.Models
+﻿namespace Dwarf.Town.Models
 {
     public class BackPack
     {
--- DwarfTown/Dwarf.Town/SimulationState.cs
+++ DwarfTown/Dwarf.Town/SimulationState.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Dwarf.Town
+﻿namespace Dwarf.Town
 {
-    class SimulationState
+    internal class SimulationState
     {
     }
 }
