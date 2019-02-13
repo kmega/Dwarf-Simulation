7416ac522457289b4a8e13ebed470750849fd39e
Keboon <jankocon7@gmail.com>
2019-02-13 23:02:35 +0100

    Added Logger functionality

---
+++ ThorinsCompany/ThorinsCompany/Raports/Logger.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ThorinsCompany.Raports
+{
+    public sealed class Logger
+    {
+        static private Logger logger = new Logger();
+        private List<string> reports = new List<string>();
+
+        private Logger() { }
+        static public Logger GetInstance() => logger;
+        public void AddLog(string log)
+        {
+            reports.Add(log);
+        }
+        public List<string> GetLogs() => reports;
+
+        public void ClearData()
+        {
+            reports.Clear();
+        }
+
+    }
+}
---
+++ ThorinsCompany/ThorinsCompanyTests/LoggerTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using ThorinsCompany.Raports;
+
+namespace ThorinsCompanyTests
+{
+    public class LoggerTests
+    {
+
+        [Test]
+        public void ShouldReturnEmptyListWhenDataIsCleared()
+        {
+            // given
+            Logger logger = Logger.GetInstance();
+            int expectedListSize = 0;
+
+            // when
+            logger.AddLog("Dwarf bought Alcohol");
+            logger.AddLog("Dwarf bought Food");
+            logger.ClearData();
+
+            // then
+            int actualListSize = logger.GetLogs().Count;
+            Assert.AreEqual(expectedListSize, actualListSize);
+        }
+
+        [Test]
+        public void ShouldReturnLogsWhenLogsExist()
+        {
+            // given
+            Logger logger = Logger.GetInstance();
+            logger.ClearData();
+            List<string> Logs = new List<string>();
+            Logs.Add("Dwarf bought Alcohol");
+            Logs.Add("Dwarf bought Food");
+            Logs.Add("Dwarf bought Alcohol");
+
+            // when
+            foreach (string log in Logs)
+            {
+                logger.AddLog(log);
+            }
+
+            // then
+            CollectionAssert.AreEqual(Logs, logger.GetLogs());
+
+        }
+
+    }
+}
