ee1f08b48836bf04b4815558102f765eeb8f588e
Michał Czyrnek <e.s.michael.c@gmail.cok>
2019-02-13 18:04:23 +0100

    Create GuildFactory

---
+++ DwarfTown/Dwarf.TownTests/GuildTest/GuildTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace Dwarf.TownTests
+{
+    [TestFixture]
+    public class GuildTests
+    {
+        [Test]
+        public void ReturPaymentForOneDwarfWithOneDirtGold()
+        {
+           
+        }
+    }
+}
