a1557667d91ae9a983eae7254d92daba169e5fe3
Krzysztof Gebski <krzysztof.gebski@gmail.com>
2019-02-13 18:01:10 +0100

    Added Canteen and Hospital classes

---
+++ DwarfTown/Dwarf.Town/Locations/Canteen.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dwarf.Town.Locations
+{
+    public class Canteen
+    {
+        public Canteen(int foodRations)
+        {
+            FoodRations = foodRations;
+        }
+        public int FoodRations { get; set; }
+
+        public void SpendFoodRations(IList<Dwarf> dwarves)
+        {
+            FoodRations -= dwarves.Count;
+        }
+    }
+}
---
+++ DwarfTown/Dwarf.Town/Locations/Hospital.cs
@@ -0,0 +1,43 @@
+﻿using Dwarf.Town.Enums;
+using Dwarf.Town.Interfaces;
+using System;
+
+namespace Dwarf.Town.Locations
+{
+    internal class Hospital : IChance, IGenerate
+    {
+        public Dwarf Generate()
+        {
+            Dwarf dwarf = new Dwarf();
+
+            if (GenerateChance(1, 100) == 1)
+            {
+                int chance = GenerateChance(1, 100);
+
+                if (chance >= 1 && chance <= 33)
+                {
+                    dwarf = new Dwarf() { DwarfType = DwarfType.FATHER };
+                }
+                if (chance > 33 && chance <= 66)
+                {
+                    dwarf = new Dwarf() { DwarfType = DwarfType.IDLER };
+                }
+                if (chance > 66 && chance <= 99)
+                {
+                    dwarf = new Dwarf() { DwarfType = DwarfType.SINGLE };
+                }
+                if (chance == 100)
+                {
+                    dwarf = new Dwarf() { DwarfType = DwarfType.SUICIDE };
+                }
+            }
+            return dwarf;
+        }
+
+        public int GenerateChance(int lowerBound, int upperBound)
+        {
+            Random random = new Random();
+            return random.Next(lowerBound, upperBound);
+        }
+    }
+}
