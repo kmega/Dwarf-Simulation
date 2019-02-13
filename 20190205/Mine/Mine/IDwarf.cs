28662e3a7c19728aed04ab7ac3440db301d27c74
Michal Targiel <mtargiel@gmail.com>
2019-02-13 16:28:30 +0100

    init

---
+++ 20190205/Mine/Mine/IDwarf.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mine
+{
+    interface IDwarf
+    {
+        IWork work { get; set; }
+        IBackPack backpack { get; set; }
+        
+
+    }
+
+    internal interface IWork
+    {
+    }
+}
---
+++ 20190205/Mine/Mine/ore.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mine
+{
+    enum Ore
+    {
+        Gold,
+        Silver,
+        DirtGold,
+        Mithril
+
+    }
+}
