09e73d7461283767a4c2b9ab636ef579eb090d0d
piotr <piotrrzeznik@gmail.com>
2019-02-13 15:40:41 +0100

    new base

---
+++ 20190205/Mine/Mine/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Mine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
