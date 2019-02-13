722c5450728547b36eb0c019c27da5a96c1b8e57
Marcin Pochrzest <pochrzest.marcin@gmail.com>
2019-02-13 18:28:43 +0100

    Hospital in progress, waiting for dwarftype generator withour suicides

--- 20190213/DwarfLifeSimulation/DwarfLifeSimulation/Locations/Hospital/Hospital.cs
+++ 20190213/DwarfLifeSimulation/DwarfLifeSimulation/Locations/Hospital/Hospital.cs
@@ -1,5 +1,8 @@
 ﻿using DwarfLifeSimulation.ApplicationLogic;
 using DwarfLifeSimulation.Interfaces;
+using DwarfLifeSimulation.Randomizer.DwarfNameRandomizer;
+using DwarfLifeSimulation.Randomizer.DwarfTypeRandomizer;
+using DwarfLifeSimulation.Randomizer.IsDwarfBornRandomizer;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -8,6 +11,31 @@ namespace DwarfLifeSimulation.Locations.Hospital
 {
     public class Hospital
     {
+        private IIsDwarfBornRandomizer _isDwarfBornRandomizer;
+        private IDwarfTypeRandomizer _dwarfTypeRandomizer;
+        private IDwarfNameRandomizer _dwarfNameRandomizer;
+
+        public Hospital(IIsDwarfBornRandomizer isDwarfBornRandomizer, 
+            IDwarfTypeRandomizer dwarfTypeRandomizer, 
+            IDwarfNameRandomizer dwarfNameRandomizer)
+        {
+            SetInitialGenerators(isDwarfBornRandomizer,
+                dwarfTypeRandomizer, dwarfNameRandomizer);           
+        }
+        public Hospital()
+        {
+            SetInitialGenerators(new DwarfBornGenerationStrategy(),
+              new DwarfTypeGenerationStrategy(), new DwarfNameGenerationStrategy()); 
+        }
+        private void SetInitialGenerators(IIsDwarfBornRandomizer isDwarfBornRandomizer,
+            IDwarfTypeRandomizer dwarfTypeRandomizer,
+            IDwarfNameRandomizer dwarfNameRandomizer)
+        {
+            _isDwarfBornRandomizer = isDwarfBornRandomizer;
+            _dwarfTypeRandomizer = dwarfTypeRandomizer;
+            _dwarfNameRandomizer = dwarfNameRandomizer;
+        }
+
         public void Create(SimulationState simulationState)
         {
             if(simulationState.Turn == 1)
