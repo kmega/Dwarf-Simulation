0da1c1ad2f40cb2b0e7ae56d2b1d9aa12d60c4e4
Lukasz Radzik <radziklukasz@wp.pl>
2019-02-14 18:49:41 +0100

    Add Hospital with a few tests

--- DwarfSimulation/DwarfSimulation/Locations/Hospital.cs
+++ DwarfSimulation/DwarfSimulation/Locations/Hospital.cs
@@ -1,10 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 
 namespace DwarfSimulation
 {
     internal class Hospital
     {
+        internal List<Dwarf> BornDwarf(List<Dwarf> dwarves, bool chance)
+        {
+            if (chance)
+            {
+                Builder builder = new Builder();
+               dwarves = dwarves.Concat(builder.CreateDwarves(1)).ToList();
+                return dwarves;
+            }
+            return dwarves;
+        }
+
+        internal bool ChanceOfBirth()
+        {
+            Randomizer random = new Randomizer();
+            int chance = random.ReturnFromTo(1, 100);
+            return (chance == 1);
+        }
     }
 }
