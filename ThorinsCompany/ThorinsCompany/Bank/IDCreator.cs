d9739d338f4c2d1763cd0986a1c9bfc9da9faf20
Jan <jankocon7@gmail.com>
2019-02-14 16:50:57 +0100

    Added IDCreator

---
+++ ThorinsCompany/ThorinsCompany/Bank/IDCreator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ThorinsCompany.Bank
+{
+    public static class IDCreator
+    {
+        private static int UniqeID = 0;
+        public static int GetUniqeID()
+        {
+            UniqeID++;
+            return UniqeID;
+        }
+    }
+}
