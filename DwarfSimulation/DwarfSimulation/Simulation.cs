60f149baa053e9ed033ec8e1951c06e5c0a3944a
Lukasz Radzik <radziklukasz@wp.pl>
2019-02-15 17:30:59 +0100

    Change class Simulation, Create location object instance into day method

--- DwarfSimulation/DwarfSimulation/Simulation.cs
+++ DwarfSimulation/DwarfSimulation/Simulation.cs
@@ -10,47 +10,43 @@ namespace DwarfSimulation
         List<Shaft> _shafts = new List<Shaft>();
         Raport _raport = new Raport();
 
-        //location
-        Hospital _hospital = new Hospital(new Randomizer());
-        Graveyard _graveyard = new Graveyard();
-        DiningRoom _diningRoom = new DiningRoom();
-        Shop _shop = new Shop();
-        Guild guild = new Guild(new Randomizer());
-    
-
         internal void Start()
         {
             Prepare();
             for (int i = 0; i < 30; i++)
             {
                 if (!Day(i)) break;
-               
-
             }
             _raport.Display();
         }
 
         internal bool Day(int dayNumber)
         {
+            Randomizer randomizer = new Randomizer();
+
             //Hospital
+            Hospital hospital = new Hospital(randomizer);
             if (dayNumber != 0)
-            { _dwarves = _hospital.BornDwarf(_dwarves, _raport); }
+            { _dwarves = hospital.BornDwarf(_dwarves, _raport); }
 
 
             //Mines
             //Guild
+            Guild guild = new Guild(randomizer);
 
             //Graveyard
-            _dwarves = _graveyard.DeleteDeadDwarfFromList(_dwarves, _raport);
-            if (!_graveyard.AnybodyLives(_dwarves)) return false;
+            Graveyard graveyard = new Graveyard();
+            _dwarves = graveyard.DeleteDeadDwarfFromList(_dwarves, _raport);
+            if (!graveyard.AnybodyLives(_dwarves)) return false;
 
             //DinningRoom
-            if (!_diningRoom.CanEat(_dwarves, _raport)) return false;
-            _diningRoom.DwarfsEat(_dwarves, _raport);
+            DiningRoom diningRoom = new DiningRoom();
+            if (!diningRoom.CanEat(_dwarves, _raport)) return false;
+            diningRoom.DwarfsEat(_dwarves, _raport);
 
 
             //Shop
-
+            Shop shop = new Shop();
 
             return true;
         }
