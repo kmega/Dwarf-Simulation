45123c8c36f6fc8eab08fa4c1022d86de63afe64
Paweł Howaniec <paolopek@gmail.com>
2019-02-15 17:51:44 +0100

    added logger logic, change to simulation state for end raport display

--- 20190213/DwarfLifeSimulation/DwarfLifeSimulation/ApplicationLogic/SimulationState.cs
+++ 20190213/DwarfLifeSimulation/DwarfLifeSimulation/ApplicationLogic/SimulationState.cs
@@ -1,5 +1,6 @@
 ﻿using DwarfLifeSimulation.Dwarves;
 using DwarfLifeSimulation.Dwarves.Interfaces;
+using DwarfLifeSimulation.Enums;
 using System.Collections.Generic;
 
 namespace DwarfLifeSimulation.ApplicationLogic
@@ -8,8 +9,16 @@ namespace DwarfLifeSimulation.ApplicationLogic
     {
         public int turn { get; set; }
         public List<IDwarf> dwarves { get; set; }
+		public Dictionary<ProductType, decimal> _shopState { get; set; }
+		public int _numberOfBirths { get; set; }
+		public Dictionary<MineralType, decimal[]> _extractedOre { get; set; }
+		public int _deadDwarves { get; set; }
+		public decimal _shopBankAccount { get; set; }
+		public decimal _guildBankAccount { get; set; }
+		public decimal _taxBankAccount { get; set; }
 
-        public SimulationState()
+
+		public SimulationState()
         {
             turn = 1;
             dwarves = new List<IDwarf>();
--- 20190213/DwarfLifeSimulation/DwarfLifeSimulation/Loggers/ILog.cs
+++ 20190213/DwarfLifeSimulation/DwarfLifeSimulation/Loggers/ILog.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using DwarfLifeSimulation.ApplicationLogic;
 
 namespace DwarfLifeSimulation.Loggers
 {
     public interface ILog
     {
         void AddLog(string message);
-        void DisplayReport();
+        void DisplayReport(SimulationState state);
     }
 }
