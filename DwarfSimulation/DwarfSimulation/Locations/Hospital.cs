2d6b05d4c9efa39928b18b4cf7f734779bf9b8a5
Lukasz Radzik <radziklukasz@wp.pl>
2019-02-15 15:28:50 +0100

    Add method Display and Update Raport in Hospital class, create class simulation

--- DwarfSimulation/DwarfSimulation/Locations/Hospital.cs
+++ DwarfSimulation/DwarfSimulation/Locations/Hospital.cs
@@ -7,22 +7,61 @@ namespace DwarfSimulation
 {
     internal class Hospital
     {
-        internal List<Dwarf> BornDwarf(List<Dwarf> dwarves, bool chance)
+        internal List<Dwarf> BornDwarf(List<Dwarf> dwarves, bool IsBorn, Raport raport)
         {
-            if (chance)
+            if (IsBorn)
             {
+                List<Dwarf> BornDwarf = new List<Dwarf>();
                 Builder builder = new Builder();
-               dwarves = dwarves.Concat(builder.CreateDwarves(1)).ToList();
+                BornDwarf = builder.CreateDwarves(1);
+
+                Display(BornDwarf);
+                UpdateRaport(raport, BornDwarf);
+
+                dwarves = dwarves.Concat(BornDwarf).ToList();
                 return dwarves;
             }
             return dwarves;
         }
 
-        internal bool ChanceOfBirth()
+        internal bool IsBorn()
         {
             Randomizer random = new Randomizer();
             int chance = random.ReturnFromTo(1, 100);
             return (chance == 1);
+        }
+
+        internal void Display(List<Dwarf> BornDwarf)
+        {
+            List<string> output = new List<string>();
+            Outputer outputer = new Outputer();
+            output.Add("");
+            output.Add("### Hospital ###");
+            output.Add("Born dwarf " + BornDwarf[0].DwarfType);
+            outputer.Display(output);
+        }
+
+        internal void UpdateRaport(Raport raport, List<Dwarf> BornDwarf)
+        {
+            raport.TotalBorn += 1;
+
+            switch (BornDwarf[0].DwarfType)
+            {
+                case DwarfType.Father:
+                    raport.FathersBorn += 1; 
+                    break;
+                case DwarfType.Single:
+                    raport.SingleBorn += 1;
+                    break;
+                case DwarfType.Lazy:
+                    raport.LazyBorn += 1;
+                    break;
+                case DwarfType.Suicider:
+                    raport.SuiciderBorn += 1;
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
--- DwarfSimulation/DwarfSimulationTests/HospitalTests.cs
+++ DwarfSimulation/DwarfSimulationTests/HospitalTests.cs
@@ -20,12 +20,15 @@ namespace HospitalTests
             List<Dwarf> dwarves = CreateDwarves(10);
             Assert.AreEqual(dwarves.Count, 10);
             Hospital hospital = new Hospital();
+            Raport raport = new Raport();
+            Assert.AreEqual(raport.TotalBorn, 0);
 
             //when
-            dwarves = hospital.BornDwarf(dwarves, true);
+            dwarves = hospital.BornDwarf(dwarves, true, raport);
 
             //then
             Assert.AreEqual(dwarves.Count, 11);
+            Assert.AreEqual(raport.TotalBorn, 1);
         }
 
         [Test]
@@ -35,12 +38,15 @@ namespace HospitalTests
             List<Dwarf> dwarves = CreateDwarves(10);
             Assert.AreEqual(dwarves.Count, 10);
             Hospital hospital = new Hospital();
+            Raport raport = new Raport();
+            Assert.AreEqual(raport.TotalBorn, 0);
 
             //when
-            dwarves = hospital.BornDwarf(dwarves, false);
+            dwarves = hospital.BornDwarf(dwarves, false, raport);
 
             //then
             Assert.AreEqual(dwarves.Count, 10);
+            Assert.AreEqual(raport.TotalBorn, 0);
         }
     }
 }
