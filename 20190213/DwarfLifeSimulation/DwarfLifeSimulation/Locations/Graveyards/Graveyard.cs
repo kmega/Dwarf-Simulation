dcefcc2dae95f84ace5411fcb045575a2944f272
Aykro <pochrzest.marcin@gmail.com>
2019-02-15 13:06:53 +0100

    Added Graveyard

---
+++ 20190213/DwarfLifeSimulation/DwarfLifeSimulation/Locations/Graveyards/Graveyard.cs
@@ -0,0 +1,36 @@
+﻿using DwarfLifeSimulation.Dwarves.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DwarfLifeSimulation.Locations.Graveyards
+{
+    public class Graveyard
+    {
+        private List<IDwarf> deadDwarves;
+        
+
+        public int DeadDwarvesAmount
+        {
+            get { return deadDwarves.Count; }
+        }
+
+        public Graveyard()
+        {
+            deadDwarves = new List<IDwarf>();
+        }
+
+        public void BuryDeadDwarves(List<IDwarf> dwarves)
+        {
+            for(int i = dwarves.Count; i>0; i--)
+            {
+                if(dwarves[i]._isAlive == false)
+                {
+                    var dead = dwarves[i];
+                    deadDwarves.Add(dead);
+                    dwarves.Remove(dead);
+                }
+            }
+        }
+    }
+}
