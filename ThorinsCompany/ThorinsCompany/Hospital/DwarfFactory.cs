fb988c34c13dc5dfedb037850a016bdaf59e7de9
Jan <jankocon7@gmail.com>
2019-02-17 10:21:19 +0100

    Fixed DwarfFactory

--- ThorinsCompany/ThorinsCompany/Hospital/DwarfFactory.cs
+++ ThorinsCompany/ThorinsCompany/Hospital/DwarfFactory.cs
@@ -16,7 +16,7 @@ namespace ThorinsCompany
                 case DwarfType.Lazy:
                     return new Dwarf(dwarfType, new LazyShoppingStrategy(), new StandardWorkingStrategy());
                 case DwarfType.Single:
-                    return new Dwarf(dwarfType, new LazyShoppingStrategy(), new StandardWorkingStrategy());
+                    return new Dwarf(dwarfType, new SingleShoppingStrategy(), new StandardWorkingStrategy());
                 case DwarfType.Bomber:
                     return new Dwarf(dwarfType, new LazyShoppingStrategy(), new BomberWorkingStrategy());      
             }
