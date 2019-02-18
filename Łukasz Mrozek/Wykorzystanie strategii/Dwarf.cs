4375388405ef7ebb6ce69fe57f5054362d32eeac
--replace-all <luki990@onet.eu>
2019-02-18 16:46:06 +0100

    Łukasz Mrozek - fragmenty kodu do rekrutacji

---
+++ Łukasz Mrozek/Wykorzystanie strategii/Dwarf.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DwarfsTown
+{
+    public class Dwarf
+    {
+        public TypeEnum Type { get; private set; }
+        public BankAccount BankAccount;
+        public Backpack Backpack;
+        IBehaviorOnTheShaft shaftStrategy;
+
+        public Dwarf(TypeEnum type)
+        {
+            Type = new TypeEnum();
+            Backpack = new Backpack();
+            BankAccount = new BankAccount();
+            Type = type;
+            if (type == TypeEnum.Saboteur) shaftStrategy = new SaboteurStrategy();
+            else shaftStrategy = new CommonDwarfStrategy();
+        }
+
+        public void Working(Shaft shaft)
+        {
+            shaftStrategy.DoYourJob(shaft, this.Backpack);
+        }
+    }
+}
