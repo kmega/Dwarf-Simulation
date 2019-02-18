b8fba5d82124c242c68bf0da0c8f0f3d6e6b0d0d
hinren <projekt.pierscien@gmail.com>
2019-02-18 18:49:55 +0100

    AutoFac

---
+++ AutoFac/AutoFac/AutoFac/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autofac;
+
+namespace AutoFaca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //A ->IB -> B -> IC, ID, IE
+            //IC -> C
+            //ID -> D
+            //IE - IF, IG
+            //IF -> F
+            //IG -> G
+            var builder = new ContainerBuilder();
+        }
+        interface IB
+        {
+
+        }
+        interface IG
+        {
+
+        }
+        interface IF
+        {
+
+        }
+        interface IE
+        {
+
+        }
+        interface IC
+        {
+
+        }
+        interface ID
+        {
+
+        }
+        class A : IB
+        {
+            public A()
+            {
+
+            }
+        }
+        class B : IC, ID, IE
+        {
+            public B()
+            {
+
+            }
+        }
+        class E : IF, IG
+        {
+            public E()
+            {
+
+            }
+        }
+        class C
+        {
+            public C()
+            {
+
+            }
+        }
+        class D
+        {
+            public D()
+            {
+
+            }
+        }
+        class F
+        {
+            public F()
+            {
+
+            }
+        }
+        class G
+        {
+            public G()
+            {
+
+            }
+        }
+    }
+}
---
+++ AutoFac/AutoFac/AutoFac/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Ogólne informacje o zestawie są kontrolowane poprzez następujący 
+// zestaw atrybutów. Zmień wartości tych atrybutów, aby zmodyfikować informacje
+// powiązane z zestawem.
+[assembly: AssemblyTitle("AutoFac")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AutoFac")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Ustawienie elementu ComVisible na wartość false sprawia, że typy w tym zestawie są niewidoczne
+// dla składników COM. Jeśli potrzebny jest dostęp do typu w tym zestawie z
+// COM, ustaw wartość true dla atrybutu ComVisible tego typu.
+[assembly: ComVisible(false)]
+
+// Następujący identyfikator GUID jest identyfikatorem biblioteki typów w przypadku udostępnienia tego projektu w modelu COM
+[assembly: Guid("f95beeb6-d4e5-4aae-8ff3-5f21576cd4f5")]
+
+// Informacje o wersji zestawu zawierają następujące cztery wartości:
+//
+//      Wersja główna
+//      Wersja pomocnicza
+//      Numer kompilacji
+//      Rewizja
+//
+// Możesz określić wszystkie wartości lub użyć domyślnych numerów kompilacji i poprawki
+// przy użyciu symbolu „*”, tak jak pokazano poniżej:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
