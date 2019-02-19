4df37a4e1df1be0da160e92d1c4465a3363d72b9
piotr570 <piotr.rzeznik@edu.uekat.pl>
2019-02-19 21:55:51 +0100

    renamed few things

--- 20190214/Mine2.0/MIne2.0Tests/T_TeamSplitter.cs
+++ 20190214/Mine2.0/MIne2.0Tests/T_TeamSplitter.cs
@@ -8,35 +8,31 @@ namespace MIne2._0Tests
     public class T_TeamSplitter
     {
         [Test]
-        public void splits3()
+        public void SplitsListOf3IntoOneWorkinglistAndBaselistIsEmpty()
         {
             //given
-            var splitter = new TeamSplitter();
-
             List<IWork> workers = new List<IWork>(new Hospital().CreateInitialState(3));
 
-            var result = new TeamSplitter().SplitWorkersIntoTeam(workers);
             //when
+            var splittedWorkers = new TeamSplitter().SplitWorkersIntoTeam(workers);
 
             //then
             Assert.IsTrue(workers.Count == 0);
-            Assert.IsTrue(result.Count == 3);
+            Assert.IsTrue(splittedWorkers.Count == 3);
         }
 
         [Test]
-        public void splits7()
+        public void SplitsListOf3IntoOneWorkinglistAndBaselistHasTwoAwaitingDwarves()
         {
             //given
-            var splitter = new TeamSplitter();
-
             List<IWork> workers = new List<IWork>(new Hospital().CreateInitialState(7));
 
-            var result = new TeamSplitter().SplitWorkersIntoTeam(workers);
             //when
+            var splittedWorkers = new TeamSplitter().SplitWorkersIntoTeam(workers);
 
             //then
             Assert.IsTrue(workers.Count == 2);
-            Assert.IsTrue(result.Count == 5);
+            Assert.IsTrue(splittedWorkers.Count == 5);
         }
     }
 }
